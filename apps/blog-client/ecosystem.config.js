34acca83fcd6e89665a9ec2d16d576e5b46cf641
yungblud <killi8n@gmail.com>
2024-10-28 19:20:37 +0900

    feat: :fire: implemented ecosystem.config.js blog client

---
+++ apps/blog-client/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'blog-client',
+      script: 'server.js',
+      cwd: '.next/standalone/apps/blog-client', // Set working directory to the standalone output
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
