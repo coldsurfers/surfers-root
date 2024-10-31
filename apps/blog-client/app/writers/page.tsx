0750f3f2549d7f314367cce55c2fbb9817a9349e
yungblud <killi8n@gmail.com>
2024-10-31 10:52:45 +0900

    feat(blog-client): :art: implemented writers page

--- apps/blog-client/app/writers/page.tsx
+++ apps/blog-client/app/writers/page.tsx
@@ -1,3 +1,13 @@
-export default function WritersPage() {
-  return null
+import notionInstance from 'lib/notionInstance'
+import { WritersPageClient } from './page.client'
+
+async function retrieveUsers() {
+  const users = await notionInstance.users.list({})
+  const { results } = users
+  return results.filter((value) => value.type === 'person')
+}
+
+export default async function WritersPage() {
+  const users = await retrieveUsers()
+  return <WritersPageClient users={users} />
 }
