0ea8bbeaee55a947862c2a8f1f9d791dbb6b1a92
yungblud <killi8n@gmail.com>
2024-11-01 22:39:09 +0900

    feat(common): :art: modified lint settings

--- packages/ocean-road-design-tokens/dist/js/color/variables-theme.js
+++ packages/ocean-road-design-tokens/dist/js/color/variables-theme.js
@@ -4,265 +4,221 @@
  */
 
 module.exports = {
-  "color": {
-    "background": {
-      "1": {
-        "value": "#ffffff",
-        "darkValue": "#000000",
-        "comment": "background color 1",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.white}",
-          "darkValue": "{oc.black}",
-          "comment": "background color 1"
+  color: {
+    background: {
+      1: {
+        value: '#ffffff',
+        darkValue: '#000000',
+        comment: 'background color 1',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.white}',
+          darkValue: '{oc.black}',
+          comment: 'background color 1',
         },
-        "name": "ColorBackground1",
-        "attributes": {
-          "category": "color",
-          "type": "background",
-          "item": "1"
+        name: 'ColorBackground1',
+        attributes: {
+          category: 'color',
+          type: 'background',
+          item: '1',
         },
-        "path": [
-          "color",
-          "background",
-          "1"
-        ]
+        path: ['color', 'background', '1'],
       },
-      "2": {
-        "value": "#f1f3f5",
-        "darkValue": "#212529",
-        "comment": "background color 2",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.1}",
-          "darkValue": "{oc.gray.9}",
-          "comment": "background color 2"
+      2: {
+        value: '#f1f3f5',
+        darkValue: '#212529',
+        comment: 'background color 2',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.1}',
+          darkValue: '{oc.gray.9}',
+          comment: 'background color 2',
         },
-        "name": "ColorBackground2",
-        "attributes": {
-          "category": "color",
-          "type": "background",
-          "item": "2"
+        name: 'ColorBackground2',
+        attributes: {
+          category: 'color',
+          type: 'background',
+          item: '2',
         },
-        "path": [
-          "color",
-          "background",
-          "2"
-        ]
+        path: ['color', 'background', '2'],
       },
-      "3": {
-        "value": "#e9ecef",
-        "darkValue": "#343a40",
-        "comment": "background color 3",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.2}",
-          "darkValue": "{oc.gray.8}",
-          "comment": "background color 3"
+      3: {
+        value: '#e9ecef',
+        darkValue: '#343a40',
+        comment: 'background color 3',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.2}',
+          darkValue: '{oc.gray.8}',
+          comment: 'background color 3',
         },
-        "name": "ColorBackground3",
-        "attributes": {
-          "category": "color",
-          "type": "background",
-          "item": "3"
+        name: 'ColorBackground3',
+        attributes: {
+          category: 'color',
+          type: 'background',
+          item: '3',
         },
-        "path": [
-          "color",
-          "background",
-          "3"
-        ]
+        path: ['color', 'background', '3'],
       },
-      "4": {
-        "value": "#dee2e6",
-        "darkValue": "#495057",
-        "comment": "background color 3",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.3}",
-          "darkValue": "{oc.gray.7}",
-          "comment": "background color 3"
+      4: {
+        value: '#dee2e6',
+        darkValue: '#495057',
+        comment: 'background color 3',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.3}',
+          darkValue: '{oc.gray.7}',
+          comment: 'background color 3',
         },
-        "name": "ColorBackground4",
-        "attributes": {
-          "category": "color",
-          "type": "background",
-          "item": "4"
+        name: 'ColorBackground4',
+        attributes: {
+          category: 'color',
+          type: 'background',
+          item: '4',
         },
-        "path": [
-          "color",
-          "background",
-          "4"
-        ]
+        path: ['color', 'background', '4'],
       },
-      "5": {
-        "value": "#ced4da",
-        "darkValue": "#868e96",
-        "comment": "background color 3",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.4}",
-          "darkValue": "{oc.gray.6}",
-          "comment": "background color 3"
+      5: {
+        value: '#ced4da',
+        darkValue: '#868e96',
+        comment: 'background color 3',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.4}',
+          darkValue: '{oc.gray.6}',
+          comment: 'background color 3',
         },
-        "name": "ColorBackground5",
-        "attributes": {
-          "category": "color",
-          "type": "background",
-          "item": "5"
+        name: 'ColorBackground5',
+        attributes: {
+          category: 'color',
+          type: 'background',
+          item: '5',
         },
-        "path": [
-          "color",
-          "background",
-          "5"
-        ]
-      }
+        path: ['color', 'background', '5'],
+      },
     },
-    "foreground": {
-      "1": {
-        "value": "#212529",
-        "darkValue": "#f1f3f5",
-        "comment": "foreground color 1",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.9}",
-          "darkValue": "{oc.gray.1}",
-          "comment": "foreground color 1"
+    foreground: {
+      1: {
+        value: '#212529',
+        darkValue: '#f1f3f5',
+        comment: 'foreground color 1',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.9}',
+          darkValue: '{oc.gray.1}',
+          comment: 'foreground color 1',
         },
-        "name": "ColorForeground1",
-        "attributes": {
-          "category": "color",
-          "type": "foreground",
-          "item": "1"
+        name: 'ColorForeground1',
+        attributes: {
+          category: 'color',
+          type: 'foreground',
+          item: '1',
         },
-        "path": [
-          "color",
-          "foreground",
-          "1"
-        ]
+        path: ['color', 'foreground', '1'],
       },
-      "2": {
-        "value": "#343a40",
-        "darkValue": "#e9ecef",
-        "comment": "foreground color 2",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.8}",
-          "darkValue": "{oc.gray.2}",
-          "comment": "foreground color 2"
+      2: {
+        value: '#343a40',
+        darkValue: '#e9ecef',
+        comment: 'foreground color 2',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.8}',
+          darkValue: '{oc.gray.2}',
+          comment: 'foreground color 2',
         },
-        "name": "ColorForeground2",
-        "attributes": {
-          "category": "color",
-          "type": "foreground",
-          "item": "2"
+        name: 'ColorForeground2',
+        attributes: {
+          category: 'color',
+          type: 'foreground',
+          item: '2',
         },
-        "path": [
-          "color",
-          "foreground",
-          "2"
-        ]
+        path: ['color', 'foreground', '2'],
       },
-      "3": {
-        "value": "#495057",
-        "darkValue": "#dee2e6",
-        "comment": "foreground color 3",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.7}",
-          "darkValue": "{oc.gray.3}",
-          "comment": "foreground color 3"
+      3: {
+        value: '#495057',
+        darkValue: '#dee2e6',
+        comment: 'foreground color 3',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.7}',
+          darkValue: '{oc.gray.3}',
+          comment: 'foreground color 3',
         },
-        "name": "ColorForeground3",
-        "attributes": {
-          "category": "color",
-          "type": "foreground",
-          "item": "3"
+        name: 'ColorForeground3',
+        attributes: {
+          category: 'color',
+          type: 'foreground',
+          item: '3',
         },
-        "path": [
-          "color",
-          "foreground",
-          "3"
-        ]
+        path: ['color', 'foreground', '3'],
       },
-      "4": {
-        "value": "#868e96",
-        "darkValue": "#ced4da",
-        "comment": "foreground color 4",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.6}",
-          "darkValue": "{oc.gray.4}",
-          "comment": "foreground color 4"
+      4: {
+        value: '#868e96',
+        darkValue: '#ced4da',
+        comment: 'foreground color 4',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.6}',
+          darkValue: '{oc.gray.4}',
+          comment: 'foreground color 4',
         },
-        "name": "ColorForeground4",
-        "attributes": {
-          "category": "color",
-          "type": "foreground",
-          "item": "4"
+        name: 'ColorForeground4',
+        attributes: {
+          category: 'color',
+          type: 'foreground',
+          item: '4',
         },
-        "path": [
-          "color",
-          "foreground",
-          "4"
-        ]
-      }
+        path: ['color', 'foreground', '4'],
+      },
     },
-    "border": {
-      "1": {
-        "value": "#f1f3f5",
-        "darkValue": "#495057",
-        "comment": "border color 1",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.1}",
-          "darkValue": "{oc.gray.7}",
-          "comment": "border color 1"
+    border: {
+      1: {
+        value: '#f1f3f5',
+        darkValue: '#495057',
+        comment: 'border color 1',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.1}',
+          darkValue: '{oc.gray.7}',
+          comment: 'border color 1',
         },
-        "name": "ColorBorder1",
-        "attributes": {
-          "category": "color",
-          "type": "border",
-          "item": "1"
+        name: 'ColorBorder1',
+        attributes: {
+          category: 'color',
+          type: 'border',
+          item: '1',
         },
-        "path": [
-          "color",
-          "border",
-          "1"
-        ]
+        path: ['color', 'border', '1'],
       },
-      "2": {
-        "value": "#e9ecef",
-        "darkValue": "#868e96",
-        "comment": "border color 2",
-        "filePath": "tokens/color/theme-alias.json",
-        "isSource": true,
-        "original": {
-          "value": "{oc.gray.2}",
-          "darkValue": "{oc.gray.6}",
-          "comment": "border color 2"
+      2: {
+        value: '#e9ecef',
+        darkValue: '#868e96',
+        comment: 'border color 2',
+        filePath: 'tokens/color/theme-alias.json',
+        isSource: true,
+        original: {
+          value: '{oc.gray.2}',
+          darkValue: '{oc.gray.6}',
+          comment: 'border color 2',
         },
-        "name": "ColorBorder2",
-        "attributes": {
-          "category": "color",
-          "type": "border",
-          "item": "2"
+        name: 'ColorBorder2',
+        attributes: {
+          category: 'color',
+          type: 'border',
+          item: '2',
         },
-        "path": [
-          "color",
-          "border",
-          "2"
-        ]
-      }
-    }
-  }
-};
+        path: ['color', 'border', '2'],
+      },
+    },
+  },
+}
