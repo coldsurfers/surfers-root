124c254f59ba43ca88ba14ffd5acbc7210c68fb0
yungblud <killi8n@gmail.com>
2024-11-04 14:58:17 +0900

    feat(coldsurf-io): :art: implemented ocean road theme provider

--- apps/coldsurf-io/app/layout.tsx
+++ apps/coldsurf-io/app/layout.tsx
@@ -1,11 +1,11 @@
 import {
   AuthStoreProvider,
+  OceanRoadThemeRegistry,
   QueryClientRegistry,
   RegistryProvider,
   StyleSheetRegistry,
   StyledComponentsRegistry,
 } from '@/libs/registries'
-import '@coldsurfers/hotsurf/global-light-only.css'
 import type { Metadata } from 'next'
 import { Noto_Sans as notoSans } from 'next/font/google'
 import { cookies } from 'next/headers'
@@ -25,9 +25,21 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
 
   return (
     <html lang="en">
-      <head></head>
-      <body className={[notoSansFont.className].join(' ')}>
-        <RegistryProvider registries={[StyledComponentsRegistry, StyleSheetRegistry]}>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" type="image/png" />
+      </head>
+      <body className={notoSansFont.className}>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+              const theme = systemDark ? 'dark' : 'light';
+              document.documentElement.setAttribute('data-theme', theme);
+          `,
+          }}
+        />
+        <RegistryProvider registries={[StyledComponentsRegistry, StyleSheetRegistry, OceanRoadThemeRegistry]}>
           <AuthStoreProvider accessToken={accessToken} refreshToken={refreshToken}>
             <QueryClientRegistry>
               <LayoutWrapper>{children}</LayoutWrapper>
