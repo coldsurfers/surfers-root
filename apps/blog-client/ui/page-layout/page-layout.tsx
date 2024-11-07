dde55d1901b72889e84303aabb739d9f0bd6ae8d
yungblud <killi8n@gmail.com>
2024-11-07 10:52:17 +0900

    chore(blog-client): :art: modified UI and pdf snapshot

--- apps/blog-client/ui/page-layout/page-layout.tsx
+++ apps/blog-client/ui/page-layout/page-layout.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { Header } from '@/ui'
+import { usePathname } from 'i18n/routing'
 import { PropsWithChildren } from 'react'
 import { StyledPageLayoutContainer } from './page-layout.styled'
 
 export const PageLayout = ({ children }: PropsWithChildren) => {
+  const pathname = usePathname()
   return (
     <StyledPageLayoutContainer>
-      <Header />
+      {process.env.NODE_ENV === 'development' && pathname === '/resume' ? null : <Header />}
       {children}
     </StyledPageLayoutContainer>
   )
