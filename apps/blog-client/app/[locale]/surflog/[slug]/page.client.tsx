573bb95b3026818ac6ce69fef2886209c44a97d1
yungblud <killi8n@gmail.com>
2024-11-08 13:07:22 +0900

    fix(blog-client): :art: removed early import prism js

--- apps/blog-client/app/[locale]/surflog/[slug]/page.client.tsx
+++ apps/blog-client/app/[locale]/surflog/[slug]/page.client.tsx
@@ -1,9 +1,6 @@
 'use client'
 
 import { LogDetailRenderer } from '@/features/notion'
-// prismjs
-import 'prismjs/components/prism-jsx'
-import 'prismjs/themes/prism-tomorrow.css'
 
 export const SurflogSlugPageClient = (props: { pageBlocks: never[]; pageTitle: string }) => {
   return <LogDetailRenderer {...props} />
