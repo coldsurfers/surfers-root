0082ce1d86e696d8b00f3efa26c8f82c6fe76a9e
yungblud <killi8n@gmail.com>
2024-11-09 13:45:58 +0900

    feat(blog-client): :page_facing_up: added page styled

--- apps/blog-client/app/[locale]/tags/page.tsx
+++ apps/blog-client/app/[locale]/tags/page.tsx
@@ -1,8 +1,13 @@
+import { getTags } from '@/lib'
 import { PageProps } from 'i18n/types'
 import { setRequestLocale } from 'next-intl/server'
+import { TagsPageClient } from './page.client'
 
-export default function TagsPage({ params }: PageProps) {
+export default async function TagsPage({ params }: PageProps) {
   const { locale } = params
   setRequestLocale(locale)
-  return null
+
+  const allTags = await getTags()
+
+  return <TagsPageClient tags={allTags} />
 }
