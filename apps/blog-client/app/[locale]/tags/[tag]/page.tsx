441a27f39b3d312391bb3f5afada3c5a0a4c5acf
yungblud <killi8n@gmail.com>
2024-11-09 14:15:52 +0900

    feat(blog-client): :zap: generateStaticParams for tags page

--- apps/blog-client/app/[locale]/tags/[tag]/page.tsx
+++ apps/blog-client/app/[locale]/tags/[tag]/page.tsx
@@ -1,7 +1,24 @@
-import { queryLogs } from '@/lib'
+import { getTags, queryLogs } from '@/lib'
+import { routing } from 'i18n/routing'
 import { PageProps } from 'i18n/types'
 import { setRequestLocale } from 'next-intl/server'
 import { TagDetailPageClient } from './page.client'
+
+export async function generateStaticParams() {
+  const locales = routing.locales.map((locale) => ({ locale }))
+  const allTags = await getTags()
+  const params = locales
+    .map((locale) => {
+      return allTags.map((tag) => {
+        return {
+          tag: tag.name,
+          locale: locale.locale,
+        }
+      })
+    })
+    .flat()
+  return params
+}
 
 export default async function TagDetailPage({
   params,
--- apps/blog-client/app/[locale]/tags/page.tsx
+++ apps/blog-client/app/[locale]/tags/page.tsx
@@ -1,7 +1,12 @@
 import { getTags } from '@/lib'
+import { routing } from 'i18n/routing'
 import { PageProps } from 'i18n/types'
 import { setRequestLocale } from 'next-intl/server'
 import { TagsPageClient } from './page.client'
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }))
+}
 
 export default async function TagsPage({ params }: PageProps) {
   const { locale } = params
