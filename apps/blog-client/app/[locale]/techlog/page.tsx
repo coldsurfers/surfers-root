7300b23a103feb4cb86d813b1e96f3579d006f67
yungblud <killi8n@gmail.com>
2024-11-10 19:39:58 +0900

    refactor(blog-client): :recycle: refactored blog log data types

--- apps/blog-client/app/[locale]/techlog/page.tsx
+++ apps/blog-client/app/[locale]/techlog/page.tsx
@@ -1,8 +1,8 @@
 import { queryLogs } from '@/lib/utils'
+import { LogListPage } from '@/ui'
 import { routing } from 'i18n/routing'
-import { AppLocale, PageProps } from 'i18n/types'
+import { PageProps } from 'i18n/types'
 import { setRequestLocale } from 'next-intl/server'
-import { TechlogPageClient } from './page.client'
 
 export const revalidate = 3600
 
@@ -10,13 +10,9 @@ export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }))
 }
 
-async function initPage(locale: AppLocale) {
-  return await await queryLogs('techlog', locale)
-}
-
 export default async function TechlogPage({ params }: PageProps) {
   setRequestLocale(params.locale)
-  const logs = await initPage(params.locale)
+  const logs = await queryLogs('techlog', params.locale)
 
-  return <TechlogPageClient logs={logs} />
+  return <LogListPage logs={logs} />
 }
