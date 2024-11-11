3618f6c64492814ddee80be977646d7d292d0c5c
yungblud <killi8n@gmail.com>
2024-11-11 22:24:37 +0900

    feat(wamuseum-client): :rocket: implemented gql client with utils for ssr

--- apps/wamuseum-client/hooks/useConcertListQuery.ts
+++ apps/wamuseum-client/hooks/useConcertListQuery.ts
@@ -12,7 +12,7 @@ export default function useConcertListQuery(
         createdAt: 'asc' | 'desc'
       }
     }
-  >
+  >,
 ) {
   return useQuery<
     {
---
+++ apps/wamuseum-client/libs/index.ts
@@ -0,0 +1,3 @@
+export * from './constants'
+export * from './create-apollo-client'
+export * from './use-apollo'
