cd3e652ade971bbf4201f9a43c31565cb8006fac
yungblud <killi8n@gmail.com>
2024-11-13 23:47:25 +0900

    feat(ocean-road): :art: implemented numberOfLines to text

--- packages/ocean-road/src/text/text.styled.ts
+++ packages/ocean-road/src/text/text.styled.ts
@@ -1,7 +1,18 @@
 import styled from '@emotion/styled'
 import { semantics } from '../tokens'
 
-export const StyledTextContainer = styled.span`
+export const StyledTextContainer = styled.span<{ numberOfLines?: number }>`
   color: ${semantics.color.foreground[1]};
   white-space: pre-wrap;
+
+  ${({ numberOfLines }) =>
+    numberOfLines &&
+    `
+  display: -webkit-box;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: ${numberOfLines};
+  -webkit-box-orient: vertical;
+  word-break: break-all;
+  `}
 `
--- packages/ocean-road/src/text/text.tsx
+++ packages/ocean-road/src/text/text.tsx
@@ -7,15 +7,18 @@ export const Text = ({
   children,
   style,
   as,
+  numberOfLines,
   ...otherProps
 }: PropsWithChildren<{
   style?: CSSProperties
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   as?: ElementType<any, keyof JSX.IntrinsicElements>
+  numberOfLines?: number
 }>) => {
   return (
     <StyledTextContainer
       as={as}
+      numberOfLines={numberOfLines}
       style={{
         ...style,
       }}
