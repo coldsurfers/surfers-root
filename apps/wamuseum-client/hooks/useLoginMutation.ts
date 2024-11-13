f5acdd32513bdec1438e1e38d4981031e0d0059f
yungblud <killi8n@gmail.com>
2024-11-13 19:43:56 +0900

    refactor(wamuseum-client): :fire: refactored create user request form

--- apps/wamuseum-client/hooks/useLoginMutation.ts
+++ apps/wamuseum-client/hooks/useLoginMutation.ts
@@ -1,24 +1,11 @@
 import { ApolloCache, DefaultContext, MutationHookOptions, useMutation } from '@apollo/client'
-import { LoginData } from '../gql/schema'
 import { LoginMutation } from '../gql/mutations'
+import { LoginData } from '../gql/schema'
 
-export default function useLoginMutation(
-  options?: MutationHookOptions<
-    { login: LoginData },
-    { input: { email: string; password: string } },
-    DefaultContext,
-    ApolloCache<any>
-  >,
-) {
-  return useMutation<
-    {
-      login: LoginData
-    },
-    {
-      input: {
-        email: string
-        password: string
-      }
-    }
-  >(LoginMutation, options)
+type TData = { login: LoginData }
+type TVaraibles = { input: { email: string; password: string } }
+type Options = MutationHookOptions<TData, TVaraibles, DefaultContext, ApolloCache<unknown>>
+
+export default function useLoginMutation(options?: Options) {
+  return useMutation<TData, TVaraibles>(LoginMutation, options)
 }
