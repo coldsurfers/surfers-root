13daf7a7ec8e64b1ba43157961d0ad46e0bd4f99
yungblud <killi8n@gmail.com>
2024-11-13 23:49:11 +0900

    feat(blog-client): :art: removed ui store

--- apps/blog-client/ui/header/header.tsx
+++ apps/blog-client/ui/header/header.tsx
@@ -1,9 +1,6 @@
 'use client'
 
-import { useUIStore } from '@/stores'
 import { Link, usePathname } from 'i18n/routing'
-import { useEffect } from 'react'
-import { useShallow } from 'zustand/shallow'
 import {
   StyledHeaderBigContainer,
   StyledHeaderContainer,
@@ -13,32 +10,6 @@ import {
 
 export const Header = () => {
   const pathname = usePathname()
-  const { activeTab, setActiveTab } = useUIStore(
-    useShallow((state) => ({
-      activeTab: state.activeTab,
-      setActiveTab: state.setActiveTab,
-    })),
-  )
-
-  useEffect(() => {
-    const handleRouteChange = (pathname: string) => {
-      switch (true) {
-        case pathname.startsWith('/surflog'):
-          setActiveTab('surflog')
-          break
-        case pathname.startsWith('/techlog'):
-          setActiveTab('techlog')
-          break
-        case pathname.startsWith('/writers'):
-          setActiveTab('writers')
-          break
-        default:
-          setActiveTab(null)
-      }
-    }
-
-    handleRouteChange(pathname)
-  }, [pathname, setActiveTab])
 
   return (
     <StyledHeaderBigContainer>
@@ -47,19 +18,13 @@ export const Header = () => {
       </Link>
       <StyledHeaderContainer>
         <Link href="/surflog">
-          <StyledHeaderHeading $isActive={activeTab === 'surflog' || pathname.startsWith('/surflog')}>
-            Surflogs
-          </StyledHeaderHeading>
+          <StyledHeaderHeading $isActive={pathname.startsWith('/surflog')}>Surflogs</StyledHeaderHeading>
         </Link>
         <Link href="/techlog">
-          <StyledHeaderHeading $isActive={activeTab === 'techlog' || pathname.startsWith('/techlog')}>
-            Techlogs
-          </StyledHeaderHeading>
+          <StyledHeaderHeading $isActive={pathname.startsWith('/techlog')}>Techlogs</StyledHeaderHeading>
         </Link>
         <Link href="/writers">
-          <StyledHeaderHeading $isActive={activeTab === 'writers' || pathname.startsWith('/writers')}>
-            Writers
-          </StyledHeaderHeading>
+          <StyledHeaderHeading $isActive={pathname.startsWith('/writers')}>Writers</StyledHeaderHeading>
         </Link>
       </StyledHeaderContainer>
     </StyledHeaderBigContainer>
