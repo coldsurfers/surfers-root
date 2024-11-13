27760aa54440eb9a1c8464af7b703702f5583348
yungblud <killi8n@gmail.com>
2024-11-13 14:17:41 +0900

    feat(blog-client): :art: added yaml prisma

--- apps/blog-client/features/notion/renderer/renderer.js
+++ apps/blog-client/features/notion/renderer/renderer.js
@@ -10,6 +10,7 @@ import 'prismjs/components/prism-json'
 import 'prismjs/components/prism-jsx'
 import 'prismjs/components/prism-tsx'
 import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-yaml'
 
 import 'prismjs/themes/prism-tomorrow.css'
 
