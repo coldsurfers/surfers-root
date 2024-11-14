1b25d1ea4abc644bd97f27dbc6a3d124c58aac51
yungblud <killi8n@gmail.com>
2024-11-14 11:29:38 +0900

    feat(billets-app): :lock: removed ArtistAddScreen

--- apps/billets-app/src/navigations/my-stack-navigation/my-stack-navigation.types.ts
+++ apps/billets-app/src/navigations/my-stack-navigation/my-stack-navigation.types.ts
@@ -1,13 +1,11 @@
 import { CompositeScreenProps } from '@react-navigation/native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { Screens } from '../../lib/navigations'
-import { ArtistAddScreenParam } from '../../screens/ArtistAddScreen'
 import { MyScreenParam } from '../../screens/MyScreen.types'
 import { MainTabProp } from '../main-tab-navigation/main-tab-navigation.types'
 
 export type MyStackParam = {
   [Screens.MyScreen]: MyScreenParam
-  [Screens.ArtistAddScreen]: ArtistAddScreenParam
 }
 
 export type MyStackProp<T extends keyof MyStackParam> = CompositeScreenProps<
