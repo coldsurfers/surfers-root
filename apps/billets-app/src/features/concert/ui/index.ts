c8f0acbfcb9f7c0c7011836920f5942e17c706e8
yungblud <killi8n@gmail.com>
2024-11-14 12:38:47 +0900

    feat(billets-app): :art: header item for concert detail section list

--- apps/billets-app/src/features/concert/ui/index.ts
+++ apps/billets-app/src/features/concert/ui/index.ts
@@ -1,3 +1,4 @@
 export * from './concert-detail-section-list'
+export * from './concert-detail-section-list-header-item'
 export * from './concert-detail-section-list-item'
 export * from './concert-list-item'
