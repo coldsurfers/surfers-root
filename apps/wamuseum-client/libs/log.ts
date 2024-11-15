ceb2fffac49de30569c9e8342692ef912d654f2e
yungblud <killi8n@gmail.com>
2024-11-15 15:42:05 +0900

    chore(wamuseum-client): :zap: added exports lib

--- apps/wamuseum-client/libs/log.ts
+++ apps/wamuseum-client/libs/log.ts
@@ -1,3 +1,1 @@
-const log = (message: unknown) => process.env.NODE_ENV === 'development' && console.log(message)
-
-export default log
+export const log = (message: unknown) => process.env.NODE_ENV === 'development' && console.log(message)
