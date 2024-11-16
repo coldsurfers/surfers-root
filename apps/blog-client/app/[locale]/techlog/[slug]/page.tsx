f3fbba95981fa1da662e946f903dad225070c802
yungblud <killi8n@gmail.com>
2024-11-16 23:42:12 +0900

    feat(blog-client): :art: features logs with query

--- apps/blog-client/app/[locale]/techlog/[slug]/page.tsx
+++ apps/blog-client/app/[locale]/techlog/[slug]/page.tsx
@@ -1,4 +1,5 @@
-import { getTechlogDetail, LogDetailRenderer, queryLogs } from '@/features'
+import { LogDetailRenderer, queryLogs } from '@/features'
+import { queryTechlogDetail } from '@/features/logs/logs.query'
 import { queryKeyFactory } from '@/lib/react-query/react-query.key-factory'
 import { getQueryClient } from '@/lib/react-query/react-query.utils'
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
@@ -18,7 +19,7 @@ export async function generateStaticParams() {
 
 export async function generateMetadata({ params }: PageProps<{ slug: string }>) {
   // fetch data
-  const page = await getTechlogDetail({ slug: params.slug ?? '', lang: params.locale })
+  const page = await queryTechlogDetail({ slug: params.slug ?? '', lang: params.locale })
   const pageTitle = page?.properties.Name.type === 'title' ? page.properties.Name.title.at(0)?.plain_text : ''
 
   if (!page || !pageTitle) {
--- apps/blog-client/features/logs/index.ts
+++ apps/blog-client/features/logs/index.ts
@@ -1,1 +1,2 @@
+export * from './logs.query'
 export * from './types'
