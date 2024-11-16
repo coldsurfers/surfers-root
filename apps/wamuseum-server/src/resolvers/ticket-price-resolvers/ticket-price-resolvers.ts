772de2d891d2dd9ebb4de6a7c1a92eee79121c3f
yungblud <killi8n@gmail.com>
2024-11-16 13:42:53 +0900

    feat(wamuseum-server): :tada: implemented concertTicketPrices query

--- apps/wamuseum-server/src/resolvers/ticket-price-resolvers/ticket-price-resolvers.ts
+++ apps/wamuseum-server/src/resolvers/ticket-price-resolvers/ticket-price-resolvers.ts
@@ -1,6 +1,24 @@
 import { Resolvers } from '../../../gql/resolvers-types'
+import { authorizeUser } from '../../utils/authHelpers'
 
 export const ticketPriceResolvers: Resolvers = {
-  Query: {},
+  Query: {
+    concertTicketPrices: async (parent, args, ctx) => {
+      try {
+        await authorizeUser(ctx, { requiredRole: 'staff' })
+        const dtos = await TicketPriceDTO.list(args.ticketId)
+        return {
+          __typename: 'TicketPriceList',
+          list: dtos.map((dto) => dto.serialize()),
+        }
+      } catch (e) {
+        return {
+          __typename: 'HttpError',
+          code: 500,
+          message: 'internal server error',
+        }
+      }
+    },
+  },
   Mutation: {},
 }
