f7528278dbd7e4e940090bce54dc82005aea6734
yungblud <killi8n@gmail.com>
2024-11-20 21:25:10 +0900

    feat(blog-client): :art: added squarelog, textlog

--- apps/blog-client/ui/log-list-page/log-list-page.tsx
+++ apps/blog-client/ui/log-list-page/log-list-page.tsx
@@ -3,12 +3,22 @@
 import { LogPlatform } from '@/features'
 import { useGetLogsQuery } from '@/lib'
 import { PostItem, PostListContainer } from '@/ui'
+import { Text } from '@coldsurfers/ocean-road'
 import { AppLocale } from 'i18n/types'
+import { useMemo } from 'react'
 
 export const LogListPage = ({ locale, platform }: { locale: AppLocale; platform: LogPlatform }) => {
   const { data: logs } = useGetLogsQuery({
     locale,
     platform,
   })
-  return <PostListContainer>{logs?.map((post) => <PostItem key={post.id} {...post} />)}</PostListContainer>
+  const title = useMemo(() => {
+    return `${platform[0].toUpperCase()}${platform.slice(1, platform.length)}`
+  }, [platform])
+  return (
+    <>
+      <Text as="h2">{title}</Text>
+      <PostListContainer>{logs?.map((post) => <PostItem key={post.id} {...post} />)}</PostListContainer>
+    </>
+  )
 }
