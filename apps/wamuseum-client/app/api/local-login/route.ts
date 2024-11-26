fe8ae65ab70740c4dcdda067667c11a1845ab979
yungblud <killi8n@gmail.com>
2024-11-26 12:11:24 +0900

    feat(wamuseum-client): :zap: implemented new auth handling for pages

--- apps/wamuseum-client/app/api/local-login/route.ts
+++ apps/wamuseum-client/app/api/local-login/route.ts
@@ -1,3 +1,4 @@
+import { COOKIE_ACCESS_TOKEN_KEY } from '@/utils/constants'
 import cookie from 'cookie'
 import { NextRequest, NextResponse } from 'next/server'
 
@@ -9,7 +10,7 @@ export async function POST(request: NextRequest) {
     return new Response('local login', {
       status: 200,
       headers: {
-        'Set-Cookie': cookie.serialize('accessToken', token, {
+        'Set-Cookie': cookie.serialize(COOKIE_ACCESS_TOKEN_KEY, token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
           maxAge: 60 * 60 * 24 * 7, // 1 week
--- apps/wamuseum-client/app/api/local-logout/route.ts
+++ apps/wamuseum-client/app/api/local-logout/route.ts
@@ -1,3 +1,4 @@
+import { COOKIE_ACCESS_TOKEN_KEY } from '@/utils/constants'
 import cookie from 'cookie'
 import { NextRequest } from 'next/server'
 
@@ -5,7 +6,7 @@ export async function POST(request: NextRequest) {
   return new Response('local logout', {
     status: 200,
     headers: {
-      'Set-Cookie': cookie.serialize('accessToken', '', {
+      'Set-Cookie': cookie.serialize(COOKIE_ACCESS_TOKEN_KEY, '', {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         expires: new Date(0), // Expire immediately
