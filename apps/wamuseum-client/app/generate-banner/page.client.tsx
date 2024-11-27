d2b462a8c408d1f607e3e3cd2923299603676af2
yungblud <killi8n@gmail.com>
2024-11-27 10:39:14 +0900

    feat(wamuseum-client): :art: implemented react hook form

--- apps/wamuseum-client/app/generate-banner/page.client.tsx
+++ apps/wamuseum-client/app/generate-banner/page.client.tsx
@@ -6,13 +6,7 @@ import { StyledPageWrapper } from './page.styled'
 export const GenerateBannerPageClient = () => {
   return (
     <StyledPageWrapper>
-      <BannerGenerator
-        artist="Jamie XX"
-        city="Seoul"
-        formattedDate="28. 11. 24"
-        title="In Waves Tour"
-        venue="YES24 LIVEHALL"
-      />
+      <BannerGenerator />
     </StyledPageWrapper>
   )
 }
--- apps/wamuseum-client/features/banner/ui/banner-generator/banner-generator.tsx
+++ apps/wamuseum-client/features/banner/ui/banner-generator/banner-generator.tsx
@@ -1,24 +1,31 @@
 'use client'
 
+import InputWithLabel from '@/ui/InputWithLabel'
 import { saveAs } from 'file-saver'
 import { toJpeg } from 'html-to-image'
-import { Bona_Nova } from 'next/font/google'
+import { Bona_Nova, Rancho, Rock_Salt } from 'next/font/google'
 import { useCallback, useRef } from 'react'
+import { useForm } from 'react-hook-form'
 import {
   ArtistText,
   CityText,
   FormattedDateText,
+  StyledBannerForm,
   StyledBannerPreviewWrapper,
   StyledGenerateButton,
   TitleText,
   VenueText,
 } from './banner-generator.styled'
-import { BannerGeneratorProps } from './banner-generator.types'
+import { BannerOptions } from './banner-generator.types'
 
 const bonaNova = Bona_Nova({ subsets: ['latin'], weight: '400' })
+const rockSalt = Rock_Salt({ subsets: ['latin'], weight: '400' })
+const rancho = Rancho({ subsets: ['latin'], weight: '400' })
 
-export const BannerGenerator = ({ artist, city, venue, formattedDate, title }: BannerGeneratorProps) => {
+export const BannerGenerator = () => {
   const bannerRef = useRef<HTMLDivElement>(null)
+  const { setValue, watch } = useForm<BannerOptions>()
+  const { artist, city, venue, formattedDate, title } = watch()
 
   const handleExport = useCallback(async () => {
     if (bannerRef.current) {
@@ -33,8 +40,20 @@ export const BannerGenerator = ({ artist, city, venue, formattedDate, title }: B
 
   return (
     <>
+      {/* Banner Form */}
+      <StyledBannerForm>
+        <InputWithLabel label="아티스트" value={artist} onChangeText={(text) => setValue('artist', text)} />
+        <InputWithLabel label="도시 이름" value={city} onChangeText={(text) => setValue('city', text)} />
+        <InputWithLabel label="콘서트 이름" value={title} onChangeText={(text) => setValue('title', text)} />
+        <InputWithLabel label="공연장" value={venue} onChangeText={(text) => setValue('venue', text)} />
+        <InputWithLabel
+          label="공연 날짜"
+          value={formattedDate}
+          onChangeText={(text) => setValue('formattedDate', text)}
+        />
+      </StyledBannerForm>
       {/* Banner Preview */}
-      <StyledBannerPreviewWrapper ref={bannerRef} className={bonaNova.className}>
+      <StyledBannerPreviewWrapper ref={bannerRef} className={rancho.className}>
         <ArtistText>{artist}</ArtistText>
         <CityText>{city}</CityText>
         <TitleText>{title}</TitleText>
