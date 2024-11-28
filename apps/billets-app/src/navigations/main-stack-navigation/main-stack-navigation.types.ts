42bc877a1a97be0f47d11b260045820f19c21374
yungblud <killi8n@gmail.com>
2024-11-28 15:26:37 +0900

    feat(billets-app): :art: implemented venue stack

--- apps/billets-app/src/navigations/main-stack-navigation/main-stack-navigation.types.ts
+++ apps/billets-app/src/navigations/main-stack-navigation/main-stack-navigation.types.ts
@@ -5,12 +5,14 @@ import { ConcertStackParam } from '../concert-stack-navigation'
 import { LoginStackParam } from '../login-stack-navigation/login-stack-navigation.types'
 import { MainTabNavigationParamList } from '../main-tab-navigation/main-tab-navigation.types'
 import { SubscribedStackParam } from '../subscribed-stack-navigation/subscribed-stack-navigation.types'
+import { VenueStackParam } from '../venue-stack-navigation'
 
 export type MainStackNavigationParamList = {
   [TabScreens.MainTabScreen]: NavigatorScreenParams<MainTabNavigationParamList>
   [StackScreens.LoginStackScreen]: NavigatorScreenParams<LoginStackParam>
   [StackScreens.ConcertStackScreen]: NavigatorScreenParams<ConcertStackParam>
   [StackScreens.SubscribedStackScreen]: NavigatorScreenParams<SubscribedStackParam>
+  [StackScreens.VenueStackScreen]: NavigatorScreenParams<VenueStackParam>
 }
 
 export type MainStackProp<T extends keyof MainStackNavigationParamList> = NativeStackScreenProps<
---
+++ apps/billets-app/src/screens/venue-detail-screen/venue-detail-screen.tsx
@@ -0,0 +1,3 @@
+export const VenueDetailScreen = () => {
+  return null
+}
