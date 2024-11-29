a6ca834e8b5db19907a3fdccec57e2ffd92bbf82
yungblud <killi8n@gmail.com>
2024-11-29 15:28:08 +0900

    feat(billets-web): :art: implemented pages

--- apps/billets-web/app/terms-of-service/page.tsx
+++ apps/billets-web/app/terms-of-service/page.tsx
@@ -1,3 +1,9 @@
-export default function TermsOfService() {
-  return <div>Terms of Service</div>
+import { NotionAPI } from 'notion-client'
+import { TermsOfServicePageClient } from './page.client'
+
+const notion = new NotionAPI()
+
+export default async function TermsOfService() {
+  const recordMap = await notion.getPage('Billets-14b2bbac578280648515e582a8d371a0')
+  return <TermsOfServicePageClient recordMap={recordMap} />
 }
