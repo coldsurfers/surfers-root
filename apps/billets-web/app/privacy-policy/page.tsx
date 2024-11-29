5793575b770ba82e9050d70a0e5b8cd90c22df41
yungblud <killi8n@gmail.com>
2024-11-29 15:28:08 +0900

    feat(billets-web): :art: implemented pages

--- apps/billets-web/app/privacy-policy/page.tsx
+++ apps/billets-web/app/privacy-policy/page.tsx
@@ -1,3 +1,9 @@
-export default function PrivacyPolicy() {
-  return <div>Privacy Policy</div>
+import { NotionAPI } from 'notion-client'
+import { PrivacyPolicyPageClient } from './page.client'
+
+const notion = new NotionAPI()
+
+export default async function PrivacyPolicy() {
+  const recordMap = await notion.getPage('Billets-14a2bbac578280d8bdbbcaff952d1ab5')
+  return <PrivacyPolicyPageClient recordMap={recordMap} />
 }
