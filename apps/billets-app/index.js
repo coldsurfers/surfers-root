cee95c2d1f174f2a152911cc33f685ab9aa56104
yungblud <killi8n@gmail.com>
2024-11-29 16:51:29 +0900

    feat(billets-app): :art: implemented gesture root view

--- apps/billets-app/index.js
+++ apps/billets-app/index.js
@@ -4,6 +4,7 @@
 
 import messaging from '@react-native-firebase/messaging'
 import { AppRegistry } from 'react-native'
+import 'react-native-gesture-handler'
 import App from './App'
 import { name as appName } from './app.json'
 import { applyConfig } from './src/lib/api/axiosClient'
