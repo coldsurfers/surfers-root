6703625ac984fee359c34d927a5f55579f672434
yungblud <killi8n@gmail.com>
2024-12-02 12:51:45 +0900

    refactor(billets-app): :art: enhanced venue detail concert list

--- apps/billets-app/src/features/venue/ui/venue-detail-concert-list/venue-detail-concert-list.tsx
+++ apps/billets-app/src/features/venue/ui/venue-detail-concert-list/venue-detail-concert-list.tsx
@@ -2,8 +2,8 @@ import { HorizontalConcertItem } from '@/features/concert/ui'
 import { useVenueConcertListQuery } from '@/lib/react-query/queries/use-venue-concert-list-query'
 import { ProfileThumbnail } from '@coldsurfers/ocean-road/native'
 import format from 'date-fns/format'
-import { useMemo } from 'react'
-import { ActivityIndicator, FlatList, StyleSheet, View } from 'react-native'
+import { useCallback, useMemo } from 'react'
+import { ActivityIndicator, FlatList, ListRenderItem, StyleSheet, View } from 'react-native'
 import { VenueDetailTop } from '../venue-detail-top'
 
 export const VenueDetailConcertList = ({
@@ -18,6 +18,7 @@ export const VenueDetailConcertList = ({
     isPending: isPendingVenueConcertList,
     isFetchingNextPage: isFetchingNextPageVenueConcerList,
     hasNextPage: hasNextPageVenueConcertList,
+    fetchNextPage: fetchNextPageVenueConcertList,
   } = useVenueConcertListQuery({
     venueId,
   })
@@ -26,6 +27,40 @@ export const VenueDetailConcertList = ({
   }, [venueConcertList?.pages])
 
   const isInitialLoading = isPendingVenueConcertList
+
+  const renderItem = useCallback<ListRenderItem<(typeof venueConcertListUIData)[number]>>(
+    (info) => {
+      return (
+        <HorizontalConcertItem
+          onPress={() => onPressItem?.({ concertId: info.item.id })}
+          title={info.item.title}
+          subtitle={format(new Date(info.item.date), 'EEE, MMM dd')}
+          description={info.item.venues.at(0)?.venueTitle ?? ''}
+          thumbnailComponent={
+            <ProfileThumbnail
+              type="square"
+              emptyBgText={info.item.title.at(0) ?? ''}
+              imageUrl={info.item.posters.at(0)?.imageUrl}
+              size="md"
+            />
+          }
+        />
+      )
+    },
+    [onPressItem],
+  )
+
+  const onEndReached = useCallback(async () => {
+    if (isPendingVenueConcertList || isFetchingNextPageVenueConcerList || !hasNextPageVenueConcertList) {
+      return
+    }
+    await fetchNextPageVenueConcertList()
+  }, [
+    fetchNextPageVenueConcertList,
+    hasNextPageVenueConcertList,
+    isFetchingNextPageVenueConcerList,
+    isPendingVenueConcertList,
+  ])
 
   if (isInitialLoading) {
     return (
@@ -39,26 +74,10 @@ export const VenueDetailConcertList = ({
     <FlatList
       ListHeaderComponent={<VenueDetailTop venueId={venueId} />}
       data={venueConcertListUIData}
-      renderItem={(info) => {
-        return (
-          <HorizontalConcertItem
-            onPress={() => onPressItem?.({ concertId: info.item.id })}
-            title={info.item.title}
-            subtitle={format(new Date(info.item.date), 'EEE, MMM dd')}
-            description={info.item.venues.at(0)?.venueTitle ?? ''}
-            thumbnailComponent={
-              <ProfileThumbnail
-                type="square"
-                emptyBgText={info.item.title.at(0) ?? ''}
-                imageUrl={info.item.posters.at(0)?.imageUrl}
-                size="md"
-              />
-            }
-          />
-        )
-      }}
+      renderItem={renderItem}
       keyExtractor={(item) => item.id}
       contentContainerStyle={styles.contentContainer}
+      onEndReached={onEndReached}
     />
   )
 }
