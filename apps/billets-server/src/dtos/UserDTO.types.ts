81f7bf119f30a8531c59fb29a7184968437bb0bc
yungblud <killi8n@gmail.com>
2024-12-02 17:25:33 +0900

    feat(billets-server): :art: implemented error types with user deactivate

--- apps/billets-server/src/dtos/UserDTO.types.ts
+++ apps/billets-server/src/dtos/UserDTO.types.ts
@@ -4,6 +4,7 @@ export const userDTOSerializedSchema = z.object({
   id: z.string(),
   email: z.string(),
   provider: z.string(),
+  deactivatedAt: z.date().nullable(),
 })
 
 export type UserDTOSerialized = z.infer<typeof userDTOSerializedSchema>
