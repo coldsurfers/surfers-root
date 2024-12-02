5c63b3f98bf94343fb135b2b6d6a1941500a0f4f
yungblud <killi8n@gmail.com>
2024-12-02 09:55:20 +0900

    feat(billets-app): :fire: implemented venue detail screen with react query and hooks

--- apps/billets-app/src/screens/venue-detail-screen/venue-detail-screen.tsx
+++ apps/billets-app/src/screens/venue-detail-screen/venue-detail-screen.tsx
@@ -1,5 +1,24 @@
+import { useVenueDetailQuery } from '@/lib/react-query'
+import { useVenueConcertListQuery } from '@/lib/react-query/queries/use-venue-concert-list-query'
 import { CommonScreenLayout } from '@/ui'
+import { useMemo } from 'react'
+import { useVenueDetailScreenRoute } from './venue-detail-screen.hooks'
 
 export const VenueDetailScreen = () => {
+  const route = useVenueDetailScreenRoute()
+  const { data: venueDetail } = useVenueDetailQuery({
+    id: route.params.id,
+  })
+  const { data: venueConcertList } = useVenueConcertListQuery({
+    venueId: route.params.id,
+  })
+
+  const venueDetailUIData = useMemo(() => {
+    return venueDetail?.data ?? null
+  }, [venueDetail?.data])
+  const venueConcertListUIData = useMemo(() => {
+    return venueConcertList?.pages.flat() ?? []
+  }, [venueConcertList?.pages])
+
   return <CommonScreenLayout></CommonScreenLayout>
 }
