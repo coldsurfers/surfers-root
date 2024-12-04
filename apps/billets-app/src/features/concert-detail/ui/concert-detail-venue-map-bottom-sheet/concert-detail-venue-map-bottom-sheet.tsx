b034a58f192346a4fedb17a067d043e51fcff16b
yungblud <killi8n@gmail.com>
2024-12-04 10:45:02 +0900

    feat(billets-app): :art: added backdrop to concert detail venue map bottom sheet modal

--- apps/billets-app/src/features/concert-detail/ui/concert-detail-venue-map-bottom-sheet/concert-detail-venue-map-bottom-sheet.tsx
+++ apps/billets-app/src/features/concert-detail/ui/concert-detail-venue-map-bottom-sheet/concert-detail-venue-map-bottom-sheet.tsx
@@ -1,9 +1,15 @@
 import { ConcertVenueMapView } from '@/features/map'
 import { openMap } from '@/lib/utils.map'
 import { Button, Text } from '@coldsurfers/ocean-road/native'
-import { BottomSheetModal, BottomSheetModalProvider, BottomSheetView } from '@gorhom/bottom-sheet'
+import {
+  BottomSheetBackdrop,
+  BottomSheetBackgroundProps,
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet'
 import Clipboard from '@react-native-clipboard/clipboard'
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useCallback } from 'react'
 import { Platform, StyleSheet, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { ConcertDetailVenueMapBottomSheetProps } from './concert-detail-venue-map-bottom-sheet.types'
@@ -11,9 +17,20 @@ import { ConcertDetailVenueMapBottomSheetProps } from './concert-detail-venue-ma
 export const ConcertDetailVenueMapBottomSheet = forwardRef<BottomSheetModal, ConcertDetailVenueMapBottomSheetProps>(
   ({ address, region, markerCoordinate }, ref) => {
     const { bottom: bottomInset } = useSafeAreaInsets()
+    const renderBackdrop = useCallback((props: BottomSheetBackgroundProps) => {
+      return <BottomSheetBackdrop {...props} appearsOnIndex={0} disappearsOnIndex={-1} />
+    }, [])
     return (
       <BottomSheetModalProvider>
-        <BottomSheetModal ref={ref}>
+        <BottomSheetModal
+          index={0}
+          ref={ref}
+          backdropComponent={renderBackdrop}
+          containerStyle={{
+            flex: 1,
+          }}
+          style={{ flex: 1 }}
+        >
           <BottomSheetView style={{ paddingBottom: bottomInset }}>
             <ConcertVenueMapView region={region} markerCoordinate={markerCoordinate} size="large" />
             <View style={styles.lineView}>
