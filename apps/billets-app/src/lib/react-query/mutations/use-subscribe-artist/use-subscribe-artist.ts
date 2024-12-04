e173eabc56d376c451929e524878b8492927b515
yungblud <killi8n@gmail.com>
2024-12-04 11:41:33 +0900

    feat(billets-app): :art: implemented subscribe artist, venue mutation

---
+++ apps/billets-app/src/lib/react-query/mutations/use-subscribe-artist/use-subscribe-artist.ts
@@ -0,0 +1,30 @@
+import client from '@/lib/api/openapi-client'
+import { OpenApiError } from '@/lib/api/openapi-error'
+import { useMutation, UseMutationOptions } from '@tanstack/react-query'
+
+type TVariables = {
+  artistId: string
+}
+
+type Options = UseMutationOptions<Awaited<ReturnType<typeof mutationFn>>, OpenApiError, TVariables>
+
+const mutationFn = async ({ artistId }: TVariables) => {
+  const response = await client.POST('/v1/subscribe/artist/{id}', {
+    params: {
+      path: {
+        id: artistId,
+      },
+    },
+  })
+  if (response.error) {
+    throw new OpenApiError(response.error)
+  }
+  return response.data
+}
+
+export const useSubscribeArtist = (options?: Options) => {
+  return useMutation({
+    ...options,
+    mutationFn,
+  })
+}
---
+++ apps/billets-app/src/lib/react-query/mutations/use-subscribe-venue/use-subscribe-venue.ts
@@ -0,0 +1,30 @@
+import client from '@/lib/api/openapi-client'
+import { OpenApiError } from '@/lib/api/openapi-error'
+import { useMutation, UseMutationOptions } from '@tanstack/react-query'
+
+type TVariables = {
+  venueId: string
+}
+
+type Options = UseMutationOptions<Awaited<ReturnType<typeof mutationFn>>, OpenApiError, TVariables>
+
+const mutationFn = async ({ venueId }: TVariables) => {
+  const response = await client.POST('/v1/subscribe/venue/{id}', {
+    params: {
+      path: {
+        id: venueId,
+      },
+    },
+  })
+  if (response.error) {
+    throw new OpenApiError(response.error)
+  }
+  return response.data
+}
+
+export const useSubscribeVenue = (options?: Options) => {
+  return useMutation({
+    ...options,
+    mutationFn,
+  })
+}
