e0adf8f9dee72c6c3ecd08dae1bd8dc1c7d178c0
yungblud <killi8n@gmail.com>
2024-12-04 11:42:47 +0900

    feat(billets-app): :art: modified namings of mutation

--- apps/billets-app/src/lib/react-query/mutations/index.ts
+++ apps/billets-app/src/lib/react-query/mutations/index.ts
@@ -1,5 +1,5 @@
 export * from './use-activate-user-mutation'
 export * from './use-deactivate-user-mutation'
 export * from './use-send-fcm-token-mutation'
-export * from './use-subscribe-artist'
-export * from './use-subscribe-venue'
+export * from './use-subscribe-artist-mutation'
+export * from './use-subscribe-venue-mutation'
