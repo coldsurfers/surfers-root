a123f5dbae4fe77864becd4c39f35847f56ab682
yungblud <killi8n@gmail.com>
2024-12-04 11:41:33 +0900

    feat(billets-app): :art: implemented subscribe artist, venue mutation

--- apps/billets-app/src/lib/react-query/mutations/index.ts
+++ apps/billets-app/src/lib/react-query/mutations/index.ts
@@ -1,3 +1,5 @@
 export * from './use-activate-user-mutation'
 export * from './use-deactivate-user-mutation'
 export * from './use-send-fcm-token-mutation'
+export * from './use-subscribe-artist'
+export * from './use-subscribe-venue'
