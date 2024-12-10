f8b7400d59015ada0c79afcb75f4b22e97791098
yungblud <killi8n@gmail.com>
2024-12-10 23:12:39 +0900

    feat(billets-web): :sparkles: implemented AASA json

--- apps/billets-web/next.config.js
+++ apps/billets-web/next.config.js
@@ -19,6 +19,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
