83b9d124f9170dc19ea53e29ae309c2c244501d5
yungblud <killi8n@gmail.com>
2024-12-10 10:21:10 +0900

    feat(billets-app): :package: set CODE_PUSH_DEPLOYMENT_KEY for react-native-config.d.ts

--- apps/billets-app/src/types/react-native-config.d.ts
+++ apps/billets-app/src/types/react-native-config.d.ts
@@ -4,6 +4,7 @@ declare module 'react-native-config' {
     GOOGLE_SIGNIN_WEB_CLIENT_ID?: string
     GOOGLE_SIGNIN_WEB_IOS_CLIENT_ID?: string
     ANDROID_GOOGLE_MAPS_API_KEY?: string
+    CODE_PUSH_DEPLOYMENT_KEY?: string
   }
 
   export const Config: NativeConfig
