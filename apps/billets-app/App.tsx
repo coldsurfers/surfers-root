542997cbdf614fe71a5d9465d80ead50a9244567
yungblud <killi8n@gmail.com>
2024-12-10 13:23:46 +0900

    feat(billets-app): :package: implemented ui codepush screen

--- apps/billets-app/App.tsx
+++ apps/billets-app/App.tsx
@@ -1,12 +1,14 @@
 import { AuthContextProvider, TabBarVisibleContextProvider, useFirebaseAnalytics, useFirebaseCrashlytics } from '@/lib'
 import { CommonScreenLayout } from '@/ui'
+import { colors } from '@coldsurfers/ocean-road'
 import { Text } from '@coldsurfers/ocean-road/native'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React, { PropsWithChildren, useEffect, useState } from 'react'
-import { StatusBar } from 'react-native'
+import { StatusBar, View } from 'react-native'
 import BootSplash from 'react-native-bootsplash'
 import codePush, { DownloadProgress } from 'react-native-code-push'
 import Config from 'react-native-config'
+import FastImage from 'react-native-fast-image'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import AppContainer from './src/AppContainer'
 
@@ -22,9 +24,34 @@ const queryClient = new QueryClient({
 })
 
 const CodepushUpdateScreen = ({ progress }: { progress: DownloadProgress }) => {
+  const percentage = `${(progress.receivedBytes / progress.totalBytes) * 100}%`
   return (
-    <CommonScreenLayout>
-      <Text>{JSON.stringify(progress)}</Text>
+    <CommonScreenLayout style={{ alignItems: 'center', justifyContent: 'center', paddingHorizontal: 12 }}>
+      <FastImage
+        source={require('assets/bootsplash/logo.png')}
+        style={{
+          width: 124,
+          height: 124,
+          borderRadius: 124 / 2,
+        }}
+      />
+      <View style={{ marginTop: 36, width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ width: '100%', backgroundColor: colors.oc.white.value, height: 24, borderRadius: 8 }}>
+          <View
+            style={{
+              width: `${(progress.receivedBytes / progress.totalBytes) * 100}%`,
+              backgroundColor: colors.oc.indigo[8].value,
+              height: '100%',
+              borderRadius: 8,
+            }}
+          />
+        </View>
+        <View style={{ marginTop: 24 }}>
+          <Text weight="medium" style={{ fontSize: 18 }}>
+            {percentage}
+          </Text>
+        </View>
+      </View>
     </CommonScreenLayout>
   )
 }
@@ -36,27 +63,38 @@ const BootSplashAwaiter = ({ children }: PropsWithChildren) => {
 
   useEffect(() => {
     const init = async () => {
-      await enableFirebaseAnalytics(true)
-      await enableFirebaseCrashlytics(true)
+      try {
+        await enableFirebaseAnalytics(true)
+        await enableFirebaseCrashlytics(true)
+      } catch (e) {
+        console.error('Error enabling Firebase services:', e)
+      }
     }
 
-    init().finally(async () => {
-      const update = await codePush.checkForUpdate(Config.CODE_PUSH_DEPLOYMENT_KEY)
-      if (update) {
-        update.download((progress) => {
-          if (progress.receivedBytes === progress.totalBytes) {
-            setProgress(null)
-            codePush.sync({
-              installMode: codePush.InstallMode.IMMEDIATE,
-            })
-            return
-          }
-          setProgress(progress)
-          BootSplash.hide({ fade: true })
-        })
-      } else {
-        BootSplash.hide({ fade: true })
+    const handleCodepushUpdate = async () => {
+      try {
+        await codePush.sync(
+          {
+            installMode: codePush.InstallMode.IMMEDIATE,
+            deploymentKey: Config.CODE_PUSH_DEPLOYMENT_KEY,
+          },
+          (status) => {
+            if (status === codePush.SyncStatus.UP_TO_DATE || status === codePush.SyncStatus.UPDATE_INSTALLED) {
+              setProgress(null)
+            }
+          },
+          (progress) => {
+            setProgress(progress)
+          },
+        )
+      } catch (e) {
+        console.error('CodePush update error:', e)
       }
+      BootSplash.hide({ fade: true })
+    }
+
+    init().finally(() => {
+      handleCodepushUpdate()
     })
   }, [enableFirebaseAnalytics, enableFirebaseCrashlytics])
 
