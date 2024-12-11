75487a09bda9de90e467afb0dab142fd1d9aa65e
yungblud <killi8n@gmail.com>
2024-12-11 11:31:54 +0900

    feat(billets-app): :tada: added google mobile ads ios app id

--- apps/billets-app/src/types/react-native-config.d.ts
+++ apps/billets-app/src/types/react-native-config.d.ts
@@ -6,6 +6,8 @@ declare module 'react-native-config' {
     ANDROID_GOOGLE_MAPS_API_KEY?: string
     IOS_CODE_PUSH_DEPLOYMENT_KEY?: string
     ANDROID_CODE_PUSH_DEPLOYMENT_KEY?: string
+    GOOGLE_MOBILE_ADS_ANDROID_APP_ID?: string
+    GOOGLE_MOBILE_ADS_IOS_APP_ID?: string
   }
 
   export const Config: NativeConfig
