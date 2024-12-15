a4a3cfc213a90dfedb1900230000b1ed094ac3b5
yungblud <killi8n@gmail.com>
2024-12-15 22:35:41 +0900

    refactor(billets-app): :zap: useSuperCluster hook

---
+++ apps/billets-app/src/features/map/hooks/use-super-cluster.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+import Supercluster from 'supercluster'
+import { z } from 'zod'
+import { mapPointSchema } from '../map.types'
+import { getZoomLevel } from '../utils'
+import { MapRegionWithZoomLevel } from './use-map-region-with-zoom-level.types'
+
+export const useSuperCluster = ({
+  mapRegionWithZoomLevel,
+  points,
+}: {
+  mapRegionWithZoomLevel: MapRegionWithZoomLevel
+  points: z.infer<typeof mapPointSchema>[]
+}) => {
+  const [supercluster] = useState(
+    () =>
+      new Supercluster({
+        radius: 40,
+        maxZoom: 16,
+        minZoom: 1,
+      }),
+  )
+
+  const [clusters, setClusters] = useState<z.infer<typeof mapPointSchema>[]>([])
+
+  useEffect(() => {
+    const region = mapRegionWithZoomLevel
+    const zoomLevel = getZoomLevel(region.latitudeDelta)
+
+    const bbox = [
+      region.longitude - region.longitudeDelta,
+      region.latitude - region.latitudeDelta,
+      region.longitude + region.longitudeDelta,
+      region.latitude + region.latitudeDelta,
+    ] as [number, number, number, number]
+
+    supercluster.load(points)
+    const clusters = supercluster.getClusters(bbox, zoomLevel)
+    const clustersValidation = mapPointSchema.array().safeParse(clusters)
+
+    if (clustersValidation.success) {
+      setClusters(clustersValidation.data)
+    } else {
+      console.error(clustersValidation.error)
+    }
+  }, [mapRegionWithZoomLevel, points, supercluster])
+
+  return {
+    clusters,
+    supercluster,
+  }
+}
--- apps/billets-app/src/screens/concert-map-screen/concert-map-screen.tsx
+++ apps/billets-app/src/screens/concert-map-screen/concert-map-screen.tsx
@@ -1,23 +1,12 @@
-import { ConcertMapMarker, getZoomLevel, mapPointSchema, useMapPoints, useMapRegionWithZoomLevel } from '@/features'
+import { ConcertMapMarker, getZoomLevel, useMapPoints, useMapRegionWithZoomLevel, useSuperCluster } from '@/features'
 import { useUserCurrentLocationStore } from '@/lib/stores/userCurrentLocationStore'
 import { CommonScreenLayout } from '@/ui'
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useMemo } from 'react'
 import { StyleSheet } from 'react-native'
 import MapView, { Camera, type Region } from 'react-native-maps'
-import Supercluster from 'supercluster'
-import { z } from 'zod'
 import { useShallow } from 'zustand/shallow'
 
 export const ConcertMapScreen = () => {
-  const [supercluster] = useState(
-    () =>
-      new Supercluster({
-        radius: 40,
-        maxZoom: 16,
-        minZoom: 1,
-      }),
-  )
-
   const { lat, lng } = useUserCurrentLocationStore(
     useShallow((state) => ({
       lat: state.latitude,
@@ -33,7 +22,10 @@ export const ConcertMapScreen = () => {
     mapRegionWithZoomLevel,
   })
 
-  const [clusters, setClusters] = useState<z.infer<typeof mapPointSchema>[]>([])
+  const { clusters } = useSuperCluster({
+    mapRegionWithZoomLevel,
+    points,
+  })
 
   const onRegionChangeComplete = useCallback(
     (region: Region) => {
@@ -44,28 +36,6 @@ export const ConcertMapScreen = () => {
     },
     [setMapRegionWithZoomLevel],
   )
-
-  useEffect(() => {
-    const region = mapRegionWithZoomLevel
-    const zoomLevel = getZoomLevel(region.latitudeDelta)
-
-    const bbox = [
-      region.longitude - region.longitudeDelta,
-      region.latitude - region.latitudeDelta,
-      region.longitude + region.longitudeDelta,
-      region.latitude + region.latitudeDelta,
-    ] as [number, number, number, number]
-
-    supercluster.load(points)
-    const clusters = supercluster.getClusters(bbox, zoomLevel)
-    const clustersValidation = mapPointSchema.array().safeParse(clusters)
-
-    if (clustersValidation.success) {
-      setClusters(clustersValidation.data)
-    } else {
-      console.error(clustersValidation.error)
-    }
-  }, [mapRegionWithZoomLevel, points, supercluster])
 
   const initialCamera = useMemo<Camera>(() => {
     return {
