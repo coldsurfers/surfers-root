f19cca8b94177285bc1950ea3941a70061f01755
yungblud <killi8n@gmail.com>
2024-12-16 13:27:52 +0900

    feat(billets-app): :art: styles for top shadow box

--- apps/billets-app/src/lib/common-styles.ts
+++ apps/billets-app/src/lib/common-styles.ts
@@ -12,6 +12,16 @@ const commonStyles = StyleSheet.create({
     // Android Shadow Property
     elevation: 5, // Elevation for Android
   },
+  topShadowBox: {
+    // iOS Shadow Properties
+    shadowColor: palettes.black, // Shadow color
+    shadowOffset: { width: 0, height: -8 }, // Shadow position
+    shadowOpacity: 0.25, // Opacity of the shadow
+    shadowRadius: 3.84, // How blurry the shadow is
+
+    // Android Shadow Property
+    elevation: 5, // Elevation for Android
+  },
 })
 
 export default commonStyles
