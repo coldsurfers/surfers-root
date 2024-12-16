8e1abbc080f41d7f6f9252405073b0ebffcaebfa
yungblud <killi8n@gmail.com>
2024-12-16 20:41:39 +0900

    feat(billets-app): :art: added memo

--- apps/billets-app/src/features/map/ui/concert-map-view/concert-map-view.tsx
+++ apps/billets-app/src/features/map/ui/concert-map-view/concert-map-view.tsx
@@ -1,5 +1,5 @@
 import { useUserCurrentLocationStore } from '@/lib/stores/userCurrentLocationStore'
-import { useCallback, useEffect, useMemo } from 'react'
+import { memo, useCallback, useEffect, useMemo } from 'react'
 import { StyleSheet } from 'react-native'
 import MapView, { Camera, Region } from 'react-native-maps'
 import { z } from 'zod'
@@ -9,93 +9,89 @@ import { mapPointSchema } from '../../map.types'
 import { getZoomLevel } from '../../utils'
 import { ConcertMapMarker } from '../concert-map-marker'
 
-export const ConcertMapView = ({
-  onChangeClusters,
-  onChangePoints,
-  onChangeVisiblePoints,
-  onChangeLocationConcerts,
-}: {
-  onChangeClusters?: (clusters: z.infer<typeof mapPointSchema>[]) => void
-  onChangePoints?: (points: z.infer<typeof mapPointSchema>[]) => void
-  onChangeVisiblePoints?: (visiblePoints: z.infer<typeof mapPointSchema>[]) => void
-  onChangeLocationConcerts?: (
-    locationConcerts: {
-      id: string
-      latitude: number
-      longitude: number
-      title: string
-    }[],
-  ) => void
-}) => {
-  const { lat, lng } = useUserCurrentLocationStore(
-    useShallow((state) => ({
-      lat: state.latitude,
-      lng: state.longitude,
-    })),
-  )
-  const { mapRegionWithZoomLevel, setMapRegionWithZoomLevel } = useMapRegionWithZoomLevel({
-    latitude: lat ?? 37.78825,
-    longitude: lng ?? -122.4324,
-  })
-
-  const { points, visiblePoints, locationConcerts } = useMapPoints({
-    mapRegionWithZoomLevel,
-  })
-
-  const { clusters } = useSuperCluster({
-    mapRegionWithZoomLevel,
-    points,
-  })
-
-  const onRegionChangeComplete = useCallback(
-    (region: Region) => {
-      setMapRegionWithZoomLevel({
-        ...region,
-        zoomLevel: getZoomLevel(region.latitudeDelta),
-      })
-    },
-    [setMapRegionWithZoomLevel],
-  )
-
-  const initialCamera = useMemo<Camera>(() => {
-    return {
-      center: {
-        ...mapRegionWithZoomLevel,
-      },
-      pitch: 180,
-      heading: 0,
-      zoom: mapRegionWithZoomLevel.zoomLevel,
-    }
-  }, [mapRegionWithZoomLevel])
-
-  useEffect(() => {
-    onChangeClusters?.(clusters)
-    onChangePoints?.(points)
-    onChangeVisiblePoints?.(visiblePoints)
-    onChangeLocationConcerts?.(locationConcerts ?? [])
-  }, [
-    clusters,
-    locationConcerts,
+export const ConcertMapView = memo(
+  ({
     onChangeClusters,
-    onChangeLocationConcerts,
     onChangePoints,
     onChangeVisiblePoints,
-    points,
-    visiblePoints,
-  ])
+    onChangeLocationConcerts,
+  }: {
+    onChangeClusters?: (clusters: z.infer<typeof mapPointSchema>[]) => void
+    onChangePoints?: (points: z.infer<typeof mapPointSchema>[]) => void
+    onChangeVisiblePoints?: (visiblePoints: z.infer<typeof mapPointSchema>[]) => void
+    onChangeLocationConcerts?: (
+      locationConcerts: {
+        id: string
+        latitude: number
+        longitude: number
+        title: string
+      }[],
+    ) => void
+  }) => {
+    const { lat, lng } = useUserCurrentLocationStore(
+      useShallow((state) => ({
+        lat: state.latitude,
+        lng: state.longitude,
+      })),
+    )
+    const { mapRegionWithZoomLevel, setMapRegionWithZoomLevel } = useMapRegionWithZoomLevel({
+      latitude: lat ?? 37.78825,
+      longitude: lng ?? -122.4324,
+    })
 
-  return (
-    <MapView
-      onRegionChangeComplete={onRegionChangeComplete}
-      initialRegion={mapRegionWithZoomLevel}
-      initialCamera={initialCamera}
-      zoomEnabled
-      style={styles.map}
-    >
-      <ConcertMapMarker clusters={clusters} />
-    </MapView>
-  )
-}
+    const { points, visiblePoints, locationConcerts } = useMapPoints({
+      mapRegionWithZoomLevel,
+    })
+
+    const { clusters } = useSuperCluster({
+      mapRegionWithZoomLevel,
+      points,
+    })
+
+    const onRegionChangeComplete = useCallback(
+      (region: Region) => {
+        setMapRegionWithZoomLevel({
+          ...region,
+          zoomLevel: getZoomLevel(region.latitudeDelta),
+        })
+      },
+      [setMapRegionWithZoomLevel],
+    )
+
+    const initialCamera = useMemo<Camera>(() => {
+      return {
+        center: {
+          ...mapRegionWithZoomLevel,
+        },
+        pitch: 180,
+        heading: 0,
+        zoom: mapRegionWithZoomLevel.zoomLevel,
+      }
+    }, [mapRegionWithZoomLevel])
+
+    useEffect(() => {
+      onChangeClusters?.(clusters)
+      onChangePoints?.(points)
+      onChangeVisiblePoints?.(visiblePoints)
+    }, [clusters, onChangeClusters, onChangePoints, onChangeVisiblePoints, points, visiblePoints])
+
+    useEffect(() => {
+      onChangeLocationConcerts?.(locationConcerts ?? [])
+    }, [locationConcerts, onChangeLocationConcerts])
+
+    return (
+      <MapView
+        onRegionChangeComplete={onRegionChangeComplete}
+        initialRegion={mapRegionWithZoomLevel}
+        initialCamera={initialCamera}
+        zoomEnabled
+        style={styles.map}
+      >
+        <ConcertMapMarker clusters={clusters} />
+      </MapView>
+    )
+  },
+)
 
 const styles = StyleSheet.create({
   map: {
