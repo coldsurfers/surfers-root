e6494aee9cbe4dae4ac91d20d4749c3abaedbcb9
yungblud <killi8n@gmail.com>
2024-12-16 18:07:53 +0900

    feat(billets-app): :zap: new state search store

--- apps/billets-app/src/features/search/store/search-store.ts
+++ apps/billets-app/src/features/search/store/search-store.ts
@@ -1,9 +1,14 @@
 import { create } from 'zustand'
+import { FULLY_EXPANDED_SNAP_INDEX } from './search-store.constants'
 import { SearchStore } from './search-store.types'
 
 export const useSearchStore = create<SearchStore>((set) => ({
   keyword: '',
   selectedLocationFilter: null,
+  snapIndex: FULLY_EXPANDED_SNAP_INDEX,
+  viewMode: 'list',
+  setSnapIndex: (index) => set({ snapIndex: index }),
+  setViewMode: (mode) => set({ viewMode: mode }),
   setKeyword: (keyword) => set({ keyword }),
   setSelectedLocationFilter: (filter) => set({ selectedLocationFilter: filter }),
 }))
--- apps/billets-app/src/features/search/store/search-store.types.ts
+++ apps/billets-app/src/features/search/store/search-store.types.ts
@@ -1,12 +1,18 @@
 export type SearchStoreLocationFilterType = 'current-location' | 'map-location'
+export type SearchStoreViewMode = 'map' | 'list'
+export type SearchStoreSnapIndex = 0 | 1
 
 export interface SearchStoreState {
   keyword: string
   selectedLocationFilter: SearchStoreLocationFilterType | null
+  snapIndex: SearchStoreSnapIndex
+  viewMode: SearchStoreViewMode
 }
 
 export interface SearchStoreActions {
   setKeyword: (keyword: string) => void
   setSelectedLocationFilter: (filter: SearchStoreLocationFilterType | null) => void
+  setSnapIndex: (index: SearchStoreSnapIndex) => void
+  setViewMode: (mode: SearchStoreViewMode) => void
 }
 export type SearchStore = SearchStoreState & SearchStoreActions
