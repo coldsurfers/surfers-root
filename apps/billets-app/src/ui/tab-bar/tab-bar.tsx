4415493f3704736634ef472744b6de591dc048b3
yungblud <killi8n@gmail.com>
2024-12-18 10:42:39 +0900

    feat(billets-app): :art: modified designs

--- apps/billets-app/src/ui/tab-bar/tab-bar.tsx
+++ apps/billets-app/src/ui/tab-bar/tab-bar.tsx
@@ -107,7 +107,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     paddingTop: 10,
-    backgroundColor: '#6F8EA3',
+    backgroundColor: colors.oc.cyan[8].value,
   },
   tabBarButton: {
     flex: 1,
