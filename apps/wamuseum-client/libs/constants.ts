97a33cc377bfbef46b89af6f39e2466e88dd1483
yungblud <killi8n@gmail.com>
2024-12-19 17:29:47 +0900

    feat(wamuseum-client): :sparkles: modified api url

--- apps/wamuseum-client/libs/constants.ts
+++ apps/wamuseum-client/libs/constants.ts
@@ -1,6 +1,6 @@
 const IS_DEV = process.env.NODE_ENV === 'development'
 
-const PROD_API_HOST = 'https://api.wa-museum.coldsurf.io'
+const PROD_API_HOST = 'https://api.wm.coldsurf.io'
 
 export const urls = {
   apolloServer: IS_DEV ? 'http://localhost:3002/api/graphql' : `${PROD_API_HOST}/api/graphql`,
