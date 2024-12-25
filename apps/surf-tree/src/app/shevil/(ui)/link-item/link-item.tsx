faf9fd5f000aea32a3cbe2eabd2289119c63e443
yungblud <killi8n@gmail.com>
2024-12-25 19:04:49 +0900

    feat(surf-tree): :art: implemented share button

--- apps/surf-tree/src/app/shevil/(ui)/link-item/link-item.tsx
+++ apps/surf-tree/src/app/shevil/(ui)/link-item/link-item.tsx
@@ -1,12 +1,24 @@
 'use client'
 
-import { StyledLinkItem, StyledLinkItemText } from './link-item.styled'
+import { EllipsisVertical } from 'lucide-react'
+import Link from 'next/link'
+import { StyledLinkItemContainer, StyledLinkItemText, StyledShareButton } from './link-item.styled'
 import { LinkItemProps } from './link-item.types'
 
 export function LinkItem({ href, title }: LinkItemProps) {
   return (
-    <StyledLinkItem href={href} target="_blank" rel="noopener noreferrer">
-      <StyledLinkItemText as="span">{title}</StyledLinkItemText>
-    </StyledLinkItem>
+    <StyledLinkItemContainer>
+      <Link
+        style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <StyledLinkItemText as="span">{title}</StyledLinkItemText>
+      </Link>
+      <StyledShareButton onClick={(e) => e.stopPropagation()}>
+        <EllipsisVertical width={16} height={16} />
+      </StyledShareButton>
+    </StyledLinkItemContainer>
   )
 }
