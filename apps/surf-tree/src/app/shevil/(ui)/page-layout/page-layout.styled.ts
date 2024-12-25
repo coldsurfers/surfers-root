6ec201a8c6d6dca7ea3e774f7622196aafcd3f60
yungblud <killi8n@gmail.com>
2024-12-25 15:13:06 +0900

    feat(surf-tree): :art: modified top acrd

--- apps/surf-tree/src/app/shevil/(ui)/page-layout/page-layout.styled.ts
+++ apps/surf-tree/src/app/shevil/(ui)/page-layout/page-layout.styled.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled'
 
 export const StyledPageLayout = styled.div`
-  margin: 0px auto;
+  margin: 2rem auto;
   height: 100%;
   width: 100%;
   max-width: 580px;
--- apps/surf-tree/src/app/shevil/(ui)/top-card/top-card.styled.ts
+++ apps/surf-tree/src/app/shevil/(ui)/top-card/top-card.styled.ts
@@ -1,7 +1,13 @@
 import { colors } from '@coldsurfers/ocean-road'
 import styled from '@emotion/styled'
 
-export const StyledTopCard = styled.div`
+export const StyledTopCard = styled.div<{ $backgroundImageUrl: string }>`
   border-radius: 8px;
   background: ${colors.oc.violet[2].value};
+  width: 580px;
+  height: 28rem;
+  background-image: url(${(props) => props.$backgroundImageUrl});
+  background-position: 50%;
+  background-size: cover;
+  mask: radial-gradient(100% 100% at center top, #000 60%, transparent 100%);
 `
