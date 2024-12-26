c3f62b72c500f2a1dd2c5433c431236c46b1ad22
yungblud <killi8n@gmail.com>
2024-12-26 21:29:13 +0900

    Revert "Revert "feat(blog-client): :art: modified styles header""
    This reverts commit a3dad4e82d5b4bc01e93124eee15a15c429c2d56.

--- apps/blog-client/ui/page-layout/page-layout.styled.ts
+++ apps/blog-client/ui/page-layout/page-layout.styled.ts
@@ -1,9 +1,19 @@
 'use client'
 
+import { media } from '@coldsurfers/ocean-road'
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 
 export const StyledPageLayoutContainer = styled.main`
   padding: 0 20px;
-  max-width: 700px;
+  max-width: 1728px;
   margin: 0 auto;
+
+  ${media.large(css`
+    max-width: 1536px;
+  `)}
+
+  ${media.medium(css`
+    width: 100%;
+  `)}
 `
--- apps/blog-client/ui/page-layout/page-layout.tsx
+++ apps/blog-client/ui/page-layout/page-layout.tsx
@@ -1,14 +1,31 @@
 'use client'
 
-import { Header } from '@/ui'
+import { ColorSchemeToggle } from '@/features/color-scheme'
+import { Text } from '@coldsurfers/ocean-road'
 import { usePathname } from 'i18n/routing'
 import { PropsWithChildren } from 'react'
+import { Header } from '../header/header'
 import { StyledPageLayoutContainer } from './page-layout.styled'
 
 export const PageLayout = ({ children }: PropsWithChildren) => {
   const pathname = usePathname()
   return (
     <StyledPageLayoutContainer>
+      <div style={{ marginTop: '2rem' }}>
+        <ColorSchemeToggle />
+      </div>
+      <Text
+        as="h1"
+        style={{
+          fontSize: '88px',
+          fontWeight: 820,
+          lineHeight: '1.05',
+          textAlign: 'center',
+          marginTop: '10rem',
+        }}
+      >
+        The COLDSURF Blog
+      </Text>
       {process.env.NODE_ENV === 'development' && pathname === '/resume' ? null : <Header />}
       {children}
     </StyledPageLayoutContainer>
