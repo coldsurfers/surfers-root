b1293a0de80aa407ad2db9f362557867fda7db26
yungblud <killi8n@gmail.com>
2024-12-26 15:25:27 +0900

    feat(surf-tree): :art: modified uis

--- apps/surf-tree/src/app/shevil/(ui)/powered-by/powered-by.tsx
+++ apps/surf-tree/src/app/shevil/(ui)/powered-by/powered-by.tsx
@@ -6,6 +6,7 @@ import { StyledPoweredBy } from './powered-by.styled'
 export const PoweredBy = () => {
   return (
     <StyledPoweredBy href="/">
+      <img src="/icons/favicon.ico" style={{ width: 25, height: 25, borderRadius: '50%', marginRight: '0.5rem' }} />
       <Text as="p" style={{ margin: 'unset' }}>
         Powered by SurfTree
       </Text>
