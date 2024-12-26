330041e0e9c3aac57ddf7f2b719130c3c9117bb0
yungblud <killi8n@gmail.com>
2024-12-26 16:03:09 +0900

    feat(surf-tree): :art: fixed layout issue server component

--- apps/surf-tree/src/app/shevil/page.tsx
+++ apps/surf-tree/src/app/shevil/page.tsx
@@ -1,52 +1,25 @@
-'use client'
-
-import { useState } from 'react'
+import { Metadata } from 'next/types'
+import { LinkItems, ShareModal } from './(components)'
 import { shevilData } from './(data)'
-import { Link } from './(data)/data.types'
-import { useToggle } from './(hooks)'
-import { HeroInfo, HighlightedLink, LinkItem, LinkItemsLayout, PageLayout, ShareModal, TopCard } from './(ui)'
-import { parseYtId } from './(utils)'
+import { HeroInfo, PageLayout, TopCard } from './(ui)'
+
+export const metadata: Metadata = {
+  title: 'Shevil | SurfTree',
+  description: shevilData.subtitle,
+  icons: {
+    icon: '/icons/favicon.ico',
+  },
+}
 
 export default function ShevilPage() {
-  const [shareModalVisible, toggleShareModalVisible] = useToggle()
-  const [sharedLink, setSharedLink] = useState<Link | null>(null)
-
   return (
     <>
       <PageLayout>
         <TopCard backgroundImageUrl={shevilData.profileImageUrl} />
         <HeroInfo title={shevilData.title} subtitle={shevilData.subtitle} />
-        <LinkItemsLayout>
-          {shevilData.links.map((link) =>
-            link.isHighlighted ? (
-              <HighlightedLink
-                key={link.title}
-                type="youtube"
-                youtubeId={parseYtId(link.url) ?? ''}
-                title={link.title}
-              />
-            ) : (
-              <LinkItem
-                key={link.title}
-                href={link.url}
-                title={link.title}
-                onClickShare={() => {
-                  toggleShareModalVisible()
-                  setSharedLink(link)
-                }}
-              />
-            ),
-          )}
-        </LinkItemsLayout>
+        <LinkItems />
       </PageLayout>
-      <ShareModal
-        visible={shareModalVisible}
-        sharedLink={sharedLink}
-        onClose={() => {
-          toggleShareModalVisible()
-          setSharedLink(null)
-        }}
-      />
+      <ShareModal />
     </>
   )
 }
