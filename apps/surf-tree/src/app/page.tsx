4fa6151734da0cde90000ea92b06a5d9409c744c
yungblud <killi8n@gmail.com>
2024-12-26 16:03:09 +0900

    feat(surf-tree): :art: fixed layout issue server component

--- apps/surf-tree/src/app/page.tsx
+++ apps/surf-tree/src/app/page.tsx
@@ -1,3 +1,19 @@
+'use client'
+
+import { useLayoutEffect } from 'react'
+
+const TIME_DURATION = 5000
+
+const TITLE = 'Welcome to SurfTree'
+const SUBTITLE = 'Moving to COLDSURF main page in 5 seconds'
+
 export default function Home() {
-  return null
+  useLayoutEffect(() => {}, [])
+
+  return (
+    <>
+      <h1>{TITLE}</h1>
+      <h2>{SUBTITLE}</h2>
+    </>
+  )
 }
