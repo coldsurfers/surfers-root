da7d308949a8d21f44dc878252deebb3dad54914
yungblud <killi8n@gmail.com>
2024-12-26 12:10:33 +0900

    feat(surf-tree): :art: implemented parse OG tag api route

--- apps/surf-tree/src/app/shevil/(utils)/utils.parse-og.ts
+++ apps/surf-tree/src/app/shevil/(utils)/utils.parse-og.ts
@@ -1,8 +1,10 @@
+import * as cheerio from 'cheerio'
+
 export type OGInfo = {
-  readonly title: string | null
-  readonly description: string | null
-  readonly image: string | null
-  readonly url: string | null
+  readonly title: string | undefined
+  readonly description: string | undefined
+  readonly image: string | undefined
+  readonly url: string | undefined
 }
 
 export async function parseOG(siteUrl: string): Promise<OGInfo | null> {
@@ -10,21 +12,13 @@ export async function parseOG(siteUrl: string): Promise<OGInfo | null> {
     const response = await fetch(siteUrl)
     const html = await response.text() // Get the HTML content as text
 
-    // Parse HTML and extract OG meta tags (client-side)
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(html, 'text/html')
-
-    const getMetaTagContent = (property: 'title' | 'description' | 'image' | 'url') => {
-      const metaTag = doc.querySelector(`meta[property="og:${property}"]`)
-      return metaTag ? metaTag.getAttribute('content') : null
-    }
+    const $ = cheerio.load(html)
 
     const ogInfo = {
-      title: getMetaTagContent('title'),
-      description: getMetaTagContent('description'),
-      image: getMetaTagContent('image'),
-      url: getMetaTagContent('url'),
-      // You can add more properties if needed
+      title: $('meta[property="og:title"]').attr('content'),
+      description: $('meta[property="og:description"]').attr('content'),
+      image: $('meta[property="og:image"]').attr('content'),
+      url: $('meta[property="og:url"]').attr('content'),
     }
     return ogInfo
   } catch (error) {
