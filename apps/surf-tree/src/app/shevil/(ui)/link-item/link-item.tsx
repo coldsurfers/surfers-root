fd070325341ac74aa6293b6ffbb67a26b4b5f73e
yungblud <killi8n@gmail.com>
2024-12-26 20:01:02 +0900

    feat(surf-tree): :art: enhanced motion

--- apps/surf-tree/src/app/shevil/(ui)/link-item/link-item.tsx
+++ apps/surf-tree/src/app/shevil/(ui)/link-item/link-item.tsx
@@ -7,7 +7,24 @@ import { LinkItemProps } from './link-item.types'
 
 export function LinkItem({ href, title, onClickShare }: LinkItemProps) {
   return (
-    <StyledLinkItemContainer>
+    <StyledLinkItemContainer
+      whileHover={{
+        transform: 'translate(4px, 4px)',
+        boxShadow: semantics.color.border[2] + ' 4px 4px 0px 0px',
+        transition: {
+          duration: 0.05,
+          ease: 'easeInOut',
+        },
+      }}
+      whileTap={{
+        transform: 'translate(4px, 4px)',
+        boxShadow: semantics.color.border[2] + ' 4px 4px 0px 0px',
+        transition: {
+          duration: 0.05,
+          ease: 'easeInOut',
+        },
+      }}
+    >
       <Link
         style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
         href={href}
