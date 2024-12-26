bea65bf3625cd4fba9471e05024ff14c7446aee3
yungblud <killi8n@gmail.com>
2024-12-26 21:16:45 +0900

    feat(blog-client): :art: modified styles header

--- apps/blog-client/app/[locale]/page.client.tsx
+++ apps/blog-client/app/[locale]/page.client.tsx
@@ -2,19 +2,10 @@
 
 import { useGetLogsQuery } from '@/lib/react-query'
 import { Paragraph, PostItem } from '@/ui'
-import { colors, media } from '@coldsurfers/ocean-road'
+import { media } from '@coldsurfers/ocean-road'
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import { AppLocale } from 'i18n/types'
-import { useTranslations } from 'next-intl'
-import { Roboto_Mono } from 'next/font/google'
-
-const robotoMono = Roboto_Mono({ subsets: ['latin'] })
-
-const Header = styled.header`
-  display: flex;
-  flex-direction: column;
-`
 
 const Posts = styled.ol`
   list-style: none;
@@ -30,20 +21,6 @@ const Title = styled(Paragraph)`
   `)}
 `
 
-const About = styled(Paragraph)<{ className: string }>`
-  font-size: 16px;
-  font-weight: 500;
-
-  background-color: #324746;
-  border-radius: 12px;
-  padding: 1.5rem;
-  color: ${colors.oc.cyan[5].value};
-
-  ${media.medium(css`
-    font-size: 14px;
-  `)}
-`
-
 export default function Page({ locale }: { locale: AppLocale }) {
   const { data: techlogs } = useGetLogsQuery({
     platform: 'techlog',
@@ -55,14 +32,9 @@ export default function Page({ locale }: { locale: AppLocale }) {
   })
   const latestTechlogs = techlogs?.slice(0, 5)
   const latestSurflogs = surflogs?.slice(0, 5)
-  const t = useTranslations()
 
   return (
-    <div>
-      <Header>
-        <About className={robotoMono.className}>{t('MainPage.about')}</About>
-      </Header>
-
+    <div style={{ marginTop: '6.5rem' }}>
       <div>
         <Title as="h2">Latest Surflogs</Title>
         <Posts>{latestSurflogs?.map((post) => <PostItem key={post.id} {...post} />)}</Posts>
