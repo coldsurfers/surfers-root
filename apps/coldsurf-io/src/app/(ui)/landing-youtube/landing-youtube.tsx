22de9a3b855b6f08e7ecc2f8d6666294ed73c883
yungblud <killi8n@gmail.com>
2024-12-28 14:25:48 +0900

    chore(coldsurf-io): :art: comment out

--- apps/coldsurf-io/src/app/(ui)/landing-youtube/landing-youtube.tsx
+++ apps/coldsurf-io/src/app/(ui)/landing-youtube/landing-youtube.tsx
@@ -4,7 +4,7 @@ import LiteYoutubeEmbed from 'react-lite-youtube-embed'
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 import { StyledLandingYoutubeContainer } from './landing-youtube.styled'
 
-const URL = 'https://www.youtube.com/watch?v=h_DjmtR0Xls'
+// const URL = 'https://www.youtube.com/watch?v=h_DjmtR0Xls'
 const ID = 'h_DjmtR0Xls'
 
 export function LandingYoutube() {
