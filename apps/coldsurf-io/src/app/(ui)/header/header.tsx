d98954cadd5c0f127005b63ac79f74cb6d3411db
yungblud <killi8n@gmail.com>
2024-12-28 10:36:25 +0900

    feat(coldsurf-io): :art: logo and title header

--- apps/coldsurf-io/src/app/(ui)/header/header.tsx
+++ apps/coldsurf-io/src/app/(ui)/header/header.tsx
@@ -1,15 +1,27 @@
 'use client'
 
 import { Text } from '@coldsurfers/ocean-road'
+import Image from 'next/image'
+import Link from 'next/link'
 import { StyledHeader, StyledHeaderWrapper } from './header.styled'
 
 export function Header() {
   return (
     <StyledHeaderWrapper>
       <StyledHeader>
-        <div style={{ paddingLeft: '32px' }}>
+        <Link href="/" style={{ paddingLeft: '32px', display: 'flex', alignItems: 'center' }}>
+          <Image
+            src={'/icons/favicon.ico'}
+            width={32}
+            height={32}
+            alt={'LOGO'}
+            style={{
+              borderRadius: '50%',
+              marginRight: '0.5rem',
+            }}
+          />
           <Text as="h2">COLDSURF</Text>
-        </div>
+        </Link>
       </StyledHeader>
     </StyledHeaderWrapper>
   )
