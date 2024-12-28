ca7fb0ad7b93ac6d714eacd43bb516a70ffdcdfc
yungblud <killi8n@gmail.com>
2024-12-28 11:40:13 +0900

    feat(coldsurf-io): :art: implemented window resize effect

--- apps/coldsurf-io/src/app/(ui)/page-layout/page-layout.tsx
+++ apps/coldsurf-io/src/app/(ui)/page-layout/page-layout.tsx
@@ -1,11 +1,27 @@
 'use client'
 
-import { PropsWithChildren, useState } from 'react'
+import { breakpoints } from '@coldsurfers/ocean-road'
+import { PropsWithChildren, useEffect, useState } from 'react'
 import { Header } from '../header'
 import { MobileMenu } from '../mobile-menu'
 
 export function PageLayout({ children }: PropsWithChildren) {
   const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > breakpoints.large) {
+        setIsOpen(false)
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <>
       <Header
