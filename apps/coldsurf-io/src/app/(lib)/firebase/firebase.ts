94ece9a6087e6d6d3976189015e95ad799d4cf57
yungblud <killi8n@gmail.com>
2024-12-29 18:04:39 +0900

    feat(coldsurf-io): :art: implemented firebase

--- apps/coldsurf-io/src/app/(lib)/firebase/firebase.ts
+++ apps/coldsurf-io/src/app/(lib)/firebase/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { Analytics, getAnalytics } from 'firebase/analytics'
-import { FirebaseApp, initializeApp } from 'firebase/app'
+import { FirebaseApp, initializeApp as fbInitializeApp } from 'firebase/app'
 import { firebaseConfig } from './firebase.constants'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,12 +14,12 @@ export class Firebase {
 
   constructor() {
     // Initialize Firebase
-    this.app = initializeApp(firebaseConfig)
+    this.app = fbInitializeApp(firebaseConfig)
     this.analytics = getAnalytics(this.app)
   }
 
   static initializeApp() {
-    const app = initializeApp(firebaseConfig)
+    const app = fbInitializeApp(firebaseConfig)
     return getAnalytics(app)
   }
 }
