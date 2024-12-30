f329a91be75653c9d2ccd223ea18f95ed2a98112
yungblud <killi8n@gmail.com>
2024-12-30 12:21:13 +0900

    feat(coldsurf-io): :art: product bottom sheet content with icon button

--- apps/coldsurf-io/src/app/products/(components)/product-card/product-card.tsx
+++ apps/coldsurf-io/src/app/products/(components)/product-card/product-card.tsx
@@ -5,20 +5,20 @@ import { useShallow } from 'zustand/shallow'
 import { ProductCard as Card } from '../../(ui)'
 import { ProductCardProps } from './product-card.types'
 
-export function ProductCard({ title, description, imgSrc, backgroundImgSrc }: ProductCardProps) {
-  const { setIsProductBottomSheetOpen } = useProductStore(
+export function ProductCard(props: ProductCardProps) {
+  const { setSelectedProduct, setIsProductBottomSheetOpen } = useProductStore(
     useShallow((state) => ({
-      isProductBottomSheetOpen: state.isProductBottomSheetOpen,
+      setSelectedProduct: state.setSelectedProduct,
       setIsProductBottomSheetOpen: state.setIsProductBottomSheetOpen,
     })),
   )
   return (
     <Card
-      onClick={() => setIsProductBottomSheetOpen(true)}
-      title={title}
-      description={description}
-      imgSrc={imgSrc}
-      backgroundImgSrc={backgroundImgSrc}
+      onClick={() => {
+        setIsProductBottomSheetOpen(true)
+        setSelectedProduct(props)
+      }}
+      {...props}
     />
   )
 }
