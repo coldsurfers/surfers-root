78e926d17387ab6657c5cced2be3b7735772b5a1
yungblud <killi8n@gmail.com>
2025-01-04 20:03:57 +0900

    refactor(billets-web): :dizzy: removed not using files

--- apps/billets-web/components/FeedCategory/FeedCategoryBadge.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-export function FeedCategoryBadge({ children }: PropsWithChildren) {
-  return (
-    <span className="inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10 cursor-pointer mr-4">
-      {children}
-    </span>
-  )
-}
