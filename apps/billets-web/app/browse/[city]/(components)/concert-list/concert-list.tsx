93b8ec2c23541cdc2d8bb91f6ca6e8e92dc29f18
yungblud <killi8n@gmail.com>
2025-01-05 14:00:02 +0900

    feat(billets-web): :memo: implemented remote city data fetch

--- apps/billets-web/app/browse/[city]/(components)/concert-list/concert-list.tsx
+++ apps/billets-web/app/browse/[city]/(components)/concert-list/concert-list.tsx
@@ -1,7 +1,33 @@
 'use client'
 
-type ConcertListProps = {}
+import { apiClient } from '@/libs/openapi-client'
+import { useQuery } from '@tanstack/react-query'
 
-export function ConcertList() {
-  return <div>ConcertList</div>
+type ConcertListProps = {
+  cityData: {
+    readonly id: string
+    readonly lat: number
+    readonly lng: number
+    readonly name: string
+    readonly uiName: string
+  }
+}
+
+export function ConcertList({ cityData }: ConcertListProps) {
+  const { data, isLoading } = useQuery({
+    queryKey: apiClient.concerts.queryKeys.getConcerts({
+      offset: 0,
+      size: 20,
+      latitude: cityData.lat,
+      longitude: cityData.lng,
+    }),
+    queryFn: () =>
+      apiClient.concerts.getConcerts({
+        offset: 0,
+        size: 20,
+        latitude: cityData.lat,
+        longitude: cityData.lng,
+      }),
+  })
+  return <div>ConcertList, {JSON.stringify(data)}</div>
 }
