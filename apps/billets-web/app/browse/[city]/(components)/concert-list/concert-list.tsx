cfd8e30b1cf13ee0996af4ab6e66fddc71c9c33c
yungblud <killi8n@gmail.com>
2025-01-05 14:16:58 +0900

    feat(billets-web): :art: implemented ConcertList items

--- apps/billets-web/app/browse/[city]/(components)/concert-list/concert-list.tsx
+++ apps/billets-web/app/browse/[city]/(components)/concert-list/concert-list.tsx
@@ -2,6 +2,21 @@
 
 import { apiClient } from '@/libs/openapi-client'
 import { useQuery } from '@tanstack/react-query'
+import { format } from 'date-fns'
+import Link from 'next/link'
+import { memo } from 'react'
+import {
+  StyledGridContainer,
+  StyledGridDate,
+  StyledGridImage,
+  StyledGridItem,
+  StyledGridTextContainer,
+  StyledGridTitle,
+  StyledListContainer,
+  StyledListHeader,
+  StyledListHeaderText,
+  StyledVenueText,
+} from './concert-list.styled'
 
 type ConcertListProps = {
   cityData: {
@@ -13,8 +28,8 @@ type ConcertListProps = {
   }
 }
 
-export function ConcertList({ cityData }: ConcertListProps) {
-  const { data, isLoading } = useQuery({
+export const ConcertList = memo(({ cityData }: ConcertListProps) => {
+  const { data } = useQuery({
     queryKey: apiClient.concerts.queryKeys.getConcerts({
       offset: 0,
       size: 20,
@@ -29,5 +44,31 @@ export function ConcertList({ cityData }: ConcertListProps) {
         longitude: cityData.lng,
       }),
   })
-  return <div>ConcertList, {JSON.stringify(data)}</div>
-}
+
+  return (
+    <StyledListContainer>
+      <StyledListHeader>
+        <StyledListHeaderText as="h1">Popular Shows in {cityData.uiName}</StyledListHeaderText>
+      </StyledListHeader>
+      <StyledGridContainer>
+        {data?.map((item) => {
+          const poster = item.posters.at(0)
+          const posterUrl = poster?.imageUrl
+          const venue = item.venues.at(0)
+          return (
+            <Link key={item.id} href={`/concert-detail/${item.id}`}>
+              <StyledGridItem>
+                <StyledGridImage src={posterUrl} alt={item.title} />
+                <StyledGridTextContainer>
+                  <StyledGridTitle as="p">{item.title}</StyledGridTitle>
+                  <StyledGridDate as="p">{format(new Date(item.date), 'EEE, MMM dd')}</StyledGridDate>
+                  {venue?.venueTitle && <StyledVenueText as="p">{venue.venueTitle}</StyledVenueText>}
+                </StyledGridTextContainer>
+              </StyledGridItem>
+            </Link>
+          )
+        })}
+      </StyledGridContainer>
+    </StyledListContainer>
+  )
+})
