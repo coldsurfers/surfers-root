17d410aac26587f3226b085c2c9a74968c5b7c2c
yungblud <killi8n@gmail.com>
2025-01-05 14:19:50 +0900

    chore(billets-web): :art: modified styles app footer

--- apps/billets-web/app/(ui)/app-footer/app-footer.tsx
+++ apps/billets-web/app/(ui)/app-footer/app-footer.tsx
@@ -7,23 +7,28 @@ import Link from 'next/link'
 const Container = styled.div`
   width: 100%;
   height: 15rem;
-  padding: 1rem;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   gap: 1rem;
+
+  margin-top: 15rem;
 `
 
 export function AppFooter() {
   return (
     <Container>
-      <Text as="p" style={{ fontWeight: 'bold' }}>
+      <Text as="p" style={{ fontWeight: 'bold', margin: 'unset' }}>
         &copy; 2024 COLDSURF, Inc.
       </Text>
       <Link href="/privacy-policy">
-        <Text as="p">Privacy Policy</Text>
+        <Text as="p" style={{ margin: 'unset' }}>
+          Privacy Policy
+        </Text>
       </Link>
       <Link href="/terms-of-service">
-        <Text as="p">Terms of Service</Text>
+        <Text as="p" style={{ margin: 'unset' }}>
+          Terms of Service
+        </Text>
       </Link>
     </Container>
   )
