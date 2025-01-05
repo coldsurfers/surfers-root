f59455a7c471aa814a808e3e77fa3db5fca04979
yungblud <killi8n@gmail.com>
2025-01-05 13:46:43 +0900

    feat(billets-web): :sparkles: enhanced metadata handling

--- apps/billets-web/app/browse/[city]/page.tsx
+++ apps/billets-web/app/browse/[city]/page.tsx
@@ -1,20 +1,56 @@
 import { apiClient } from '@/libs/openapi-client'
+import { Metadata } from 'next'
 import { redirect } from 'next/navigation'
+import { cache } from 'react'
 
-export default async function BrowseByCityPage(props: {
-  params: { city: string }
+type PageProps<T extends { [key: string]: string }> = {
+  params: T
   searchParams: Record<string, never>
-}) {
-  const city = decodeURIComponent(props.params.city)
+}
+
+const validateCityParam = cache(async (cityParam: string) => {
+  const city = decodeURIComponent(cityParam)
   const countries = await apiClient.location.getCountries()
+  const cities = countries.flatMap((c) => c.cities)
 
-  const isValidCity = countries.some((country) => {
-    return country.cities.some((c) => {
-      return c.name === city
-    })
+  const isValidCity = cities.some((c) => {
+    return c.name === city
   })
-
   if (!isValidCity) {
+    return {
+      isValid: false,
+      data: null,
+    } as const
+  }
+  return {
+    isValid: true,
+    data: {
+      uiName: cities.find((c) => c.name === city)?.uiName,
+      name: city,
+    },
+  } as const
+})
+
+export async function generateMetadata({ params }: PageProps<{ city: string }>): Promise<Metadata> {
+  const validation = await validateCityParam(params.city)
+  if (!validation.isValid) {
+    return {
+      title: 'Billets | Discover shows around the world',
+      description:
+        'Billets is a platform to find live shows around the world. Download Billets to see live shows in your city.',
+    }
+  }
+  return {
+    title: `Popular shows in ${validation.data.uiName} | Billets`,
+    description:
+      'Billets is a platform to find live shows around the world. Download Billets to see live shows in your city.',
+  }
+}
+
+export default async function BrowseByCityPage(props: PageProps<{ city: string }>) {
+  const validation = await validateCityParam(props.params.city)
+
+  if (!validation.isValid) {
     return redirect('/404')
   }
 
