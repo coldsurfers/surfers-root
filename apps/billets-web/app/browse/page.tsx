563a20db2dc389852933159449c6766288d94535
yungblud <killi8n@gmail.com>
2025-01-05 15:41:56 +0900

    feat(billets-web): :art: implemented metadata for page

--- apps/billets-web/app/browse/page.tsx
+++ apps/billets-web/app/browse/page.tsx
@@ -1,4 +1,13 @@
+import { Metadata } from 'next'
 import { redirect } from 'next/navigation'
+
+export function generateMetadata(): Metadata {
+  return {
+    title: 'Billets | Discover shows around the world',
+    description:
+      'Billets is a platform to find live shows around the world. Download Billets to see live shows in your city.',
+  }
+}
 
 export default function BrowseCityGatewayPage() {
   // @todo: implement server api by detecting validate city
