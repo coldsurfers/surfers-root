45dc91d9122e3340f06ca038c3287d01fa7d4f23
yungblud <killi8n@gmail.com>
2025-01-06 10:12:02 +0900

    feat(billets-web): :sparkles: modified styles and added ticket cta

--- apps/billets-web/app/event/[event-id]/(ui)/page-layout/page-layout.styled.tsx
+++ apps/billets-web/app/event/[event-id]/(ui)/page-layout/page-layout.styled.tsx
@@ -1,3 +1,5 @@
+import { media } from '@coldsurfers/ocean-road'
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import { HEADER_HEIGHT } from 'app/(ui)'
 
@@ -6,19 +8,44 @@ export const StyledPageLayout = styled.div`
   height: 100vh; /* Tall parent container */
   display: flex;
   align-items: flex-start;
+
+  ${media.medium(css`
+    flex-direction: column;
+    align-items: center;
+    margin-left: 1rem;
+    margin-right: 1rem;
+  `)}
 `
 
 export const StyledPosterContainer = styled.div`
   position: sticky;
   top: calc(${HEADER_HEIGHT} + 32px);
+  left: 2rem;
   width: 320px;
   aspect-ratio: 1 / 1;
   flex: 1;
+
+  ${media.medium(css`
+    width: 100%;
+    position: static;
+  `)}
 `
 
-export const StyledTopInfoContainer = styled.div`
+export const StyledRightSideContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex: 2;
   margin-left: 48px;
+
+  ${media.large(css`
+    margin-left: 62px;
+  `)}
+
+  ${media.medium(css`
+    margin-left: 0px;
+  `)}
 `
+
+export const StyledTopInfoContainer = styled.div``
+
+export const StyledTicketCTAContainer = styled.div``
