64030366f3f1f89311251cba8356185f4510d9bf
yungblud <killi8n@gmail.com>
2025-01-06 14:04:34 +0900

    feat(billets-web): :art: enhanced sitemap xml and robots txt

--- apps/billets-web/libs/constants.ts
+++ apps/billets-web/libs/constants.ts
@@ -1,2 +1,5 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === 'development' ? 'https://dev.api.billets.coldsurf.io' : 'https://api.billets.coldsurf.io'
+
+export const SITE_URL = 'https://billets.coldsurf.io'
+export const SITE_MAP_URL = 'https://billets.coldsurf.io/sitemap.xml'
