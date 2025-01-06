9649c2afe79477a3e577626610adc16d257fbf13
yungblud <killi8n@gmail.com>
2025-01-06 15:15:38 +0900

    feat(billets-web): :art: implemented ldjson

--- apps/billets-web/app/about/page.tsx
+++ apps/billets-web/app/about/page.tsx
@@ -1,3 +1,25 @@
+import { generateBilletsLdJson, generateBilletsMetadata } from '@/libs/utils'
+import { Metadata } from 'next'
+
+export async function generateMetadata(): Promise<Metadata> {
+  return generateBilletsMetadata({
+    title: 'Story and Mission | About Billets and COLDSURF',
+  })
+}
+
 export default function AboutPage() {
-  return <div>About</div>
+  return (
+    <div>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(
+            generateBilletsLdJson({
+              type: 'WebSite',
+            }),
+          ),
+        }}
+      />
+    </div>
+  )
 }
