30a1115238382505ac2dbe80777bb5e33e948d0c
yungblud <killi8n@gmail.com>
2025-01-06 14:04:34 +0900

    feat(billets-web): :art: enhanced sitemap xml and robots txt

--- apps/billets-web/app/browse/[city]/page.tsx
+++ apps/billets-web/app/browse/[city]/page.tsx
@@ -1,41 +1,11 @@
 import { apiClient } from '@/libs/openapi-client'
 import { ApiErrorBoundaryRegistry } from '@/libs/registries'
-import { getQueryClient } from '@/libs/utils'
+import { getQueryClient, validateCityParam } from '@/libs/utils'
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 import { Metadata } from 'next'
 import { redirect } from 'next/navigation'
-import { cache } from 'react'
 import { PageProps } from 'types/common-types'
 import { ConcertList } from './(components)'
-
-const validateCityParam = cache(async (cityParam: string) => {
-  const city = decodeURIComponent(cityParam)
-  const countries = await apiClient.location.getCountries()
-  const cities = countries.flatMap((c) => c.cities)
-
-  const isValidCity = cities.some((c) => {
-    return c.name === city
-  })
-  if (!isValidCity) {
-    return {
-      isValid: false,
-      data: null,
-    } as const
-  }
-  const remoteCity = cities.find((c) => c.name === city)
-  if (!remoteCity) {
-    return {
-      isValid: false,
-      data: null,
-    } as const
-  }
-  return {
-    isValid: true,
-    data: {
-      ...remoteCity,
-    },
-  } as const
-})
 
 export const revalidate = 600
 
--- apps/billets-web/libs/utils/index.ts
+++ apps/billets-web/libs/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './utils.city'
 export * from './utils.metadata'
 export * from './utils.price'
 export * from './utils.query-client'
