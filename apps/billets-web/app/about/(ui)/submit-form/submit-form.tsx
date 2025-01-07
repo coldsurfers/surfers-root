327222c0108654cb1358e65aa68128d2e73529ae
yungblud <killi8n@gmail.com>
2025-01-07 13:19:33 +0900

    feat(billets-web): :art: updated forms

--- apps/billets-web/app/about/(ui)/submit-form/submit-form.tsx
+++ apps/billets-web/app/about/(ui)/submit-form/submit-form.tsx
@@ -1,47 +1,45 @@
 'use client'
 
-import { Button, Checkbox, Text, TextArea, TextInput } from '@coldsurfers/ocean-road'
+import { Checkbox, Text } from '@coldsurfers/ocean-road'
 import { memo } from 'react'
+import {
+  StyledFormLeft,
+  StyledFormOuterContainer,
+  StyledFormRight,
+  StyledSubmitButton,
+  StyledSubmitFormContainer,
+  StyledTextArea,
+  StyledTextInput,
+} from './submit-form.styled'
 
 export const SubmitForm = memo(() => {
   return (
-    <div style={{ marginTop: '8rem' }}>
+    <StyledFormOuterContainer>
       <Text as="h1">Let me know your opinion 🗯️</Text>
-      <div style={{ display: 'flex' }}>
-        <div style={{ flex: 0.4 }}>
+      <StyledSubmitFormContainer>
+        <StyledFormLeft>
           <Text as="h3">
             We want to hear all your voices about Billets. Anything is okay, even if you are user, venue owner,
             promoters, artists.
           </Text>
-        </div>
-        <form style={{ flex: 0.6, display: 'flex', flexDirection: 'column', marginLeft: '2.5rem' }}>
-          <label style={{ marginTop: '1.5rem', marginBottom: '0.5rem' }}>
-            <Text as="p" style={{ margin: 'unset' }}>
-              How can I call you?
-            </Text>
-          </label>
-          <TextInput placeholder="name" style={{ width: '100%' }}></TextInput>
-          <label style={{ marginTop: '1.5rem', marginBottom: '0.5rem' }}>
-            <Text as="p" style={{ margin: 'unset' }}>
-              Your email, so I can contact you back
-            </Text>
-          </label>
-          <TextInput placeholder="email" style={{ width: '100%' }}></TextInput>
-          <label style={{ marginTop: '1.5rem', marginBottom: '0.5rem' }}>
-            <Text as="p" style={{ margin: 'unset' }}>
-              Give us some more details
-            </Text>
-          </label>
-          <TextArea
+        </StyledFormLeft>
+        <StyledFormRight>
+          <StyledTextInput id="billets-voice-form-name" placeholder="Name" label="How can I call you?" />
+          <StyledTextInput
+            id="billets-voice-form-email"
+            placeholder="Email"
+            label="Your email, so I can contact you back"
+          />
+          <StyledTextArea
+            label="Give us some more details"
+            id="billets-voice-form-message"
             placeholder="Please describe your message to me. e.g. 'I am owner of a venue. And I want to work with your app'"
-            style={{ resize: 'none', height: '12rem' }}
+            noResize
           />
           <Checkbox labelText="I want to receive updates from Billets" style={{ marginTop: '1rem' }} />
-          <Button type="submit" style={{ marginTop: '2rem' }}>
-            Submit
-          </Button>
-        </form>
-      </div>
-    </div>
+          <StyledSubmitButton type="submit">Submit</StyledSubmitButton>
+        </StyledFormRight>
+      </StyledSubmitFormContainer>
+    </StyledFormOuterContainer>
   )
 })
