2ae3f687419f94ba10328c610f322100ed5c3848
yungblud <killi8n@gmail.com>
2025-01-07 18:35:17 +0900

    feat(billets-server): :art: implemented rate limit mailer user voice post route

--- apps/billets-server/src/routes/mailer/mailer.route.ts
+++ apps/billets-server/src/routes/mailer/mailer.route.ts
@@ -4,10 +4,27 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { sendUserVoiceHandler } from './mailer.handler'
 import { sendEmailResponseSchema, sendUserVoiceBodySchema } from './mailer.types'
 
+const timeWindow = '1 minute'
+
 export const mailerRoute: FastifyPluginCallback = (fastify, opts, done) => {
   fastify.withTypeProvider<ZodTypeProvider>().post(
     '/user-voice',
     {
+      config: {
+        rateLimit: {
+          max: 5,
+          timeWindow,
+          keyGenerator: (req) => req.ip,
+          ban: 2,
+          errorResponseBuilder(req, context) {
+            return {
+              statusCode: 429,
+              error: 'Too Many Requests',
+              message: `You have exceeded the request limit of ${context.max} per ${timeWindow}`,
+            }
+          },
+        },
+      },
       schema: {
         tags: ['v1', 'mailer'],
         body: sendUserVoiceBodySchema,
