0f48ff525049d8c0adc8de5623fa8adbe3e62a36
yungblud <killi8n@gmail.com>
2025-01-08 10:32:09 +0900

    feat(billets-web): :art: implemented temp global time zone

--- apps/billets-web/libs/constants.ts
+++ apps/billets-web/libs/constants.ts
@@ -6,3 +6,5 @@ export const SITE_MAP_URL = 'https://billets.coldsurf.io/sitemap.xml'
 
 export const APP_STORE_ID = '1632802589'
 export const APP_STORE_URL = `https://itunes.apple.com/app/id${APP_STORE_ID}`
+
+export const GLOBAL_TIME_ZONE = 'Asia/Seoul'
