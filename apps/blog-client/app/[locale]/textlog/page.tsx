539eda00afad4acf1571d17dcbba0ebccd993a2a
yungblud <killi8n@gmail.com>
2025-01-17 11:25:20 +0900

    feat(blog-client): :art: text log page meta

--- apps/blog-client/app/[locale]/textlog/page.tsx
+++ apps/blog-client/app/[locale]/textlog/page.tsx
@@ -1,15 +1,40 @@
+import { SITE_URL } from '@/lib/constants'
+import { metadataInstance } from '@/lib/metadata'
 import { queryKeyFactory } from '@/lib/react-query/react-query.key-factory'
 import { getQueryClient } from '@/lib/react-query/react-query.utils'
 import { LogListPage, PageLayout } from '@/ui'
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 import { routing } from 'i18n/routing'
 import { PageProps } from 'i18n/types'
+import { Metadata } from 'next'
 import { setRequestLocale } from 'next-intl/server'
 
 export const revalidate = 3600
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }))
+}
+
+export function generateMetadata({ params }: PageProps) {
+  const metaTitle = 'COLDSURF Blog: Article about Books & Texts'
+  const metaDescription = 'Article about Books & Texts'
+
+  const meta = metadataInstance.generateMetadata<Metadata>({
+    title: metaTitle,
+    description: metaDescription,
+    openGraph: {
+      title: metaTitle,
+      description: metaDescription,
+      type: 'website',
+      url: `${SITE_URL}/${params.locale}/textlog`,
+    },
+    alternates: {
+      canonical: `${SITE_URL}/${params.locale}/textlog`,
+      languages: Object.fromEntries(routing.locales.map((locale) => [locale, `${SITE_URL}/${locale}/textlog`])),
+    },
+  })
+
+  return meta
 }
 
 export default async function TextLogPage({ params, searchParams }: PageProps) {
