19afb1793687308a8241cac50185a96b73727612
yungblud <killi8n@gmail.com>
2025-01-19 21:05:54 +0900

    feat(billets-app): :art: implemented location permission modal contents

--- apps/billets-app/src/features/modal/global-modal/global-modal.tsx
+++ apps/billets-app/src/features/modal/global-modal/global-modal.tsx
@@ -4,14 +4,19 @@ import { useShallow } from 'zustand/shallow'
 import { useGlobalModalStore } from '../stores/global-modal-store'
 
 export const GlobalModal = memo(() => {
-  const { visible: globalModalVisible } = useGlobalModalStore(
+  const { visible: globalModalVisible, close } = useGlobalModalStore(
     useShallow((state) => ({
       visible: state.visible,
+      close: state.close,
     })),
   )
+  const { geolocationPermissionAlertModal } = globalModalVisible
   return (
     <>
-      <GeolocationPermissionAlertModal visible={globalModalVisible.geolocationPermissionAlertModal} />
+      <GeolocationPermissionAlertModal
+        visible={geolocationPermissionAlertModal}
+        onPressBackdrop={() => close('geolocationPermissionAlertModal')}
+      />
     </>
   )
 })
