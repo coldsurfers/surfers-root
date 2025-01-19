a6be290db51c20f4582bb12ea68e62b3534c9bb1
yungblud <killi8n@gmail.com>
2025-01-19 13:29:18 +0900

    feat(billets-app): :art: implemented darkmode on LocationSelector

--- apps/billets-app/src/features/location/ui/location-selector/location-selector.tsx
+++ apps/billets-app/src/features/location/ui/location-selector/location-selector.tsx
@@ -1,34 +1,41 @@
 import commonStyles from '@/lib/common-styles'
 import { colors } from '@coldsurfers/ocean-road'
+import { useColorScheme } from '@coldsurfers/ocean-road/native'
 import { MapPin } from 'lucide-react-native'
 import { StyleSheet, TouchableOpacity, View } from 'react-native'
 
-export const LocationSelector = ({ onPress }: { onPress: () => void }) => (
-  <View style={styles.wrapper}>
-    <TouchableOpacity
-      onPress={onPress}
-      hitSlop={{
-        top: 12,
-        left: 12,
-        right: 12,
-        bottom: 12,
-      }}
-      style={styles.button}
-    >
-      <MapPin size={24} />
-    </TouchableOpacity>
-  </View>
-)
+export const LocationSelector = ({ onPress }: { onPress: () => void }) => {
+  const { semantics } = useColorScheme()
+  return (
+    <View style={styles.wrapper}>
+      <TouchableOpacity
+        onPress={onPress}
+        hitSlop={{
+          top: 12,
+          left: 12,
+          right: 12,
+          bottom: 12,
+        }}
+        style={[
+          styles.button,
+          {
+            backgroundColor: semantics.background[4],
+          },
+        ]}
+      >
+        <MapPin size={24} color={semantics.foreground[1]} />
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   wrapper: { display: 'flex', flexDirection: 'row', paddingBottom: 12 },
   button: {
-    marginLeft: 'auto',
-    marginRight: 12,
+    padding: 8,
+    marginLeft: 12,
     backgroundColor: colors.oc.white.value,
-    width: 36,
-    height: 36,
-    borderRadius: 18,
+    borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
     ...commonStyles.shadowBox,
