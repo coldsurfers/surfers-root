c2afe1659b7c7ec865887f8ed29d760903f4e0f0
yungblud <killi8n@gmail.com>
2025-01-22 10:45:52 +0900

    feat(billets-web): :art: implemented get venue detail with validation

--- apps/billets-web/app/venue/[venue-id]/page.tsx
+++ apps/billets-web/app/venue/[venue-id]/page.tsx
@@ -1,8 +1,45 @@
+import { apiClient } from '@/libs/openapi-client'
 import { ApiErrorBoundaryRegistry } from '@/libs/registries'
+import { getQueryClient } from '@/libs/utils/utils.query-client'
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { redirect } from 'next/navigation'
+import { cache } from 'react'
 import { PageProps } from 'types/common-types'
 
-function PageInner({ params }: PageProps<{ ['venue-id']: string }>) {
-  return null
+const getVenueDetail = cache((venueId: string) => apiClient.venue.getVenueDetail(venueId))
+
+async function validateVenue(venueId: string) {
+  try {
+    const response = await getVenueDetail(venueId)
+    return {
+      isValid: true,
+      data: response,
+    }
+  } catch (e) {
+    return {
+      isValid: false,
+      data: null,
+    } as const
+  }
+}
+
+async function PageInner({ params }: PageProps<{ ['venue-id']: string }>) {
+  const venueId = params['venue-id']
+
+  const validation = await validateVenue(venueId)
+
+  if (!validation.isValid) {
+    return redirect('/404')
+  }
+
+  const queryClient = getQueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: apiClient.venue.queryKeys.detail(venueId),
+    queryFn: () => getVenueDetail(venueId),
+  })
+
+  return <HydrationBoundary state={dehydrate(queryClient)}></HydrationBoundary>
 }
 
 export default function VenueDetailPage(pageProps: PageProps<{ ['venue-id']: string }>) {
