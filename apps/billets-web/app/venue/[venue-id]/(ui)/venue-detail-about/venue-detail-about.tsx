208e0bb8fc16d490d7004af675db436e163bea9e
yungblud <killi8n@gmail.com>
2025-01-22 11:58:18 +0900

    feat(billets-web): :art: implemented about styles

--- apps/billets-web/app/venue/[venue-id]/(ui)/venue-detail-about/venue-detail-about.tsx
+++ apps/billets-web/app/venue/[venue-id]/(ui)/venue-detail-about/venue-detail-about.tsx
@@ -2,16 +2,18 @@
 
 import { apiClient } from '@/libs/openapi-client'
 import { useQuery } from '@tanstack/react-query'
+import { StyledTitleText } from './venue-detail-about.styled'
 
 export function VenueDetailAbout({ venueId }: { venueId: string }) {
-  const { data: venueDetail, isLoading: isLoadingVenueDetail } = useQuery({
+  const { data: venueDetail } = useQuery({
     queryKey: apiClient.venue.queryKeys.detail(venueId),
     queryFn: () => apiClient.venue.getVenueDetail(venueId),
   })
 
   return (
-    <div>
-      <p>{venueDetail?.address}</p>
-    </div>
+    <>
+      <StyledTitleText as="h2">About</StyledTitleText>
+      <StyledTitleText>{venueDetail?.address}</StyledTitleText>
+    </>
   )
 }
--- apps/billets-web/app/venue/[venue-id]/(ui)/venue-detail-top/venue-detail-top.tsx
+++ apps/billets-web/app/venue/[venue-id]/(ui)/venue-detail-top/venue-detail-top.tsx
@@ -5,7 +5,7 @@ import { useQuery } from '@tanstack/react-query'
 import { VenueDetailTopTitleText } from './venue-detail-top.styled'
 
 export function VenueDetailTop({ venueId }: { venueId: string }) {
-  const { data: venueDetail, isLoading: isLoadingVenueDetail } = useQuery({
+  const { data: venueDetail } = useQuery({
     queryKey: apiClient.venue.queryKeys.detail(venueId),
     queryFn: () => apiClient.venue.getVenueDetail(venueId),
   })
