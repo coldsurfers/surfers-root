f1ff4546ab46d22dfd7237c304ada1a566c7bbc3
yungblud <killi8n@gmail.com>
2025-01-22 12:33:08 +0900

    feat(billets-web): :art: implemented thumburl

--- apps/billets-web/app/artist/[artist-id]/(ui)/artist-detail-top/artist-detail-top.tsx
+++ apps/billets-web/app/artist/[artist-id]/(ui)/artist-detail-top/artist-detail-top.tsx
@@ -2,11 +2,31 @@
 
 import { apiClient } from '@/libs/openapi-client'
 import { useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
+import {
+  StyledArtistDetailTopContainer,
+  StyledArtistDetailTopLeft,
+  StyledArtistDetailTopRight,
+  StyledArtistThumbnail,
+} from './artist-detail-top.styled'
 
 export function ArtistDetailTop({ artistId }: { artistId: string }) {
   const { data: artistDetail } = useQuery({
     queryKey: apiClient.artist.queryKeys.detail(artistId),
     queryFn: () => apiClient.artist.getArtistDetail(artistId),
   })
-  return <p>{artistDetail?.name}</p>
+  const thumbUrl = useMemo(() => {
+    if (!artistDetail?.thumbUrl) {
+      return ''
+    }
+    return `${artistDetail.thumbUrl}&width=500&height=500&format=png`
+  }, [artistDetail?.thumbUrl])
+  return (
+    <StyledArtistDetailTopContainer>
+      <StyledArtistDetailTopLeft>{artistDetail?.name}</StyledArtistDetailTopLeft>
+      <StyledArtistDetailTopRight>
+        <StyledArtistThumbnail src={thumbUrl} />
+      </StyledArtistDetailTopRight>
+    </StyledArtistDetailTopContainer>
+  )
 }
