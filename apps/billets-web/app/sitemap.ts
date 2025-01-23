b3bd23cbdc9f2721dc20fa373182361c29667589
yungblud <killi8n@gmail.com>
2025-01-23 09:03:34 +0900

    feat(billets-web): :art: modified sitemap

--- apps/billets-web/app/sitemap.ts
+++ apps/billets-web/app/sitemap.ts
@@ -1,7 +1,5 @@
 import { SITE_URL } from '@/libs/constants'
 import { dbClient } from '@/libs/db/db.client'
-import { apiClient } from '@/libs/openapi-client'
-import { validateCityParam } from '@/libs/utils'
 import { cache } from 'react'
 
 const generateUrl = (subPath: string) => {
@@ -14,6 +12,14 @@ const findAllVenues = cache(async () => {
 const findAllArtists = cache(async () => {
   const artists = await dbClient.artist.findMany()
   return artists
+})
+const findAllCities = cache(async () => {
+  const cities = await dbClient.locationCity.findMany()
+  return cities
+})
+const findAllEvents = cache(async () => {
+  const events = await dbClient.concert.findMany()
+  return events
 })
 
 export default async function sitemap() {
@@ -50,13 +56,11 @@ export default async function sitemap() {
     },
   ]
 
-  const availableCities = (await apiClient.location.getCountries())
-    .flatMap((value) => value.cities)
-    .map((value) => value.name)
+  const allCities = await findAllCities()
 
   // "/browse/[city]"
-  const browseByCitySitemaps = availableCities.map((city) => {
-    const url = generateUrl(`/browse/${city}`)
+  const browseByCitySitemaps = allCities.map((city) => {
+    const url = generateUrl(`/browse/${city.name}`)
     const lastModified = new Date()
     const changeFrequency = 'weekly'
     const priority = 0.8
@@ -68,36 +72,21 @@ export default async function sitemap() {
     }
   })
 
+  const allEvents = await findAllEvents()
+
   // "/event/[event-id]"
-  const eventSitemaps = (
-    await Promise.all(
-      availableCities.map(async (city) => {
-        const validation = await validateCityParam(city)
-        if (!validation.isValid) {
-          return []
-        }
-        const { data: cityData } = validation
-        const events = await apiClient.event.getEvents({
-          offset: 0,
-          // @todo: temp
-          size: 100,
-          latitude: cityData.lat,
-          longitude: cityData.lng,
-        })
-        const lastModified = new Date()
-        const changeFrequency = 'weekly'
-        const priority = 0.8
-        return (
-          events?.map((event) => ({
-            url: generateUrl(`/event/${event.data.id}`),
-            lastModified,
-            changeFrequency,
-            priority,
-          })) ?? []
-        )
-      }),
-    )
-  ).flat()
+  const eventSitemaps = allEvents.map((event) => {
+    const url = generateUrl(`/event/${event.id}`)
+    const lastModified = new Date()
+    const changeFrequency = 'weekly'
+    const priority = 0.8
+    return {
+      url,
+      lastModified,
+      changeFrequency,
+      priority,
+    }
+  })
 
   // "/venue/[venue-id]"
   const allVenues = await findAllVenues()
