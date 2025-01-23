96318aeb8949859501ded96aabc2937333f1075d
yungblud <killi8n@gmail.com>
2025-01-23 10:37:31 +0900

    fix(billets-web): :art: modified empty text with image

--- apps/billets-web/app/event/[event-id]/(ui)/lineup/lineup.styled.ts
+++ apps/billets-web/app/event/[event-id]/(ui)/lineup/lineup.styled.ts
@@ -11,13 +11,27 @@ export const StyledLineupContainer = styled(Link)`
   padding-bottom: 1rem;
 `
 
-export const StyledLineupImage = styled.img`
+export const StyledLineupImageWrapper = styled.div`
   border-radius: 50%;
   width: 56px;
   height: 56px;
+  background-color: ${semantics.color.background[3]};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const StyledLineupImageEmptyText = styled(Text)`
+  margin: unset;
+  font-weight: 600;
+`
+
+export const StyledLineupImage = styled.img`
+  border-radius: 50%;
+  width: 100%;
+  height: 100%;
   object-fit: cover;
   object-position: 50%;
-  background-color: ${semantics.color.background[3]};
 `
 
 export const StyledLineupNameText = styled(Text)`
