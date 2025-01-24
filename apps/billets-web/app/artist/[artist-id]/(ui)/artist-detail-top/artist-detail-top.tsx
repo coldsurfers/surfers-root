6ffbe625d08aa2502508e7f5f73d44ae47f1b5ad
yungblud <killi8n@gmail.com>
2025-01-24 22:22:49 +0900

    feat(billets-web): :art: implemented thumb copyright

--- apps/billets-web/app/artist/[artist-id]/(ui)/artist-detail-top/artist-detail-top.tsx
+++ apps/billets-web/app/artist/[artist-id]/(ui)/artist-detail-top/artist-detail-top.tsx
@@ -2,7 +2,8 @@
 
 import { apiClient } from '@/libs/openapi-client'
 import { useQuery } from '@tanstack/react-query'
-import { useMemo } from 'react'
+import { ImageModal } from 'app/(ui)'
+import { useCallback, useMemo, useState } from 'react'
 import {
   StyledArtistDetailTopContainer,
   StyledArtistDetailTopLeft,
@@ -17,6 +18,7 @@ import {
 } from './artist-detail-top.styled'
 
 export function ArtistDetailTop({ artistId }: { artistId: string }) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const { data: artistDetail } = useQuery({
     queryKey: apiClient.artist.queryKeys.detail(artistId),
     queryFn: () => apiClient.artist.getArtistDetail(artistId),
@@ -27,24 +29,42 @@ export function ArtistDetailTop({ artistId }: { artistId: string }) {
     }
     return `${artistDetail.thumbUrl}&width=500&height=500&format=png`
   }, [artistDetail?.thumbUrl])
+  const imageModalThumbUrl = useMemo(() => {
+    if (!artistDetail?.thumbUrl) {
+      return ''
+    }
+    return `${artistDetail.thumbUrl}&width=1300&height=1300`
+  }, [artistDetail?.thumbUrl])
+  const thumbCopyright = useMemo(() => {
+    return artistDetail?.thumbCopyright ?? undefined
+  }, [artistDetail?.thumbCopyright])
+  const openModal = useCallback(() => setIsModalOpen(true), [])
   return (
-    <StyledArtistDetailTopContainer>
-      <StyledArtistDetailTopLeft>
-        <StyledArtistTopDescriptionWrapper>
-          <StyledArtistTopSectionTitleText>Artist</StyledArtistTopSectionTitleText>
-          <StyledArtistNameText>{artistDetail?.name}</StyledArtistNameText>
-        </StyledArtistTopDescriptionWrapper>
-      </StyledArtistDetailTopLeft>
-      <StyledArtistDetailTopRight>
-        <StyledArtistThumbnailWrapper>
-          {thumbUrl && (
-            <StyledContentWrapper>
-              <StyledArtistThumbnail src={thumbUrl} />
-              <StyledInfoIcon />
-            </StyledContentWrapper>
-          )}
-        </StyledArtistThumbnailWrapper>
-      </StyledArtistDetailTopRight>
-    </StyledArtistDetailTopContainer>
+    <>
+      <StyledArtistDetailTopContainer>
+        <StyledArtistDetailTopLeft>
+          <StyledArtistTopDescriptionWrapper>
+            <StyledArtistTopSectionTitleText>Artist</StyledArtistTopSectionTitleText>
+            <StyledArtistNameText>{artistDetail?.name}</StyledArtistNameText>
+          </StyledArtistTopDescriptionWrapper>
+        </StyledArtistDetailTopLeft>
+        <StyledArtistDetailTopRight>
+          <StyledArtistThumbnailWrapper>
+            {thumbUrl && (
+              <StyledContentWrapper>
+                <StyledArtistThumbnail src={thumbUrl} onClick={openModal} />
+                <StyledInfoIcon onClick={openModal} />
+              </StyledContentWrapper>
+            )}
+          </StyledArtistThumbnailWrapper>
+        </StyledArtistDetailTopRight>
+      </StyledArtistDetailTopContainer>
+      <ImageModal
+        visible={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        src={imageModalThumbUrl}
+        copyright={thumbCopyright}
+      />
+    </>
   )
 }
