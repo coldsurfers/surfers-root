4cca895fec76fbf4e621cdf0079d63dbddd590ee
yungblud <killi8n@gmail.com>
2025-01-24 22:22:49 +0900

    feat(billets-web): :art: implemented thumb copyright

--- apps/billets-web/app/event/[event-id]/(ui)/poster-thumbnail/poster-thumbnail.tsx
+++ apps/billets-web/app/event/[event-id]/(ui)/poster-thumbnail/poster-thumbnail.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import { useMemo } from 'react'
+import { ImageModal } from 'app/(ui)'
+import { useCallback, useMemo, useState } from 'react'
+import { components } from 'types/api'
 import {
   StyledContentWrapper,
   StyledInfoIcon,
@@ -9,16 +11,40 @@ import {
   StyledPosterThumbnailEmptyText,
 } from './poster-thumbnail.styled'
 
-export function PosterThumbnail({ src, alt }: { src: string; alt?: string }) {
+export function PosterThumbnail({
+  src,
+  alt,
+  copyright,
+}: {
+  src: string
+  alt?: string
+  copyright?: components['schemas']['CopyrightDTOSchema']
+}) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const source = useMemo(() => {
     if (!src) return ''
     return `${src}&width=350&height=350`
   }, [src])
+  const imageModalSource = useMemo(() => {
+    if (!src) return ''
+    return `${src}&width=1300&height=1300`
+  }, [src])
+  const openModal = useCallback(() => {
+    setIsModalOpen(true)
+  }, [])
   return source ? (
-    <StyledContentWrapper>
-      <StyledPosterThumbnail src={source} alt={alt} />
-      <StyledInfoIcon />
-    </StyledContentWrapper>
+    <>
+      <StyledContentWrapper>
+        <StyledPosterThumbnail src={source} alt={alt} onClick={openModal} />
+        <StyledInfoIcon onClick={openModal} />
+      </StyledContentWrapper>
+      <ImageModal
+        visible={isModalOpen}
+        src={imageModalSource}
+        onClose={() => setIsModalOpen(false)}
+        copyright={copyright}
+      />
+    </>
   ) : (
     <StyledPosterThumbnailEmpty>
       <StyledPosterThumbnailEmptyText>{alt}</StyledPosterThumbnailEmptyText>
