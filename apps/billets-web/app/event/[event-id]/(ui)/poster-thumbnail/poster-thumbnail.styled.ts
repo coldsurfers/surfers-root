4f08ea7292f894e065e55ef38d911e96b4d85698
yungblud <killi8n@gmail.com>
2025-01-24 18:38:28 +0900

    feat(billets-web): :art: modified thumbnail img event detail

--- apps/billets-web/app/event/[event-id]/(ui)/poster-thumbnail/poster-thumbnail.styled.ts
+++ apps/billets-web/app/event/[event-id]/(ui)/poster-thumbnail/poster-thumbnail.styled.ts
@@ -1,3 +1,4 @@
+import { semantics, Text } from '@coldsurfers/ocean-road'
 import styled from '@emotion/styled'
 
 export const StyledPosterThumbnail = styled.img`
@@ -6,4 +7,27 @@ export const StyledPosterThumbnail = styled.img`
   object-position: 50%;
   width: 100%;
   aspect-ratio: 1 / 1;
+`
+
+export const StyledPosterThumbnailEmpty = styled.div`
+  border-radius: 12px;
+  object-fit: cover;
+  object-position: 50%;
+  width: 100%;
+  aspect-ratio: 1 / 1;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${semantics.color.background[1]};
+`
+
+export const StyledPosterThumbnailEmptyText = styled(Text)`
+  text-align: center;
+  font-weight: 600;
+  font-size: 22px;
+
+  padding-left: 1rem;
+  padding-right: 1rem;
 `
--- apps/billets-web/app/event/[event-id]/(ui)/poster-thumbnail/poster-thumbnail.tsx
+++ apps/billets-web/app/event/[event-id]/(ui)/poster-thumbnail/poster-thumbnail.tsx
@@ -1,11 +1,22 @@
 'use client'
 
 import { useMemo } from 'react'
-import { StyledPosterThumbnail } from './poster-thumbnail.styled'
+import {
+  StyledPosterThumbnail,
+  StyledPosterThumbnailEmpty,
+  StyledPosterThumbnailEmptyText,
+} from './poster-thumbnail.styled'
 
 export function PosterThumbnail({ src, alt }: { src: string; alt?: string }) {
   const source = useMemo(() => {
+    if (!src) return ''
     return `${src}&width=350&height=350`
   }, [src])
-  return <StyledPosterThumbnail src={source} alt={alt} />
+  return source ? (
+    <StyledPosterThumbnail src={source} alt={alt} />
+  ) : (
+    <StyledPosterThumbnailEmpty>
+      <StyledPosterThumbnailEmptyText>{alt}</StyledPosterThumbnailEmptyText>
+    </StyledPosterThumbnailEmpty>
+  )
 }
