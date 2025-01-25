a91b96d5003f1864dc1b33286480eb1e2a8e5a4e
yungblud <killi8n@gmail.com>
2025-01-25 12:35:53 +0900

    feat(blog-client): :art: removed page

--- apps/blog-client/app/[locale]/writers/page.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled'
-
-export const StyledWritersPageHeader = styled.section`
-  margin-top: 8px;
-  margin-bottom: 50px;
-
-  display: flex;
-  flex-direction: column;
-`
