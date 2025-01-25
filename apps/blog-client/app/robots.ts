93f69c1e0e1940c1ab79a318f8e9d8025a4d0b45
yungblud <killi8n@gmail.com>
2025-01-25 12:35:40 +0900

    feat(blog-client): :art: updated robots.txt

--- apps/blog-client/app/robots.ts
+++ apps/blog-client/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/en/resume', '/en/writers', '/ko/resume', '/ko/writers'],
+      disallow: ['/en/resume', '/en/about', '/en/about/paul', '/ko/resume', '/ko/about', '/ko/about/paul'],
     },
     sitemap: 'https://blog.coldsurf.io/sitemap.xml',
     host: 'https://blog.coldsurf.io',
