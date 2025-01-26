fb22d77e6713c846f2089fc7e7c19607c9bea24a
yungblud <killi8n@gmail.com>
2025-01-26 14:33:23 +0900

    feat(ocean-road): :art: modified media breakpoints

--- packages/ocean-road/src/utils/breakpoints.ts
+++ packages/ocean-road/src/utils/breakpoints.ts
@@ -2,8 +2,8 @@ const breakpoints = {
   'small': 576,
   'medium': 768,
   'large': 992,
-  'x-large': 1200,
-  'xx-large': 1400,
+  'x-large': 1280,
+  'xx-large': 1440,
 } as const
 
 export default breakpoints
