4f3a2b488bb7470322df17e96cdffeeb0adb6f6e
yungblud <killi8n@gmail.com>
2025-01-28 10:18:58 +0900

    refactor(billets-web): :art: api error boundary with query reset

--- apps/billets-web/libs/registries/api-error-boundary-registry/api-error-boundary-registry.tsx
+++ apps/billets-web/libs/registries/api-error-boundary-registry/api-error-boundary-registry.tsx
@@ -1,17 +1,21 @@
 'use client'
 
+import { QueryErrorResetBoundary } from '@tanstack/react-query'
 import { NetworkError } from 'app/(ui)'
 import { PropsWithChildren } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
 export const ApiErrorBoundaryRegistry = ({ children }: PropsWithChildren) => {
   return (
-    <ErrorBoundary
-      FallbackComponent={({ resetErrorBoundary }) => {
-        return <NetworkError onClickRetry={resetErrorBoundary} />
-      }}
-    >
-      {children}
-    </ErrorBoundary>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary
+          onReset={reset}
+          fallbackRender={({ resetErrorBoundary }) => <NetworkError onClickRetry={resetErrorBoundary} />}
+        >
+          {children}
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   )
 }
