16f4ba1ecb15cd6d08afdcc6322111c7c3b8b732
yungblud <killi8n@gmail.com>
2025-01-28 13:54:01 +0900

    feat(billets-server): :art: filter disabled location city

--- apps/billets-server/src/repositories/location.repository.impl.ts
+++ apps/billets-server/src/repositories/location.repository.impl.ts
@@ -5,7 +5,11 @@ import { LocationRepository } from './location.repository'
 
 export class LocationRepositoryImpl implements LocationRepository {
   async findAllCity(): Promise<LocationCityDTO[]> {
-    const data = await dbClient.locationCity.findMany({})
+    const data = await dbClient.locationCity.findMany({
+      where: {
+        disabled: false,
+      },
+    })
     return data.map((value) => this.toLocationCityDTO(value))
   }
   async findAllConcertsByGeohashes(geohashes: string[]): Promise<LocationConcertDTO[]> {
@@ -58,6 +62,11 @@ export class LocationRepositoryImpl implements LocationRepository {
     const data = await dbClient.locationCountry.findMany({
       include: {
         locationCities: {
+          where: {
+            locationCity: {
+              disabled: false,
+            },
+          },
           include: {
             locationCity: true,
           },
