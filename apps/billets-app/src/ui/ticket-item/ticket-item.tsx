a38619b3a5a3d2a933e865a4d5b8d9de27d48117
yungblud <killi8n@gmail.com>
2025-01-29 18:37:30 +0900

    feat(billets-app): :art: implemented ad

--- apps/billets-app/src/ui/ticket-item/ticket-item.tsx
+++ apps/billets-app/src/ui/ticket-item/ticket-item.tsx
@@ -1,21 +1,40 @@
+import { useInterstitialAd } from '@/features/google-ads/google-ads.hooks'
+import { concertTicketBtnPressCountForInterstitialAdStorage } from '@/lib/storage/concert-ticket-btn-press-count-for-interstitial-ad-storage'
 import { components } from '@/types/api'
 import { Button, Text, useColorScheme } from '@coldsurfers/ocean-road/native'
-import { memo } from 'react'
+import { memo, useCallback } from 'react'
 import { Linking, StyleSheet, View } from 'react-native'
 
 type TicketItemProps = components['schemas']['TicketDTOSchema']
 
 export const TicketItem = memo(({ sellerName, url }: TicketItemProps) => {
   const { semantics } = useColorScheme()
-  const onPress = () => {
-    Linking.openURL(url)
-  }
+
+  const link = useCallback(() => Linking.openURL(url), [url])
+
+  const { show, loaded } = useInterstitialAd({
+    onAdOpened: () => console.log('opened'),
+    onAdClosed: link,
+  })
+
+  const onPress = useCallback(() => {
+    const prevCount = concertTicketBtnPressCountForInterstitialAdStorage.get() ?? 0
+    const nextCount = prevCount + 1
+    concertTicketBtnPressCountForInterstitialAdStorage.set(nextCount)
+    const shouldShowAd = nextCount % 5 === 0 && loaded
+    if (shouldShowAd) {
+      show()
+    } else {
+      link()
+    }
+  }, [link, loaded, show])
+
   return (
     <View style={[styles.ticketWrapper, { backgroundColor: semantics.background[4] }]}>
       <Text weight="bold" style={{ color: semantics.foreground[1] }}>
         {sellerName}
       </Text>
-      <Button size="md" onPress={onPress} style={styles.ticketBtn}>
+      <Button theme="pink" size="md" onPress={onPress} style={styles.ticketBtn}>
         티켓 찾기
       </Button>
     </View>
