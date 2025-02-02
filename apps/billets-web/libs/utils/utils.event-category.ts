8966bcca7bdd3c50b93deca1c89798f81b3a82ec
yungblud <killi8n@gmail.com>
2025-02-02 17:35:38 +0900

    feat(billets-web): :art: implemented metadata

--- apps/billets-web/libs/utils/utils.event-category.ts
+++ apps/billets-web/libs/utils/utils.event-category.ts
@@ -31,3 +31,16 @@ export const validateEventCategoryParam = cache(async (eventCategoryParam: strin
     },
   } as const
 })
+
+export const getEventCategoryUIName = (originalName: string) => {
+  switch (originalName) {
+    case 'Gigs':
+      return '콘서트'
+    case 'Theatre':
+      return '연극 / 뮤지컬'
+    case 'Dance':
+      return '무용'
+    default:
+      return originalName
+  }
+}
