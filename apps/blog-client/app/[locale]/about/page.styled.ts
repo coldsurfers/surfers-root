80d9d6f794ce5b99ad57f4754dc4fda275807eef
yungblud <killi8n@gmail.com>
2025-02-08 10:25:12 +0900

    feat(blog-client): :art: implemented new about page

--- apps/blog-client/app/[locale]/about/page.styled.ts
+++ apps/blog-client/app/[locale]/about/page.styled.ts
@@ -1,4 +1,10 @@
+'use client'
+
 import styled from '@emotion/styled'
+
+export const StyledAboutPageInnerLayout = styled.div`
+  margin-top: 6.5rem;
+`
 
 export const StyledWritersPageHeader = styled.section`
   margin-top: 8px;
