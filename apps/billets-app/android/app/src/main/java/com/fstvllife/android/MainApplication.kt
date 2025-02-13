50aca9376f0d0fde41e64c1e7b22f60c01725067
yungblud <killi8n@gmail.com>
2025-02-13 21:09:27 +0900

    feat(billets-app): :art: upgraded react native

--- apps/billets-app/android/app/src/main/java/com/fstvllife/android/MainApplication.kt
+++ apps/billets-app/android/app/src/main/java/com/fstvllife/android/MainApplication.kt
@@ -10,7 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import org.wonday.orientation.OrientationActivityLifecycle
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.microsoft.codepush.react.CodePush
 
 class MainApplication : Application(), ReactApplication {
@@ -41,8 +41,7 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
-//    registerActivityLifecycleCallbacks(OrientationActivityLifecycle.getInstance());
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
