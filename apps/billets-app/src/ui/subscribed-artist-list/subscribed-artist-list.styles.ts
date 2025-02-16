c87318e360da6a0d94260c2fb1a31e0af399624f
yungblud <killi8n@gmail.com>
2025-02-16 19:04:38 +0900

    feat(billets-app): :art: implemented subscribed artist list screen

---
+++ apps/billets-app/src/ui/subscribed-artist-list/subscribed-artist-list.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+export const subscribedArtistListStyles = StyleSheet.create({
+  contentContainer: {
+    paddingHorizontal: 16,
+    marginTop: 12,
+  },
+  itemSeparator: {
+    width: 10,
+  },
+})
