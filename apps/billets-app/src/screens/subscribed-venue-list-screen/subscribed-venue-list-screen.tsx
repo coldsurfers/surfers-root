6c8e7bf0b647dbb237f265559e8eba137609b809
yungblud <killi8n@gmail.com>
2025-02-17 19:37:38 +0900

    feat(billets-app): :art: implemented venue list

--- apps/billets-app/src/screens/subscribed-venue-list-screen/subscribed-venue-list-screen.tsx
+++ apps/billets-app/src/screens/subscribed-venue-list-screen/subscribed-venue-list-screen.tsx
@@ -1,3 +1,12 @@
 import { CommonScreenLayout } from '@/ui'
+import { SubscribedVenueList } from '@/ui/subscribed-venue-list'
+import { Spinner } from '@coldsurfers/ocean-road/native'
+import { Suspense } from 'react'
 
-export const SubscribedVenueListScreen = () => <CommonScreenLayout></CommonScreenLayout>
+export const SubscribedVenueListScreen = () => (
+  <CommonScreenLayout withBottomTab={false}>
+    <Suspense fallback={<Spinner positionCenter />}>
+      <SubscribedVenueList />
+    </Suspense>
+  </CommonScreenLayout>
+)
---
+++ apps/billets-app/src/ui/subscribed-venue-list/subscribed-venue-list.tsx
@@ -0,0 +1,91 @@
+import { apiClient } from '@/lib/api/openapi-client'
+import { useSubscribedArtistListScreenNavigation } from '@/screens/subscribed-artist-list-screen/subscribed-artist-list-screen.hooks'
+import { useColorScheme } from '@coldsurfers/ocean-road/native'
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
+import { Suspense, useCallback, useMemo, useState } from 'react'
+import { FlatList, ListRenderItem, View } from 'react-native'
+import { SubscribedVenueListItem } from '../subscribed-venue-list-item'
+import { subscribedVenueListStyles } from './subscribed-venue-list.styles'
+
+const ItemSeparator = () => <View style={subscribedVenueListStyles.itemSeparator} />
+
+const PER_PAGE = 20
+
+export function SubscribedVenueList({ listHeaderComponent }: { listHeaderComponent?: React.ComponentType<unknown> }) {
+  const { semantics } = useColorScheme()
+  const navigation = useSubscribedArtistListScreenNavigation()
+  const [isRefreshing, setIsRefreshing] = useState(false)
+  const { data, fetchNextPage, isFetchingNextPage, isLoading, hasNextPage, isPending, refetch } =
+    useSuspenseInfiniteQuery({
+      initialPageParam: 0,
+      queryKey: apiClient.subscribe.queryKeys.venueList({}),
+      queryFn: ({ pageParam = 0 }) => apiClient.subscribe.getVenueList({ offset: pageParam, size: PER_PAGE }),
+      getNextPageParam: (lastPage, allPages) => {
+        if (!lastPage) {
+          return undefined
+        }
+        if (lastPage.length < PER_PAGE) {
+          return undefined
+        }
+        return allPages.length * PER_PAGE
+      },
+    })
+  const listData = useMemo(() => {
+    return data?.pages.flat() ?? []
+  }, [data])
+  const renderItem = useCallback<ListRenderItem<(typeof listData)[number]>>(
+    ({ item }) => {
+      const onPress = () => {
+        navigation.navigate('VenueStackNavigation', {
+          params: {
+            id: item.venueId,
+          },
+          screen: 'VenueDetailScreen',
+        })
+      }
+      return (
+        <Suspense>
+          <SubscribedVenueListItem data={item} onPress={onPress} />
+        </Suspense>
+      )
+    },
+    [navigation],
+  )
+
+  const onEndReached = useCallback(async () => {
+    if (isFetchingNextPage || isLoading || !hasNextPage || isPending) {
+      return
+    }
+    await fetchNextPage()
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage, isLoading, isPending])
+
+  const onRefresh = useCallback(async () => {
+    setIsRefreshing(true)
+    await refetch()
+    setIsRefreshing(false)
+  }, [refetch])
+
+  return (
+    <FlatList
+      data={listData}
+      keyExtractor={(item) => `${item.venueId}`}
+      renderItem={renderItem}
+      ItemSeparatorComponent={ItemSeparator}
+      contentContainerStyle={{
+        paddingHorizontal: 16,
+        paddingBottom: 24,
+        marginTop: 12,
+        backgroundColor: semantics.background[3],
+      }}
+      ListHeaderComponent={listHeaderComponent}
+      onEndReached={onEndReached}
+      onRefresh={onRefresh}
+      refreshing={isRefreshing}
+      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}
+      style={{
+        backgroundColor: semantics.background[3],
+      }}
+    />
+  )
+}
