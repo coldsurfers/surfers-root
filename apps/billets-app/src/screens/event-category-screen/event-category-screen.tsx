a4601f29e4cb4b445d5c94d0866348ec69e3d2f1
yungblud <killi8n@gmail.com>
2025-02-18 21:34:00 +0900

    feat(billets-app): :art: implemented event category screen list

--- apps/billets-app/src/screens/event-category-screen/event-category-screen.tsx
+++ apps/billets-app/src/screens/event-category-screen/event-category-screen.tsx
@@ -1,3 +1,24 @@
-import { CommonScreenLayout } from '@/ui'
+import { CommonScreenLayout, ConcertList } from '@/ui'
+import { Spinner } from '@coldsurfers/ocean-road/native'
+import { Suspense, useCallback } from 'react'
+import { useEventCategoryScreenNavigation, useEventCategoryScreenRoute } from './event-category-screen.hooks'
 
-export const EventCategoryScreen = () => <CommonScreenLayout></CommonScreenLayout>
+export const EventCategoryScreen = () => {
+  const route = useEventCategoryScreenRoute()
+  const navigation = useEventCategoryScreenNavigation()
+  const onPressItem = useCallback(
+    (eventId: string) => {
+      navigation.navigate('EventDetailScreen', {
+        eventId,
+      })
+    },
+    [navigation],
+  )
+  return (
+    <CommonScreenLayout withBottomTab={false}>
+      <Suspense fallback={<Spinner positionCenter />}>
+        <ConcertList eventCategory={route.params.eventCategory} onPressItem={(item) => onPressItem(item.id)} />
+      </Suspense>
+    </CommonScreenLayout>
+  )
+}
