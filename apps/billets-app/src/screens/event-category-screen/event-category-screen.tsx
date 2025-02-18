d47e6dcf060fb6a21adacad8c2dff0bfe000c68f
yungblud <killi8n@gmail.com>
2025-02-18 21:38:36 +0900

    feat(billets-app): :art: implemented profiler

--- apps/billets-app/src/screens/event-category-screen/event-category-screen.tsx
+++ apps/billets-app/src/screens/event-category-screen/event-category-screen.tsx
@@ -1,24 +1,33 @@
+import { zodScreen } from '@/lib/navigations'
 import { CommonScreenLayout, ConcertList } from '@/ui'
 import { Spinner } from '@coldsurfers/ocean-road/native'
+import { PerformanceMeasureView, useStartProfiler } from '@shopify/react-native-performance'
 import { Suspense, useCallback } from 'react'
 import { useEventCategoryScreenNavigation, useEventCategoryScreenRoute } from './event-category-screen.hooks'
 
 export const EventCategoryScreen = () => {
+  const startNavigationTTITimer = useStartProfiler()
   const route = useEventCategoryScreenRoute()
   const navigation = useEventCategoryScreenNavigation()
   const onPressItem = useCallback(
     (eventId: string) => {
+      startNavigationTTITimer({
+        source: zodScreen.EventCategoryScreen.name,
+        uiEvent: undefined,
+      })
       navigation.navigate('EventDetailScreen', {
         eventId,
       })
     },
-    [navigation],
+    [navigation, startNavigationTTITimer],
   )
   return (
-    <CommonScreenLayout withBottomTab={false}>
-      <Suspense fallback={<Spinner positionCenter />}>
-        <ConcertList eventCategory={route.params.eventCategory} onPressItem={(item) => onPressItem(item.id)} />
-      </Suspense>
-    </CommonScreenLayout>
+    <PerformanceMeasureView interactive={false} screenName={zodScreen.EventCategoryScreen.name}>
+      <CommonScreenLayout withBottomTab={false}>
+        <Suspense fallback={<Spinner positionCenter />}>
+          <ConcertList eventCategory={route.params.eventCategory} onPressItem={(item) => onPressItem(item.id)} />
+        </Suspense>
+      </CommonScreenLayout>
+    </PerformanceMeasureView>
   )
 }
