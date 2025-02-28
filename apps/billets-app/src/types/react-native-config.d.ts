198246811896da32aa15746ed59d1e180872d298
yungblud <killi8n@gmail.com>
2025-02-28 21:11:30 +0900

    feat(billets-app): :art: removed android codepush key

--- apps/billets-app/src/types/react-native-config.d.ts
+++ apps/billets-app/src/types/react-native-config.d.ts
@@ -5,7 +5,6 @@ declare module 'react-native-config' {
     GOOGLE_SIGNIN_WEB_IOS_CLIENT_ID?: string
     ANDROID_GOOGLE_MAPS_API_KEY?: string
     IOS_CODE_PUSH_DEPLOYMENT_KEY?: string
-    ANDROID_CODE_PUSH_DEPLOYMENT_KEY?: string
     GOOGLE_MOBILE_ADS_ANDROID_APP_ID?: string
     GOOGLE_MOBILE_ADS_IOS_APP_ID?: string
     GOOGLE_MOBILE_ADS_IOS_INTERSTITIAL_ID?: string
