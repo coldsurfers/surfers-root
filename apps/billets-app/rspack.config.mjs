6f8b90d39ec552dd4319e6f0e9bc430f4074e482
yungblud <killi8n@gmail.com>
2025-03-02 00:04:20 +0900

    feat(billets-app): :art: rspack setup

--- apps/billets-app/rspack.config.mjs
+++ apps/billets-app/rspack.config.mjs
@@ -1,134 +1,57 @@
-// https://github.com/callstack/repack/blob/2390ead8ae0da12f7af59422b4de6b6162acf635/apps/tester-federation-v2/rspack.config.host-app.mjs
-// @ts-check
 import * as Repack from '@callstack/repack'
 import { ReanimatedPlugin } from '@callstack/repack-plugin-reanimated'
-import rspack from '@rspack/core'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const dirname = Repack.getDirname(import.meta.url)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-/** @type {(env: import('@callstack/repack').EnvOptions) => import('@rspack/core').Configuration} */
-export default (env) => {
-  const {
-    mode = 'development',
-    context = dirname,
-    entry = './index.js',
-    platform = process.env.PLATFORM,
-    minimize = mode === 'production',
-    devServer = undefined,
-    bundleFilename = undefined,
-    sourceMapFilename = undefined,
-    assetsPath = undefined,
-  } = env
+/**
+ * Rspack configuration enhanced with Re.Pack defaults for React Native.
+ *
+ * Learn about Rspack configuration: https://rspack.dev/config/
+ * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
+ */
 
-  if (!platform) {
-    throw new Error('Missing platform')
-  }
+console.log(process.env.PLATFORM)
 
-  process.env.BABEL_ENV = mode
-
-  return {
-    mode,
-    devtool: false,
-    context,
-    entry,
-    resolve: {
-      ...Repack.getResolveOptions(platform),
-      alias: {
-        'react': new URL('../../node_modules/react', import.meta.url).pathname,
-        '@coldsurfers/ocean-road/native': new URL('../../packages/ocean-road/src/native/index.ts', import.meta.url)
-          .pathname,
-        '@/screens': new URL('./src/screens', import.meta.url).pathname,
-        '@/navigations': new URL('./src/navigations', import.meta.url).pathname,
-        '@/lib': new URL('./src/lib', import.meta.url).pathname,
-        '@/ui': new URL('./src/ui', import.meta.url).pathname,
-        '@/features': new URL('./src/features', import.meta.url).pathname,
-        '@/types': new URL('./src/types', import.meta.url).pathname,
-        'assets': new URL('./assets', import.meta.url).pathname,
+/** @type {import('@rspack/core').Configuration} */
+export default {
+  context: __dirname,
+  entry: './index.js',
+  resolve: {
+    ...Repack.getResolveOptions(process.env.PLATFORM),
+    alias: {
+      'react': new URL('../../node_modules/react', import.meta.url).pathname,
+      '@coldsurfers/ocean-road/native': new URL('../../packages/ocean-road/src/native/index.ts', import.meta.url)
+        .pathname,
+      '@/screens': new URL('./src/screens', import.meta.url).pathname,
+      '@/navigations': new URL('./src/navigations', import.meta.url).pathname,
+      '@/lib': new URL('./src/lib', import.meta.url).pathname,
+      '@/ui': new URL('./src/ui', import.meta.url).pathname,
+      '@/features': new URL('./src/features', import.meta.url).pathname,
+      '@/types': new URL('./src/types', import.meta.url).pathname,
+      'assets': new URL('./assets', import.meta.url).pathname,
+    },
+  },
+  module: {
+    rules: [
+      ...Repack.getJsTransformRules(),
+      ...Repack.getAssetTransformRules(),
+      {
+        type: 'javascript/auto',
+        test: /(@hot-updater|hot-updater)/,
+        use: {
+          loader: 'babel-loader', // Babel을 Webpack에서 사용
+          options: {
+            babelrc: false,
+            configFile: false,
+            presets: ['module:@react-native/babel-preset'],
+            plugins: ['hot-updater/babel-plugin'],
+          },
+        },
       },
-    },
-    output: {
-      clean: true,
-      hashFunction: 'xxhash64',
-      path: path.join(dirname, 'build', 'host-app', platform),
-      filename: 'index.bundle',
-      chunkFilename: '[name].chunk.bundle',
-      publicPath: Repack.getPublicPath({ platform, devServer }),
-      uniqueName: 'BilletsApp',
-    },
-    optimization: {
-      minimize,
-      chunkIds: 'named',
-    },
-    module: {
-      rules: [
-        Repack.REACT_NATIVE_LOADING_RULES,
-        Repack.NODE_MODULES_LOADING_RULES,
-        Repack.FLOW_TYPED_MODULES_LOADING_RULES,
-        {
-          test: /\.[jt]sx?$/,
-          type: 'javascript/auto',
-          exclude: [/node_modules/],
-          use: {
-            loader: 'builtin:swc-loader',
-            options: {
-              env: {
-                targets: { 'react-native': '0.75.3' },
-              },
-              jsc: {
-                assumptions: {
-                  setPublicClassFields: true,
-                  privateFieldsAsProperties: true,
-                },
-                externalHelpers: true,
-                transform: {
-                  react: {
-                    runtime: 'automatic',
-                  },
-                },
-              },
-            },
-          },
-        },
-        {
-          test: Repack.getAssetExtensionsRegExp(Repack.ASSET_EXTENSIONS),
-          use: {
-            loader: '@callstack/repack/assets-loader',
-            options: {
-              platform,
-              devServerEnabled: Boolean(devServer),
-            },
-          },
-        },
-        {
-          test: /\.(js|jsx)$/, // JavaScript 및 JSX 파일 처리
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader', // Babel을 Webpack에서 사용
-            options: {
-              configFile: './babel.config.js', // babel.config.js 사용
-            },
-          },
-        },
-      ],
-    },
-    plugins: [
-      new Repack.RepackPlugin({
-        context,
-        mode,
-        platform,
-        devServer,
-        output: {
-          bundleFilename,
-          sourceMapFilename,
-          assetsPath,
-        },
-      }),
-      // silence missing @react-native-masked-view optionally required by @react-navigation/elements
-      new rspack.IgnorePlugin({
-        resourceRegExp: /^@react-native-masked-view/,
-      }),
-      new ReanimatedPlugin(),
     ],
-  }
+  },
+  plugins: [new Repack.RepackPlugin(), new ReanimatedPlugin()],
 }
