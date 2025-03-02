5b6fd0a3c19d217f3f18c2f1f96c4dc30d799aa8
yungblud <killi8n@gmail.com>
2025-03-02 10:50:37 +0900

    feat(billets-app): :art: modified rspack with dynamic configuration

--- apps/billets-app/rspack.config.mjs
+++ apps/billets-app/rspack.config.mjs
@@ -13,45 +13,60 @@ const __dirname = path.dirname(__filename)
  * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
  */
 
-console.log(process.env.PLATFORM)
+/** @type {(env: import('@callstack/repack').EnvOptions) => import('@rspack/core').Configuration} */
+export default (env) => {
+  const { platform = process.env.PLATFORM, mode = 'development' } = env
 
-/** @type {import('@rspack/core').Configuration} */
-export default {
-  context: __dirname,
-  entry: './index.js',
-  resolve: {
-    ...Repack.getResolveOptions(process.env.PLATFORM),
-    alias: {
-      'react': new URL('../../node_modules/react', import.meta.url).pathname,
-      '@coldsurfers/ocean-road/native': new URL('../../packages/ocean-road/src/native/index.ts', import.meta.url)
-        .pathname,
-      '@/screens': new URL('./src/screens', import.meta.url).pathname,
-      '@/navigations': new URL('./src/navigations', import.meta.url).pathname,
-      '@/lib': new URL('./src/lib', import.meta.url).pathname,
-      '@/ui': new URL('./src/ui', import.meta.url).pathname,
-      '@/features': new URL('./src/features', import.meta.url).pathname,
-      '@/types': new URL('./src/types', import.meta.url).pathname,
-      'assets': new URL('./assets', import.meta.url).pathname,
+  if (!platform) {
+    throw new Error('Missing platform')
+  }
+
+  console.log('platform', platform)
+  console.log('mode', mode)
+
+  process.env.BABEL_ENV = mode
+
+  return {
+    mode,
+    output: {
+      path: path.resolve(__dirname, 'build/generated', platform),
     },
-  },
-  module: {
-    rules: [
-      ...Repack.getJsTransformRules(),
-      ...Repack.getAssetTransformRules(),
-      {
-        type: 'javascript/auto',
-        test: /(@hot-updater|hot-updater)/,
-        use: {
-          loader: 'babel-loader', // Babel을 Webpack에서 사용
-          options: {
-            babelrc: false,
-            configFile: false,
-            presets: ['module:@react-native/babel-preset'],
-            plugins: ['hot-updater/babel-plugin'],
+    context: __dirname,
+    entry: './index.js',
+    resolve: {
+      ...Repack.getResolveOptions(platform),
+      alias: {
+        'react': new URL('../../node_modules/react', import.meta.url).pathname,
+        '@coldsurfers/ocean-road/native': new URL('../../packages/ocean-road/src/native/index.ts', import.meta.url)
+          .pathname,
+        '@/screens': new URL('./src/screens', import.meta.url).pathname,
+        '@/navigations': new URL('./src/navigations', import.meta.url).pathname,
+        '@/lib': new URL('./src/lib', import.meta.url).pathname,
+        '@/ui': new URL('./src/ui', import.meta.url).pathname,
+        '@/features': new URL('./src/features', import.meta.url).pathname,
+        '@/types': new URL('./src/types', import.meta.url).pathname,
+        'assets': new URL('./assets', import.meta.url).pathname,
+      },
+    },
+    module: {
+      rules: [
+        ...Repack.getJsTransformRules(),
+        ...Repack.getAssetTransformRules(),
+        {
+          type: 'javascript/auto',
+          test: /(@hot-updater|hot-updater)/,
+          use: {
+            loader: 'babel-loader', // Babel을 Webpack에서 사용
+            options: {
+              babelrc: false,
+              configFile: false,
+              presets: ['module:@react-native/babel-preset'],
+              plugins: ['hot-updater/babel-plugin'],
+            },
           },
         },
-      },
-    ],
-  },
-  plugins: [new Repack.RepackPlugin(), new ReanimatedPlugin()],
+      ],
+    },
+    plugins: [new Repack.RepackPlugin(), new ReanimatedPlugin()],
+  }
 }
