c64af4bd0745810626feb1a997e8a843d8bb6cb3
yungblud <killi8n@gmail.com>
2025-03-03 10:32:49 +0900

    fix(billets-server): :art: fixed serverless monorepo setup

--- apps/billets-server/tsup.config.ts
+++ apps/billets-server/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/start-server.ts', 'src/lambda.ts'],
+  entry: ['src/lambda.ts'],
   outDir: 'build',
   target: 'node18',
   platform: 'node',
