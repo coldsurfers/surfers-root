5e774fee2eae9ecfbbaa05c8f6f31ab020f1389c
yungblud <killi8n@gmail.com>
2025-03-09 11:52:04 +0900

    feat(billets-web): :art: implemented zustand store for common ui

--- apps/billets-web/app/(ui)/app-header/app-header.search-ui.tsx
+++ apps/billets-web/app/(ui)/app-header/app-header.search-ui.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import { useCommonUIStore } from '@/libs/stores'
 import { SearchIconUI, SearchIconWrapper } from './app-header.styled'
 
 export const AppHeaderSearchUI = () => {
+  const { openFloatingSearchBar } = useCommonUIStore()
   return (
-    <SearchIconWrapper>
+    <SearchIconWrapper onClick={openFloatingSearchBar}>
       <SearchIconUI />
     </SearchIconWrapper>
   )
--- apps/billets-web/app/(ui)/floating-search-bar/floating-search-bar.tsx
+++ apps/billets-web/app/(ui)/floating-search-bar/floating-search-bar.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { GLOBAL_Z_INDEX } from '@/libs/constants'
+import { useCommonUIStore } from '@/libs/stores'
 import { Modal } from '@coldsurfers/ocean-road'
 import { FloatingSearchBarWrapper } from './floating-search-bar.styled'
 
 export const FloatingSearchBar = () => {
+  const { floatingSearchBarVisible, closeFloatingSearchBar } = useCommonUIStore()
   return (
-    <Modal visible onClose={() => {}} zIndex={GLOBAL_Z_INDEX.APP_HEADER + 1}>
+    <Modal visible={floatingSearchBarVisible} onClose={closeFloatingSearchBar} zIndex={GLOBAL_Z_INDEX.APP_HEADER + 1}>
       <FloatingSearchBarWrapper />
     </Modal>
   )
