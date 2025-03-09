3408bc67f4f60530748f7eacf84a696a9a941eb4
yungblud <killi8n@gmail.com>
2025-03-09 20:07:43 +0900

    Revert "Revert "feat(billets-server): :art: utils for generateSlug""
    This reverts commit d114fe7adc075dbdc9b0cbda44b64d23da9d26f5.

---
+++ apps/billets-server/src/lib/utils/utils.generate-slug.ts
@@ -0,0 +1,28 @@
+import slugify from 'slugify'
+import { dbClient } from '../db'
+
+// Function to generate unique slugs
+export async function generateSlug(title: string) {
+  let slug = slugify(title, { lower: true, strict: true })
+
+  // Check for existing slugs in the database
+  let existing = await dbClient.concert.findUnique({
+    where: {
+      slug,
+    },
+  })
+
+  // If slug already exists, append a number
+  if (existing) {
+    let counter = 1
+    let newSlug
+    do {
+      newSlug = `${slug}-${counter}`
+      existing = await dbClient.concert.findUnique({ where: { slug: newSlug } })
+      counter++
+    } while (existing)
+    slug = newSlug
+  }
+
+  return slug
+}
