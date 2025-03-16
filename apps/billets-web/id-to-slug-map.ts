4900dc0de63877d578d508c11247dc50b0c6c280
yungblud <killi8n@gmail.com>
2025-03-16 15:45:32 +0900

    feat(billets-web): :art: modify middleware redirection

---
+++ apps/billets-web/id-to-slug-map.ts
@@ -0,0 +1,7 @@
+// @TODO: automatically create this file from prisma API
+
+const idToSlugMap = {
+  uuid: 'slug',
+} as Record<string, string>
+
+export default idToSlugMap
--- apps/billets-web/middleware.ts
+++ apps/billets-web/middleware.ts
@@ -1,8 +1,24 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
+import idToSlugMapJson from './id-to-slug-map'
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/browse') {
+  const { pathname } = request.nextUrl
+  // /browse 로 올 시 자동으로 /browse/seoul로 리다이렉트
+  if (pathname === '/browse') {
     return NextResponse.redirect(new URL('/browse/seoul', request.url))
+  }
+
+  // /event/[event-id] 패턴으로 올 경우 대비
+  const match = pathname.match(/^\/event\/(\d+)$/)
+
+  if (match) {
+    const eventId = match[1]
+    const slug = idToSlugMapJson[eventId]
+
+    if (slug) {
+      const newUrl = new URL(`/event/${slug}`, request.url)
+      return NextResponse.redirect(newUrl, 301) // 301 (영구 리디렉션)
+    }
   }
 }
