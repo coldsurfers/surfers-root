19fb5d34a87c56125aad7f4f8fde530c66c05b3e
yungblud <killi8n@gmail.com>
2025-03-16 16:04:56 +0900

    feat(billets-web): :art: change middleware

--- apps/billets-web/middleware.ts
+++ apps/billets-web/middleware.ts
@@ -1,5 +1,4 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import idToSlugMapJson from './id-to-slug-map'
 
 export function middleware(request: NextRequest) {
@@ -10,15 +9,25 @@ export function middleware(request: NextRequest) {
   }
 
   // /event/[event-id] 패턴으로 올 경우 대비
-  const match = pathname.match(/^\/event\/(\d+)$/)
+  // UUID 패턴만 매칭 (대소문자 허용)
+  const match = pathname.match(
+    /^\/event\/([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})$/,
+  )
 
   if (match) {
     const eventId = match[1]
     const slug = idToSlugMapJson[eventId]
+    console.log('slug', slug)
 
     if (slug) {
       const newUrl = new URL(`/event/${slug}`, request.url)
       return NextResponse.redirect(newUrl, 301) // 301 (영구 리디렉션)
     }
+    return NextResponse.redirect(new URL('/404', request.url))
   }
+}
+
+// 특정 경로에만 미들웨어 적용
+export const config = {
+  matcher: ['/browse', '/event/:path*'],
 }
