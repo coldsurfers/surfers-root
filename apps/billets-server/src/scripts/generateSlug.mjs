476e4194d5188105228e564bc12985b3da4c37a4
yungblud <killi8n@gmail.com>
2025-03-23 19:17:36 +0900

    feat: :art: updated scripts

--- apps/billets-server/src/scripts/generateSlug.mjs
+++ apps/billets-server/src/scripts/generateSlug.mjs
@@ -6,33 +6,38 @@ const dbClient = new PrismaClient({
 })
 
 export async function generateSlug(title) {
-  await dbClient.$connect()
-  let slug = slugify(title, {
-    replacement: '-', // 공백을 "-"로 변환
-    lower: true, // 소문자로 변환
-    strict: false, // 특수 문자 제거
-    remove: /[[\]*+~.()'"?!:@,&<>〈〉]/g, // 특정 특수문자 제거
-  })
+  try {
+    await dbClient.$connect()
+    let slug = slugify(title, {
+      replacement: '-', // 공백을 "-"로 변환
+      lower: true, // 소문자로 변환
+      strict: false, // 특수 문자 제거
+      remove: /[[\]*+~.()'"?!:@,&<>〈〉]/g, // 특정 특수문자 제거
+    })
 
-  // Check for existing slugs in the database
-  let existing = await dbClient.concert.findUnique({
-    where: {
-      slug,
-    },
-  })
+    // Check for existing slugs in the database
+    let existing = await dbClient.concert.findUnique({
+      where: {
+        slug,
+      },
+    })
 
-  // If slug already exists, append a number
-  if (existing) {
-    let counter = 1
-    let newSlug
-    do {
-      newSlug = `${slug}-${counter}`
-      existing = await dbClient.concert.findUnique({ where: { slug: newSlug } })
-      counter++
-    } while (existing)
-    slug = newSlug
+    // If slug already exists, append a number
+    if (existing) {
+      let counter = 1
+      let newSlug
+      do {
+        newSlug = `${slug}-${counter}`
+        existing = await dbClient.concert.findUnique({ where: { slug: newSlug } })
+        counter++
+      } while (existing)
+      slug = newSlug
+    }
+    return slug
+  } catch (e) {
+    console.error(e)
+    return undefined
+  } finally {
+    await dbClient.$disconnect()
   }
-
-  await dbClient.$disconnect()
-  return slug
 }
