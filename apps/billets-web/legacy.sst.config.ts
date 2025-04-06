61971017be01de78bad2416196f3e3950ac2b71f
yungblud <killi8n@gmail.com>
2025-04-06 19:36:18 +0900

    feat: added legacy.sst.config.ts

---
+++ apps/billets-web/legacy.sst.config.ts
@@ -0,0 +1,42 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// legacy 배포가 필요할 시, sst.config.ts로 복붙해서 사용하세요
+export default $config({
+  app(input) {
+    const name = input?.stage === 'production' ? 'billets-web' : 'billets-web-staging'
+    const removal = input?.stage === 'production' || input?.stage === 'staging' ? 'retain' : 'remove'
+    const protect = ['production', 'staging'].includes(input?.stage)
+    return {
+      name,
+      removal,
+      protect,
+      home: 'aws',
+      providers: {
+        aws: {
+          region: 'ap-northeast-2',
+        },
+      },
+    }
+  },
+  async run() {
+    const name = (() => {
+      switch (process.env.APP_PLATFORM) {
+        case 'production':
+          return 'BilletsWeb'
+        // @ts-ignore
+        case 'staging':
+        default:
+          return 'BilletsWebStaging'
+      }
+    })()
+    new sst.aws.Nextjs(name, {
+      domain: {
+        name: process.env.BILLETS_WEB_DOMAIN_NAME!,
+        cert: process.env.BILLETS_WEB_DOMAIN_CERT_ARN!,
+      },
+    })
+  },
+})
