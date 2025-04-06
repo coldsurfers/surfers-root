399788f6ea547db462c5834172a459bda93ff4c5
yungblud <killi8n@gmail.com>
2025-04-06 18:46:07 +0900

    Revert "feat: add billets subdomain"
    This reverts commit 05f247afd61cc12f80c1cd4e7f49b64555b4cd33.

--- apps/billets-server/src/server.ts
+++ apps/billets-server/src/server.ts
@@ -66,13 +66,7 @@ app.register(cors, {
   allowedHeaders: ['Authorization', 'Content-Type'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  origin: [
-    'http://localhost:3000',
-    'https://coldsurf.io',
-    'https://staging.coldsurf.io',
-    'https://billets.coldsurf.io',
-    'https://staging.billets.coldsurf.io',
-  ],
+  origin: ['http://localhost:3000', 'https://coldsurf.io', 'https://staging.coldsurf.io'],
 })
 
 app.register(jwtPlugin)
