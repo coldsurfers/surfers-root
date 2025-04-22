06329d30e91d275276e6340e5f7426b4235501d9
yungblud <killi8n@gmail.com>
2025-04-22 16:29:30 +0900

    feat(blog): migrate constants

---
+++ apps/billets-web/app/blog/(constants)/index.ts
@@ -0,0 +1,10 @@
+import { Series, SeriesCategory } from '../(types)/series'
+
+export const SITE_URL = 'https://coldsurf.io/blog'
+export const ALL_SERIES: Series[] = ['YMLT', 'YMRT', 'YMWT', 'YMCT']
+export const ALL_SERIES_CATEGORIES: SeriesCategory[] = ['sound', 'tech', 'text', 'video']
+
+export const PAGINATION_PER_LINE = 3
+export const PAGINATION_PER_PAGE = 9
+
+export const TEMP_FIXED_APP_LOCALE = 'ko'
