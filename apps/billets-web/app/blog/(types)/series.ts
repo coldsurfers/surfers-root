58e83430af21340f3fbe477355f9d37cb1b1c6ef
yungblud <killi8n@gmail.com>
2025-04-22 16:50:35 +0900

    feat(blog): migrate types

---
+++ apps/billets-web/app/blog/(types)/series.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod'
+import { AppLocaleSchema } from './i18n'
+
+export const SeriesCategorySchema = z.union([
+  z.literal('video'),
+  z.literal('text'),
+  z.literal('sound'),
+  z.literal('tech'),
+])
+export type SeriesCategory = z.infer<typeof SeriesCategorySchema>
+
+export const SeriesItemSchema = z.object({
+  id: z.string(),
+  createdTime: z.string(),
+  lastEditedTime: z.string(),
+  dateLocale: z.string(),
+  slug: z.string(),
+  title: z.any().array(), // RichTextItemResponse[]
+  status: z.string(),
+  writer: z.object({}), // PartialUserObjectResponse
+  lang: AppLocaleSchema,
+  seriesCategory: SeriesCategorySchema,
+  thumbnailUrl: z.string().nullable(),
+})
+export type SeriesItem = z.infer<typeof SeriesItemSchema>
