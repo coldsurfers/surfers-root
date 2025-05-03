3b44a28b7c9db045d892c57e5986209ca7c018e0
Dong-Ho Choi <killi8n@gmail.com>
2025-05-03 14:25:21 +0900

    fix wamuseum redirect issue (#398)
    * feat: modify dev server
    
    * fix: redirect issue
    
    * chore: update script
    
    * refactor: authUtils

---
+++ apps/wamuseum-client/app/auth/local-logout/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { authUtils } from '@/utils/utils.auth'
+import { Spinner } from '@coldsurfers/ocean-road'
+import { useRouter } from 'next/navigation'
+import { useLayoutEffect } from 'react'
+
+export default function LocalLogoutPage() {
+  const router = useRouter()
+
+  useLayoutEffect(() => {
+    authUtils.localLogout().then(() => {
+      router.replace('/auth/signin')
+    })
+  }, [router])
+
+  return (
+    <>
+      <Spinner variant="page-overlay" />
+    </>
+  )
+}
--- apps/wamuseum-client/middleware.ts
+++ apps/wamuseum-client/middleware.ts
@@ -2,9 +2,18 @@ import { NextRequest, NextResponse } from 'next/server'
 import { COOKIE_ACCESS_TOKEN_KEY } from './utils/constants'
 
 export function middleware(req: NextRequest) {
-  const isLoggedIn = req.cookies.get(COOKIE_ACCESS_TOKEN_KEY)
-  if (!isLoggedIn) {
-    return NextResponse.redirect(new URL('/auth/signin', req.url))
+  // /auth 로 시작하지 않는 경로에 대해서 로그인 여부를 확인한다.
+  if (!req.nextUrl.pathname.startsWith('/auth')) {
+    const isLoggedIn = req.cookies.get(COOKIE_ACCESS_TOKEN_KEY)
+    if (!isLoggedIn) {
+      return NextResponse.redirect(new URL('/auth/local-logout', req.url))
+    }
+  } else {
+    // /auth 로 시작하는 경로에 대해서 로그인 여부를 확인한다.
+    const isLoggedIn = req.cookies.get(COOKIE_ACCESS_TOKEN_KEY)
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
   }
 
   return NextResponse.next()
@@ -14,6 +23,8 @@ export const config = {
   matcher: [
     // https://stackoverflow.com/questions/73229148/uncaught-syntaxerror-expected-expression-got-while-using-next-js-middlewar
     // _next도 추가해주어야 하는데, 이유는 위의 stack overflow에 있다.
-    '/((?!_next|auth|api).*)', // `auth`로 시작하지 않는 모든 경로
+    // '/((?!_next|auth|api).*)', // `/auth`로 시작하지 않는 모든 경로
+    '/((?!_next|api).*)', // client 경로 외의 모든 경로
+    // '/:path*', // 모든 경로
   ],
 }
