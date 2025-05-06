176d5905c4accf3879b126e241b021c064884efd
yungblud <killi8n@gmail.com>
2025-05-06 12:12:12 +0900

    feat: update iOS native staging scheme

--- apps/billets-app/src/lib/constants.ts
+++ apps/billets-app/src/lib/constants.ts
@@ -1,5 +1,6 @@
 import { Dimensions } from 'react-native'
 import Config from 'react-native-config'
+import { match } from 'ts-pattern'
 
 export const TAB_BAR_HEIGHT = 75
 export const CONCERT_DETAIL_LIST_HEADER_HEIGHT = Math.floor(Dimensions.get('screen').height / 2)
@@ -9,6 +10,10 @@ export const GOOGLE_SIGNIN_OPTIONS = {
   iosClientId: Config.GOOGLE_SIGNIN_WEB_IOS_CLIENT_ID ?? '',
 }
 
-export const API_BASE_URL = __DEV__ ? 'https://dev.api.billets.coldsurf.io' : 'https://api.billets.coldsurf.io'
+export const API_BASE_URL = (() => {
+  return match(Config.PLATFORM)
+    .with('development', () => 'https://dev.api.billets.coldsurf.io')
+    .otherwise(() => 'https://api.billets.coldsurf.io')
+})()
 
 export const KOPIS_COPYRIGHT_TEXT = '출처: (재)예술경영지원센터 공연예술통합전산망(www.kopis.or.kr)'
