77ca23c4ef26f9c7b3b03a441a33eb3f5b9f8a4c
yungblud <killi8n@gmail.com>
2025-05-07 21:06:31 +0900

    feat: update 2.0.2 for ota update view

--- apps/billets-app/src/ui/ota-update-view/ota-update-view.tsx
+++ apps/billets-app/src/ui/ota-update-view/ota-update-view.tsx
@@ -1,24 +1,25 @@
 import { colors } from '@coldsurfers/ocean-road'
-import { Text } from '@coldsurfers/ocean-road/native'
+import { Text, useColorScheme } from '@coldsurfers/ocean-road/native'
 import { View } from 'react-native'
 import FastImage from 'react-native-fast-image'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { CommonScreenLayout } from '../common-screen-layout'
 
 export const OtaUpdateView = ({ updateProgressPercentage }: { updateProgressPercentage: number }) => {
+  const { semantics } = useColorScheme()
   return (
     <SafeAreaProvider>
-      <CommonScreenLayout style={{ alignItems: 'center', justifyContent: 'center', paddingHorizontal: 12 }}>
+      <CommonScreenLayout style={{ alignItems: 'center', justifyContent: 'center', paddingHorizontal: 48 }}>
         <FastImage
           source={require('assets/bootsplash/logo.png')}
           style={{
-            width: 124,
-            height: 124,
-            borderRadius: 62,
+            width: 64,
+            height: 64,
+            borderRadius: 32,
           }}
         />
         <View style={{ marginTop: 36, width: '100%', alignItems: 'center', justifyContent: 'center' }}>
-          <View style={{ width: '100%', backgroundColor: colors.oc.white.value, height: 24, borderRadius: 8 }}>
+          <View style={{ width: '100%', backgroundColor: colors.oc.white.value, height: 8, borderRadius: 12 }}>
             <View
               style={{
                 width: `${updateProgressPercentage}%`,
@@ -29,8 +30,11 @@ export const OtaUpdateView = ({ updateProgressPercentage }: { updateProgressPerc
             />
           </View>
           <View style={{ marginTop: 24 }}>
-            <Text weight="medium" style={{ fontSize: 18 }}>
-              {updateProgressPercentage}
+            <Text
+              weight="medium"
+              style={{ fontSize: 14, color: semantics.foreground[1], textAlign: 'center', lineHeight: 24 }}
+            >
+              {`업데이트를 설치하고 있어요\n${updateProgressPercentage}%`}
             </Text>
           </View>
         </View>
