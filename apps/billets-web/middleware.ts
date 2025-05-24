11644692a2aa1b9511e5eaf9eda58b1cbbfa8d62
yungblud <killi8n@gmail.com>
2025-05-24 10:36:57 +0900

    feat(coldsurf-io): :art: middleware

--- apps/billets-web/middleware.ts
+++ apps/billets-web/middleware.ts
@@ -32,11 +32,28 @@ export function middleware(request: NextRequest) {
     }
     return NextResponse.redirect(new URL('/404', request.url))
   }
+
+  // @TODO: temp key
+  const COOKIE_ACCESS_TOKEN_KEY = 'access-token'
+  // 로그인 페이지에 접근 했지만, 이미 로그인 된 경우 '/' 로 리다이렉트
+  if (request.nextUrl.pathname.startsWith('/login')) {
+    const isLoggedIn = request.cookies.get(COOKIE_ACCESS_TOKEN_KEY)
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
+  return NextResponse.next()
 }
 
 // 특정 경로에만 미들웨어 적용
 export const config = {
-  matcher: ['/browse', '/event/:path*'],
+  // matcher: ['/browse', '/event/:path*'],
+  matcher: [
+    // https://stackoverflow.com/questions/73229148/uncaught-syntaxerror-expected-expression-got-while-using-next-js-middlewar
+    // _next도 추가해주어야 하는데, 이유는 위의 stack overflow에 있다.
+    // '/((?!_next|auth|api).*)', // `/auth`로 시작하지 않는 모든 경로
+    '/((?!_next|api).*)', // client 경로 외의 모든 경로
+  ],
   // billets.coldsurf.io 레거시 처리용
   // matcher: ['/:path*'],
 }
