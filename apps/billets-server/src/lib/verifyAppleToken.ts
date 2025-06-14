ea76f4180f0e81d295a2baa00e60576b8620118a
yungblud <killi8n@gmail.com>
2025-06-15 00:15:07 +0900

    fix(billets-server): :art: fix cjs issue

--- apps/billets-server/src/lib/verifyAppleToken.ts
+++ apps/billets-server/src/lib/verifyAppleToken.ts
@@ -1,12 +1,10 @@
-import { createRemoteJWKSet, jwtVerify } from 'jose'
-
 const appleJWKsUrl = new URL('https://appleid.apple.com/auth/keys')
-
-// Apple의 JWK 세트 리모트 로딩 핸들러
-const JWKS = createRemoteJWKSet(appleJWKsUrl)
 
 export async function verifyAppleIdToken(idToken: string, clientId: string) {
   try {
+    const { jwtVerify, createRemoteJWKSet } = await import('jose')
+    // Apple의 JWK 세트 리모트 로딩 핸들러
+    const JWKS = createRemoteJWKSet(appleJWKsUrl)
     const { payload } = await jwtVerify(idToken, JWKS, {
       issuer: 'https://appleid.apple.com',
       audience: clientId,
