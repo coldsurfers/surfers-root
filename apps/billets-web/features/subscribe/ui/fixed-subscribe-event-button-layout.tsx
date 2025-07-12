7a34148d8e86e9d4031f6de5faf11390d645dc84
Dong-Ho Choi <killi8n@gmail.com>
2025-07-12 12:06:16 +0900

    feat(coldsurf-io): subscribe button ui (#419)
    * feat: :art: add subscribe btn ui
    
    * feat: :sparkles: implement uis
    
    * feat: :art: implement featureFlags

---
+++ apps/billets-web/features/subscribe/ui/fixed-subscribe-event-button-layout.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { semantics } from '@coldsurfers/ocean-road';
+import styled from '@emotion/styled';
+import { withStopPropagation } from 'app/shared/lib';
+import { motion } from 'framer-motion';
+import type { PropsWithChildren } from 'react';
+
+const Container = styled(motion.div)`
+    position: absolute;
+    bottom: 12px;
+    right: 12px;
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: ${semantics.color.background[1]};
+    border-radius: 50%;
+
+    box-shadow: 0 1px 3px ${semantics.color.border[1]}, 0 1px 2px ${semantics.color.border[2]};
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+`;
+
+export const FixedSubscribeEventButtonLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <Container
+      onClick={withStopPropagation()}
+      whileHover={{
+        scale: 1.1,
+      }}
+      transition={{ type: 'spring', stiffness: 300 }}
+    >
+      {children}
+    </Container>
+  );
+};
