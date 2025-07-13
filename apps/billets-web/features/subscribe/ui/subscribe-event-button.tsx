60d762e175937a44ccd8574e0f9810bc8c76060f
yungblud <killi8n@gmail.com>
2025-07-13 21:10:32 +0900

    feat: implement event subscribe feature

--- apps/billets-web/features/subscribe/ui/subscribe-event-button.tsx
+++ apps/billets-web/features/subscribe/ui/subscribe-event-button.tsx
@@ -5,7 +5,7 @@ import { useIsLoggedIn, withStopPropagation } from '@/shared/lib';
 import { useLoginModalStore } from '@/shared/store';
 import { semantics } from '@coldsurfers/ocean-road';
 import styled from '@emotion/styled';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Heart } from 'lucide-react';
 import { useCallback } from 'react';
 
@@ -22,15 +22,61 @@ type Props = {
 };
 
 export const SubscribeEventButton = ({ eventId }: Props) => {
+  const queryClient = useQueryClient();
   const { isLoggedIn } = useIsLoggedIn();
   const openLoginModal = useLoginModalStore((state) => state.open);
+  const { data: subscribedEvent } = useQuery({
+    queryKey: apiClient.subscribe.queryKeys.eventSubscribe({ eventId }),
+    queryFn: () =>
+      apiClient.subscribe.getEvent({
+        eventId,
+      }),
+  });
+  const isSubscribed = !!subscribedEvent;
   const { mutate: subscribeEvent } = useMutation({
     mutationFn: () =>
       apiClient.subscribe.subscribeEvent({
         eventId,
       }),
-    onSuccess: (data) => {},
-    onError: (error) => {},
+    onMutate: () => {
+      queryClient.setQueryData<
+        Awaited<ReturnType<typeof apiClient.subscribe.getEvent>> | undefined
+      >(apiClient.subscribe.queryKeys.eventSubscribe({ eventId }), () => {
+        const newData: Awaited<ReturnType<typeof apiClient.subscribe.getEvent>> = {
+          eventId,
+          subscribedAt: new Date().toISOString(),
+          thumbUrl: undefined,
+          userId: '',
+        };
+        return newData;
+      });
+    },
+    onSettled: (data) => {
+      if (!data) {
+        queryClient.removeQueries({
+          queryKey: apiClient.subscribe.queryKeys.eventSubscribe({
+            eventId,
+          }),
+        });
+        return;
+      }
+      queryClient.invalidateQueries({
+        queryKey: apiClient.subscribe.queryKeys.eventSubscribe({
+          eventId,
+        }),
+      });
+    },
+  });
+
+  const { mutate: unsubscribeEvent } = useMutation({
+    mutationFn: () => apiClient.subscribe.unsubscribeEvent({ eventId }),
+    onSettled: () => {
+      queryClient.removeQueries({
+        queryKey: apiClient.subscribe.queryKeys.eventSubscribe({
+          eventId,
+        }),
+      });
+    },
   });
 
   const onClick = useCallback(() => {
@@ -38,12 +84,20 @@ export const SubscribeEventButton = ({ eventId }: Props) => {
       openLoginModal();
       return;
     }
+    if (isSubscribed) {
+      unsubscribeEvent();
+      return;
+    }
     subscribeEvent();
-  }, [subscribeEvent, isLoggedIn, openLoginModal]);
+  }, [subscribeEvent, isLoggedIn, openLoginModal, isSubscribed, unsubscribeEvent]);
 
   return (
     <Container onClick={withStopPropagation(onClick)}>
-      <Heart size={24} color={semantics.color.foreground[4]} />
+      <Heart
+        size={24}
+        color={semantics.color.foreground[4]}
+        fill={isSubscribed ? semantics.color.foreground[4] : 'transparent'}
+      />
     </Container>
   );
 };
