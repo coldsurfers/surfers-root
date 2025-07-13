a0378f58ab12ba68aee20147924998e0a75df385
yungblud <killi8n@gmail.com>
2025-07-13 21:30:10 +0900

    fix(coldsurf-io): :wrench: fixed user fetchQuery

--- apps/billets-web/app/handle/[handle]/page.tsx
+++ apps/billets-web/app/handle/[handle]/page.tsx
@@ -4,44 +4,51 @@ import {
   UserProfileDetailTop,
 } from '@/features/profile';
 import { apiClient } from '@/libs/openapi-client';
+import { ApiErrorBoundaryRegistry } from '@/libs/registries';
 import { getQueryClient } from '@/libs/utils';
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import { notFound } from 'next/navigation';
 
-export default async function HandleDetailPage({ params }: { params: { handle: string } }) {
+export default async function HandleDetailPage({
+  params,
+}: { params: Promise<{ handle: string }> }) {
   const paramsHandle = decodeURIComponent((await params).handle);
   const queryClient = getQueryClient();
-  const user = await queryClient.fetchQuery({
-    queryKey: apiClient.user.queryKeys.me,
-    queryFn: () => apiClient.user.getMe(),
-  });
+  try {
+    // const user = await queryClient.fetchQuery({
+    //   queryKey: apiClient.user.queryKeys.me,
+    //   queryFn: () => apiClient.user.getMe(),
+    // });
 
-  await queryClient.prefetchQuery({
-    queryKey: apiClient.user.queryKeys.profile(paramsHandle),
-    queryFn: () => apiClient.user.getUserProfile(paramsHandle),
-  });
+    await queryClient.prefetchQuery({
+      queryKey: apiClient.user.queryKeys.profile(paramsHandle),
+      queryFn: () => apiClient.user.getUserProfile(paramsHandle),
+    });
+  } catch {}
 
   // @TODO: add modify handle button ui
-  const isAuthenticated = (() => {
-    if (!user?.handle) {
-      return false;
-    }
-    if (!paramsHandle) {
-      return false;
-    }
-    return user.handle === paramsHandle;
-  })();
+  // const isAuthenticated = (() => {
+  //   if (!user?.handle) {
+  //     return false;
+  //   }
+  //   if (!paramsHandle) {
+  //     return false;
+  //   }
+  //   return user.handle === paramsHandle;
+  // })();
 
   if (!paramsHandle) {
     notFound();
   }
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProfileDetailPageLayout>
-        <UserProfileDetailTop handle={paramsHandle} />
-        <UserProfileDetailEventList handle={paramsHandle} />
-      </ProfileDetailPageLayout>
-    </HydrationBoundary>
+    <ApiErrorBoundaryRegistry>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ProfileDetailPageLayout>
+          <UserProfileDetailTop handle={paramsHandle} />
+          <UserProfileDetailEventList handle={paramsHandle} />
+        </ProfileDetailPageLayout>
+      </HydrationBoundary>
+    </ApiErrorBoundaryRegistry>
   );
 }
