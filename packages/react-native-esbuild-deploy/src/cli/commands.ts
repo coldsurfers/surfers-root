728850c5b7cb21c66d7c60920e1bc0db99231c73
yungblud <killi8n@gmail.com>
2025-07-20 20:33:54 +0900

    feat(react-native-esbuild-deploy): :rocket: implement outfile arg

--- packages/react-native-esbuild-deploy/src/cli/commands.ts
+++ packages/react-native-esbuild-deploy/src/cli/commands.ts
@@ -3,6 +3,10 @@ import { Command, Option } from 'commander';
 import { execa } from 'execa';
 import pkg from '../../package.json';
 
+type BuildOptions = {
+  outFile: string;
+};
+
 const { version: pkgVersion } = pkg;
 
 const program = new Command();
@@ -17,11 +21,15 @@ program
 program
   .command('build')
   .description('build react native with esbuild')
-  .action(async () => {
+  .addOption(new Option('-o, --out-file <out-file>', 'The out file to bundle the code for'))
+  .action(async (options: BuildOptions) => {
+    if (!options.outFile) {
+      throw Error('out file option (-o, --out-file) is required');
+    }
     console.log('run esbuild.mjs');
     await execa('rm', ['-rf', 'build/.transpiled']);
     await execa('rm', ['-rf', 'build/out']);
-    await execa('node', ['./scripts/esbuild.mjs']);
+    await execa('node', ['./scripts/esbuild.cli.mjs', '--out-file', options.outFile]);
   });
 
 program.parse(process.argv);
