296123f14442b8bd10530f86b4af893964000373
yungblud <killi8n@gmail.com>
2025-07-20 20:50:01 +0900

    feat(react-native-esbuild-deploy): :zap: implement relative path

--- packages/react-native-esbuild-deploy/src/cli/commands.ts
+++ packages/react-native-esbuild-deploy/src/cli/commands.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import path from 'node:path';
 import { Command, Option } from 'commander';
 import { execa } from 'execa';
 import pkg from '../../package.json';
@@ -27,9 +28,10 @@ program
       throw Error('out file option (-o, --out-file) is required');
     }
     console.log('run esbuild.mjs');
+    const esbuildCliMjs = path.resolve(__dirname, '../../scripts/esbuild.cli.mjs');
     await execa('rm', ['-rf', 'build/.transpiled']);
     await execa('rm', ['-rf', 'build/out']);
-    await execa('node', ['./scripts/esbuild.cli.mjs', '--out-file', options.outFile]);
+    await execa('node', [esbuildCliMjs, '--out-file', options.outFile]);
   });
 
 program.parse(process.argv);
