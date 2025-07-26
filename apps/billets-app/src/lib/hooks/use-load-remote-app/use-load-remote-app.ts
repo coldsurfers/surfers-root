ebd467f9d311d859f1a0c1e4f2b8ed48aa12e377
yungblud <killi8n@gmail.com>
2025-07-26 18:57:40 +0900

    feat(billets-app): :art: enhance use-load-remote-app

--- apps/billets-app/src/lib/hooks/use-load-remote-app/use-load-remote-app.ts
+++ apps/billets-app/src/lib/hooks/use-load-remote-app/use-load-remote-app.ts
@@ -1,5 +1,6 @@
 import { apiClient } from '@/lib/api/openapi-client';
 import { REMOTE_APPS, REMOTE_APP_BUNDLE_HOST_URL } from '@/lib/constants';
+import type { MenuItemProps } from '@coldsurfers/coldsurf-app-settings-app';
 import { loadAsyncScript } from '@coldsurfers/react-native-esbuild-deploy';
 import { useQuery } from '@tanstack/react-query';
 import { type FC, useEffect, useMemo, useState } from 'react';
@@ -9,18 +10,31 @@ type RemoteAppRegistry = {
   type: 'settings';
   components: {
     VersionText: FC<TextProps>;
+    MenuItem: FC<MenuItemProps>;
   };
 };
 
 type RemoteAppByType<T extends RemoteAppRegistry['type']> = Extract<RemoteAppRegistry, { type: T }>;
 
-export const useLoadRemoteApp = (remoteApp: {
-  appName: 'settings';
-  componentName: 'VersionText';
+/**
+ * RemoteApp의 componentName별 정확한 타입을 추출하는 유틸
+ */
+type RemoteAppComponentType<
+  T extends RemoteAppRegistry['type'],
+  C extends keyof RemoteAppByType<T>['components'],
+> = RemoteAppByType<T>['components'][C];
+export const useLoadRemoteApp = <
+  TApp extends RemoteAppRegistry['type'],
+  C extends keyof RemoteAppByType<TApp>['components'],
+>(remoteApp: {
+  appName: TApp;
+  componentName: C;
 }) => {
-  const [remoteAppComponent, setRemoteAppComponent] = useState<
-    RemoteAppByType<typeof remoteApp.appName>['components'][typeof remoteApp.componentName] | null
-  >(null);
+  const [remoteAppComponent, setRemoteAppComponent] = useState<RemoteAppComponentType<
+    TApp,
+    C
+  > | null>(null);
+
   const {
     data: manifest,
     isLoading: isLoadingManifest,
@@ -29,6 +43,7 @@ export const useLoadRemoteApp = (remoteApp: {
     queryKey: apiClient.app.queryKeys.remoteAppManifest,
     queryFn: () => apiClient.app.getRemoteAppManifest(),
   });
+
   const {
     data: remoteLoadedComponent,
     isLoading: isLoadingRemoteLoadedComponent,
@@ -36,26 +51,36 @@ export const useLoadRemoteApp = (remoteApp: {
   } = useQuery({
     queryKey: ['remote-app', remoteApp],
     queryFn: async () => {
-      if (!manifest) {
-        return null;
-      }
+      // if (__DEV__) {
+      //   if (remoteApp.appName === 'settings') {
+      //     const settingsApp = (await import('@coldsurfers/coldsurf-app-settings-app')).default;
+
+      //     console.log('settingsApp', settingsApp);
+
+      //     return settingsApp[
+      //       remoteApp.componentName as keyof typeof settingsApp
+      //     ] as RemoteAppComponentType<TApp, C>;
+      //   }
+      //   return null;
+      // }
+      if (!manifest) return null;
+
       const { latestVersion } = manifest[remoteApp.appName];
       const path = `${REMOTE_APPS[remoteApp.appName].PATH}/v${latestVersion}/index.bundle.js`;
-      const script = await loadAsyncScript<RemoteAppByType<typeof remoteApp.appName>['components']>(
-        {
-          path,
-          bundleHostUrl: REMOTE_APP_BUNDLE_HOST_URL,
-        }
-      );
+
+      const script = await loadAsyncScript<RemoteAppByType<TApp>['components']>({
+        path,
+        bundleHostUrl: REMOTE_APP_BUNDLE_HOST_URL,
+      });
+
       return script[remoteApp.componentName];
     },
+    // enabled: __DEV__ ? true : !!manifest,
     enabled: !!manifest,
   });
 
   useEffect(() => {
-    if (!remoteLoadedComponent) {
-      return;
-    }
+    if (!remoteLoadedComponent) return;
     setRemoteAppComponent(() => remoteLoadedComponent);
   }, [remoteLoadedComponent]);
 
