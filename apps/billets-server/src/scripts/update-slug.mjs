a2e1acd120f07c4e2db05a9db4429f948da576cd
yungblud <killi8n@gmail.com>
2025-07-26 20:42:28 +0900

    feat(billets-server): :hammer: update update-slug script

--- apps/billets-server/src/scripts/update-slug.mjs
+++ apps/billets-server/src/scripts/update-slug.mjs
@@ -1,19 +1,19 @@
-import { PrismaClient } from '@prisma/client'
-import { generateSlug } from './generateSlug.mjs'
+import { PrismaClient } from '@prisma/client';
+import { generateSlug } from './generateSlug.mjs';
 
 const dbClient = new PrismaClient({
   log: ['warn', 'info', 'error'],
-})
+});
 
-async function main() {
-  await dbClient.$connect()
+async function updateSlugForConcerts() {
+  await dbClient.$connect();
   const events = await dbClient.concert.findMany({
     where: {
       slug: null,
     },
-  })
+  });
   const promises = events.map(async (event) => {
-    const slug = await generateSlug(event.title)
+    const slug = await generateSlug(event.title);
     try {
       await dbClient.concert.update({
         where: {
@@ -22,13 +22,39 @@ async function main() {
         data: {
           slug,
         },
-      })
+      });
     } catch (e) {
-      console.error('already existing slug', slug, event.id, e)
+      console.error('already existing slug', slug, event.id, e);
     }
-  })
-  await Promise.allSettled(promises)
-  await dbClient.$disconnect()
+  });
+  await Promise.allSettled(promises);
+  await dbClient.$disconnect();
 }
 
-main()
+async function updateSlugForVenues() {
+  await dbClient.$connect();
+  const venues = await dbClient.venue.findMany({
+    where: {
+      slug: null,
+    },
+  });
+  const promises = venues.map(async (venue) => {
+    const slug = await generateSlug(venue.name);
+    try {
+      await dbClient.concert.update({
+        where: {
+          id: venue.id,
+        },
+        data: {
+          slug,
+        },
+      });
+    } catch (e) {
+      console.error('already existing slug', slug, event.id, e);
+    }
+  });
+  await Promise.allSettled(promises);
+  await dbClient.$disconnect();
+}
+
+updateSlugForVenues();
