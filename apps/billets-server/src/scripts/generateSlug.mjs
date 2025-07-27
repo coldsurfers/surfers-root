fbc81b1e9e922fc57d0f286da1d9c016a618f5fd
yungblud <killi8n@gmail.com>
2025-07-27 20:12:49 +0900

    fix(billets-server): :art: fix safe slug gen

--- apps/billets-server/src/scripts/generateSlug.mjs
+++ apps/billets-server/src/scripts/generateSlug.mjs
@@ -10,7 +10,7 @@ export function getSafeSlug(value) {
     replacement: '-', // 공백을 "-"로 변환
     lower: true, // 소문자로 변환
     strict: false, // 특수 문자 제거
-    remove: /[[\]*+~.()'"?!:@,&<>〈〉#]/g, // 특정 특수문자 제거
+    remove: /[[\]*+~.()'"?!:@,&<>〈〉]/g, // 특정 특수문자 제거
   });
   return slug;
 }
