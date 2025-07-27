d01f7e6dc7812366833230b5615d57d389334193
yungblud <killi8n@gmail.com>
2025-07-27 20:23:47 +0900

    fix(billets-web): :zap: remove generateSafeSlug

--- apps/billets-web/libs/utils/utils.slug.ts
+++ apps/billets-web/libs/utils/utils.slug.ts
@@ -1,15 +1,4 @@
-import slugify from 'slugify';
-
-export const generateSafeSlug = (slug: string) => {
-  return slugify(slug, {
-    replacement: '-', // 공백을 "-"로 변환
-    lower: true, // 소문자로 변환
-    strict: false, // 특수 문자 제거
-    remove: /[[\]*+~.()'"?!:@,&<>〈〉]/g, // 특정 특수문자 제거
-  });
-};
-
 export const generateSlugHref = (slug: string | null) => {
-  const href = slug ? `/event/${encodeURIComponent(generateSafeSlug(slug))}` : '#';
+  const href = slug ? `/event/${encodeURIComponent(slug)}` : '#';
   return href;
 };
