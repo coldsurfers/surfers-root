579cac769bfb62fd3cefc604bc8e39c5cb4de6f6
yungblud <killi8n@gmail.com>
2025-08-03 17:41:22 +0900

    feat(ocean-road-extension): setup build system

---
+++ packages/ocean-road-extension/src/native/common-screen-layout/common-screen-layout.tsx
@@ -0,0 +1,48 @@
+import { colors } from '@coldsurfers/ocean-road';
+import { Spinner, useColorScheme } from '@coldsurfers/ocean-road/native';
+import { type PropsWithChildren, Suspense } from 'react';
+import { type StyleProp, StyleSheet, type ViewStyle } from 'react-native';
+import { type Edges, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useBottomTab } from '../utils';
+
+export const CommonScreenLayout = ({
+  children,
+  style,
+  edges = [],
+  withBottomTab = true,
+}: PropsWithChildren<{
+  edges?: Edges;
+  style?: StyleProp<ViewStyle>;
+  withBottomTab?: boolean;
+}>) => {
+  const { semantics } = useColorScheme();
+  const { bottom: bottomInset } = useSafeAreaInsets();
+  const { tabBarHeight } = useBottomTab();
+  return (
+    <SafeAreaView
+      edges={edges}
+      style={[
+        styles.layout,
+        {
+          backgroundColor: semantics.background[3],
+        },
+        style,
+        {
+          /**
+           * because we set bottom tab bar as position absolute, so we need to calculate individual bottom inset
+           */
+          paddingBottom: withBottomTab ? tabBarHeight - bottomInset : 0,
+        },
+      ]}
+    >
+      <Suspense fallback={<Spinner positionCenter />}>{children}</Suspense>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    backgroundColor: colors.oc.gray[1].value,
+  },
+});
