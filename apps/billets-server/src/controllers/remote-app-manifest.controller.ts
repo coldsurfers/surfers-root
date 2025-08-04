9f9c6f16dd91eec54d7d357fd01ee8d2ac10ede0
yungblud <killi8n@gmail.com>
2025-08-04 22:12:51 +0900

    feat(billets-server): update billets server update info

--- apps/billets-server/src/controllers/remote-app-manifest.controller.ts
+++ apps/billets-server/src/controllers/remote-app-manifest.controller.ts
@@ -16,7 +16,7 @@ export const getRemoteAppManifestHandler = async (
   try {
     return rep.status(200).send({
       settings: {
-        latestVersion: '1.0.1',
+        latestVersion: '1.0.2',
       },
     });
   } catch (e) {
