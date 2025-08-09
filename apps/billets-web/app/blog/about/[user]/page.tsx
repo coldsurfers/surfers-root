13d844490185a15a2c8eb0dfc712c55831c121f9
Dong-Ho Choi <killi8n@gmail.com>
2025-08-09 14:23:26 +0900

    feat(coldsurf-io): enhance blog notion renderer (#452)

--- apps/billets-web/app/blog/about/[user]/page.tsx
+++ apps/billets-web/app/blog/about/[user]/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata() {
 
 const notionPageId = {
   en: 'Resume-2025-1862bbac578280e39760f02a88e17941',
-  ko: 'Resume-2025-1862bbac5782802db432c9b8ee5016b4',
+  ko: 'Resume-2025-2-24a2bbac578280c7a560eff45c8527c4',
 };
 
 const AboutDetailPage = async (props: { params: Promise<{ user: string }> }) => {
