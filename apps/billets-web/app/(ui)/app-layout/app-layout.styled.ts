2f59993a952ec93261f8ecb02cb7fff468eaeb7c
Dong-Ho Choi <killi8n@gmail.com>
2025-08-15 00:24:53 +0900

    Fix/is home (#467)
    * fix(billets-web): fixed home layout
    
    * feat: update changeset

--- apps/billets-web/app/(ui)/app-layout/app-layout.styled.ts
+++ apps/billets-web/app/(ui)/app-layout/app-layout.styled.ts
@@ -1,11 +1,19 @@
 import { APP_HEADER_HEIGHT } from '@/shared/ui/constants';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { APP_CONTAINER_MAX_WIDTH } from '../constants';
 
-export const Container = styled.div`
+export const Container = styled.div<{ $isHome: boolean }>`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+  ${({ $isHome }) =>
+    !$isHome &&
+    css`
+      max-width: ${APP_CONTAINER_MAX_WIDTH}px;
+      margin-left: auto;
+      margin-right: auto;
+    `}
 `;
 
 export const ChildrenWrapper = styled.div`
