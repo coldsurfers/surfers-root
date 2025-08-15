fe753be07dff8788757264468fb050d066aa9a94
yungblud <killi8n@gmail.com>
2025-08-15 17:51:19 +0900

    refactor(billets-web): enhance hooks

--- apps/billets-web/app/(components)/infinite-home-collection/infinite-home-collection.hooks.ts
+++ apps/billets-web/app/(components)/infinite-home-collection/infinite-home-collection.hooks.ts
@@ -1,10 +1,54 @@
+import { apiClient, initialPageQuery } from '@/libs/openapi-client';
 import { useWindowWidth } from '@/shared/lib/use-window-width';
+import type { OpenApiError } from '@coldsurfers/api-sdk';
 import { breakpoints } from '@coldsurfers/ocean-road';
-import { useMemo } from 'react';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { useCallback, useMemo, useState } from 'react';
+import { flushSync } from 'react-dom';
 import { match } from 'ts-pattern';
+import { INFINITE_HOME_COLLECTION_MIN_COUNT } from './infinite-home-collection.constants';
 
-export const useInfiniteHomeCollection = () => {
+type DataT = Awaited<ReturnType<typeof apiClient.venue.getVenueDetailBySlug>>;
+type MutableDataT = {
+  name: string;
+  upcomingEvents: (DataT['upcomingEvents'][number] | { type: 'empty' })[];
+};
+
+export const useInfiniteHomeCollection = (slug: string) => {
   const windowWidth = useWindowWidth();
+
+  const { data: serverData } = useSuspenseQuery<DataT, OpenApiError, MutableDataT>({
+    queryKey: initialPageQuery.homeVenueCollection(slug).queryKey,
+    queryFn: () => apiClient.venue.getVenueDetailBySlug(slug),
+  });
+
+  const clonedData = useMemo(() => {
+    let data: (typeof serverData)['upcomingEvents'] = [];
+    if (serverData.upcomingEvents.length > INFINITE_HOME_COLLECTION_MIN_COUNT) {
+      data = serverData.upcomingEvents.slice(0, INFINITE_HOME_COLLECTION_MIN_COUNT);
+    }
+    if (data.length < INFINITE_HOME_COLLECTION_MIN_COUNT) {
+      while (data.length < INFINITE_HOME_COLLECTION_MIN_COUNT) {
+        data.push(...serverData.upcomingEvents);
+      }
+    }
+    return data;
+  }, [serverData.upcomingEvents]);
+
+  const [infiniteData, setInfiniteData] =
+    useState<(typeof serverData)['upcomingEvents']>(clonedData);
+
+  const flushNextPage = useCallback(() => {
+    flushSync(() => {
+      setInfiniteData((prev) => {
+        const newData = [...prev];
+        const pre = newData.slice(0, perPageItemCount);
+        const middle = newData.slice(perPageItemCount, perPageItemCount * 2);
+        const final = newData.slice(perPageItemCount * 2, perPageItemCount * 3);
+        return [...middle, ...final, ...pre];
+      });
+    });
+  }, []);
 
   const perPageItemCount = useMemo(() => {
     return match(windowWidth)
@@ -44,7 +88,12 @@ export const useInfiniteHomeCollection = () => {
     () => ({
       perPageItemCount,
       itemWidthPercent,
+      data: {
+        collectionItems: infiniteData,
+        collectionTitle: serverData.name,
+      },
+      flushNextPage,
     }),
-    [perPageItemCount, itemWidthPercent]
+    [perPageItemCount, itemWidthPercent, infiniteData, flushNextPage, serverData.name]
   );
 };
--- apps/billets-web/app/(components)/infinite-home-collection/infinite-home-collection.tsx
+++ apps/billets-web/app/(components)/infinite-home-collection/infinite-home-collection.tsx
@@ -1,21 +1,15 @@
 'use client';
 
-import { apiClient, initialPageQuery } from '@/libs/openapi-client';
 import { GlobalLink } from '@/shared/ui';
-import type { OpenApiError } from '@coldsurfers/api-sdk';
 import { semantics } from '@coldsurfers/ocean-road';
 import styled from '@emotion/styled';
-import { useSuspenseQuery } from '@tanstack/react-query';
 import { useAnimation } from 'framer-motion';
 import { ChevronRight } from 'lucide-react';
-import { useCallback, useMemo, useState } from 'react';
-import { flushSync } from 'react-dom';
+import { useCallback } from 'react';
 import { match } from 'ts-pattern';
-import { INFINITE_HOME_COLLECTION_MIN_COUNT } from './infinite-home-collection.constants';
 import { useInfiniteHomeCollection } from './infinite-home-collection.hooks';
 import { InfiniteHomeCollectionItem } from './infinite-home-collection.item';
 import {
-  StyledInfiniteHomeCollectionItemTitle,
   StyledInfiniteHomeCollectionScrollContainer,
   StyledInfiniteHomeCollectionScrollContainerArrow,
   StyledInfiniteHomeCollectionTitle,
@@ -27,40 +21,13 @@ const Wrapper = styled.div`
 
 type Props = {
   slug: string;
-  col?: number;
-};
-
-type DataT = Awaited<ReturnType<typeof apiClient.venue.getVenueDetailBySlug>>;
-type MutableDataT = {
-  name: string;
-  upcomingEvents: (DataT['upcomingEvents'][number] | { type: 'empty' })[];
 };
 
 export const InfiniteHomeCollection = ({ slug }: Props) => {
-  const { data: serverData } = useSuspenseQuery<DataT, OpenApiError, MutableDataT>({
-    queryKey: initialPageQuery.homeVenueCollection(slug).queryKey,
-    queryFn: () => apiClient.venue.getVenueDetailBySlug(slug),
-  });
-
-  const dataToCopy = useMemo(() => {
-    let data: (typeof serverData)['upcomingEvents'] = [];
-    if (serverData.upcomingEvents.length > INFINITE_HOME_COLLECTION_MIN_COUNT) {
-      data = serverData.upcomingEvents.slice(0, INFINITE_HOME_COLLECTION_MIN_COUNT);
-    }
-    if (data.length < INFINITE_HOME_COLLECTION_MIN_COUNT) {
-      while (data.length < INFINITE_HOME_COLLECTION_MIN_COUNT) {
-        data.push(...serverData.upcomingEvents);
-      }
-    }
-    return data;
-  }, [serverData.upcomingEvents]);
-
-  const [infiniteData, setInfiniteData] =
-    useState<(typeof serverData)['upcomingEvents']>(dataToCopy);
+  const { perPageItemCount, itemWidthPercent, data, flushNextPage } =
+    useInfiniteHomeCollection(slug);
 
   const controls = useAnimation();
-
-  const { perPageItemCount, itemWidthPercent } = useInfiniteHomeCollection();
 
   const runInfiniteAnimation = useCallback(() => {
     controls
@@ -72,31 +39,23 @@ export const InfiniteHomeCollection = ({ slug }: Props) => {
         },
       })
       .then(() => {
-        flushSync(() => {
-          setInfiniteData((prev) => {
-            const newData = [...prev];
-            const pre = newData.slice(0, perPageItemCount);
-            const middle = newData.slice(perPageItemCount, perPageItemCount * 2);
-            const final = newData.slice(perPageItemCount * 2, perPageItemCount * 3);
-            return [...middle, ...final, ...pre];
-          });
-        });
+        flushNextPage();
         controls.set({
           transform: `translateX(${0}%)`,
         });
       });
-  }, [controls, itemWidthPercent, perPageItemCount]);
+  }, [controls, flushNextPage, itemWidthPercent, perPageItemCount]);
 
   return (
     <Wrapper>
       <GlobalLink href={`/venue/${slug}`}>
         <StyledInfiniteHomeCollectionTitle as="h2">
-          {serverData.name}
+          {data.collectionTitle}
           <ChevronRight style={{ marginLeft: '0.5rem' }} />
         </StyledInfiniteHomeCollectionTitle>
       </GlobalLink>
       <StyledInfiniteHomeCollectionScrollContainer animate={controls}>
-        {infiniteData.map((value, index) => {
+        {data.collectionItems.map((value, index) => {
           return match(value)
             .with({ type: 'concert' }, (value) => {
               return (
