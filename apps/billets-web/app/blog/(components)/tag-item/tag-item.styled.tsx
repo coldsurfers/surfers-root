9f540bc450434bfbe7a9e45384985003e6009b4d
Dong-Ho Choi <killi8n@gmail.com>
2025-08-15 20:57:33 +0900

    Feat/enhnace blog notion styles (#471)
    * feat(billets-web): modify blog styles
    
    * feat(billets-web): update changeset

--- apps/billets-web/app/blog/(components)/tag-item/tag-item.styled.tsx
+++ apps/billets-web/app/blog/(components)/tag-item/tag-item.styled.tsx
@@ -1,11 +1,11 @@
-import { media, semantics, Text } from '@coldsurfers/ocean-road'
-import { css } from '@emotion/react'
-import styled from '@emotion/styled'
+import { Text, media, semantics } from '@coldsurfers/ocean-road';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 export const StyledTagItem = styled.div`
   background-color: ${semantics.color.background[4]};
   opacity: 0.8;
-  padding: 1rem;
+  padding: 0.75rem;
   border-radius: 24px;
   cursor: pointer;
 
@@ -17,13 +17,13 @@ export const StyledTagItem = styled.div`
   ${media.small(css`
     padding: 0.5rem;
   `)}
-`
+`;
 
 export const StyledTagItemText = styled(Text)`
   margin: unset;
-  font-size: 18px;
+  font-size: 1rem;
 
   ${media.small(css`
     font-size: 14px;
   `)}
-`
+`;
