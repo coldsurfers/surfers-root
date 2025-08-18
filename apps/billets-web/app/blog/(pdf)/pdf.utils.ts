f6c80413b949785ceeec9928db14c2298df080c5
yungblud <killi8n@gmail.com>
2025-08-18 13:14:08 +0900

    feat(billets-web): implement pdf library types

--- apps/billets-web/app/blog/(pdf)/pdf.utils.ts
+++ apps/billets-web/app/blog/(pdf)/pdf.utils.ts
@@ -1,24 +1,25 @@
+import type { Options } from 'html2pdf.js';
+
 export const generatePDF = async () => {
   // Set options for html2pdf
-  const options = {
+  const options: Options = {
     margin: 0.25,
     filename: 'website_screenshot.pdf',
     image: { type: 'jpeg', quality: 0.98 },
     html2canvas: { scale: 1.5 },
     jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
     pagebreak: { mode: 'avoid-all' },
-  }
+  };
 
   // Select the element to capture
-  const element = document.body
+  const element = document.body;
 
-  // @ts-expect-error: html2pdf.js has no types
-  const { default: html2pdf } = await import('html2pdf.js')
+  const { default: html2pdf } = await import('html2pdf.js');
   // Generate PDF
   html2pdf()
     .from(element)
     .set({
       ...options,
     })
-    .save()
-}
+    .save();
+};
