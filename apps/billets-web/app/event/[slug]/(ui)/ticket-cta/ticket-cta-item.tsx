036bac53f86880a6fe23c65702c0e954048ef5fe
yungblud <killi8n@gmail.com>
2025-08-30 11:53:04 +0900

    feat(billets-web): add utm_source

--- apps/billets-web/app/event/[slug]/(ui)/ticket-cta/ticket-cta-item.tsx
+++ apps/billets-web/app/event/[slug]/(ui)/ticket-cta/ticket-cta-item.tsx
@@ -1,17 +1,27 @@
-'use client'
+'use client';
 
-import { logEvent } from '@/features/firebase/firebase'
-import { Button } from '@coldsurfers/ocean-road'
-import Link from 'next/link'
-import { memo, useCallback } from 'react'
-import { StyledTicketCTAContainer, StyledTicketCTALeft, StyledTicketSellerText } from './ticket-cta.styled'
+import { logEvent } from '@/features/firebase/firebase';
+import { Button } from '@coldsurfers/ocean-road';
+import Link from 'next/link';
+import { memo, useCallback, useMemo } from 'react';
+import {
+  StyledTicketCTAContainer,
+  StyledTicketCTALeft,
+  StyledTicketSellerText,
+} from './ticket-cta.styled';
 
 type TicketCtaItemProps = {
-  seller: string
-  sellingURL: string
-}
+  seller: string;
+  sellingURL: string;
+};
 
 export const TicketCtaItem = memo(({ seller, sellingURL }: TicketCtaItemProps) => {
+  const linkHref = useMemo(() => {
+    const url = new URL(sellingURL);
+    url.searchParams.set('utm_source', 'coldsurf.io');
+    return url.toString();
+  }, [sellingURL]);
+
   const onClick = useCallback(() => {
     logEvent({
       name: 'click_find_ticket',
@@ -19,19 +29,19 @@ export const TicketCtaItem = memo(({ seller, sellingURL }: TicketCtaItemProps) =
         seller_name: seller,
         url: sellingURL,
       },
-    })
-  }, [seller, sellingURL])
+    });
+  }, [seller, sellingURL]);
 
   return (
     <StyledTicketCTAContainer key={seller}>
       <StyledTicketCTALeft>
         <StyledTicketSellerText as="p">{seller}</StyledTicketSellerText>
       </StyledTicketCTALeft>
-      <Link href={sellingURL} target="_blank" onClick={onClick}>
+      <Link href={linkHref} target="_blank" onClick={onClick}>
         <Button theme="pink" size="md" textWeight="bold" rightIcon={'SquareArrowOutUpRight'}>
           티켓 찾기
         </Button>
       </Link>
     </StyledTicketCTAContainer>
-  )
-})
+  );
+});
