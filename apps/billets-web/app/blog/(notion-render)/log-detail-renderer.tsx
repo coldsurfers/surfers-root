d08f0fcbb404707d5cc82a17c7610ca7ac7b6fdc
yungblud <killi8n@gmail.com>
2025-08-30 15:45:27 +0900

    feat(billets-web): integrate nuqs for locale management and enhance blog rendering
    - Added NuqsAdapter to the layout for improved locale handling.
    - Updated blog fetcher to include appLocale in the API request.
    - Enhanced LogDetailRenderer to display available locales and allow locale switching.
    - Modified AppLocale schema to support multiple languages.
    - Updated package dependencies in pnpm-lock.yaml and package.json.

--- apps/billets-web/app/blog/(notion-render)/log-detail-renderer.tsx
+++ apps/billets-web/app/blog/(notion-render)/log-detail-renderer.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { Text, media } from '@coldsurfers/ocean-road';
+import { Text, colors, media, semantics } from '@coldsurfers/ocean-road';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import type { PersonUserObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { format } from 'date-fns';
 import { useMemo } from 'react';
 import { PageLayout } from '../(components)/page-layout';
 import { TagList } from '../(components)/tag-list/tag-list';
@@ -13,12 +14,50 @@ import type { AppLocale } from '../(types)/i18n';
 import type { SeriesCategory } from '../(types)/series';
 import { NotionRenderer } from './notion-renderer';
 
+const AVAILABLE_APP_LOCALES: AppLocale[] = ['ko', 'en'];
+const APP_LOCALES_TO_DISPLAY_NAMES: Record<AppLocale, string> = {
+  ko: '한국어',
+  en: 'English',
+};
+
 const WriterText = styled(Text)`
   font-size: 20px;
+
+  text-align: center;
+  margin-top: 1rem;
+  margin-bottom: 0.25rem;
 
   ${media.small(css`
     font-size: 16px;
   `)}
+`;
+
+const CreatedAtText = styled(Text)`
+  font-size: 16px;
+
+  text-align: center;
+
+  color: ${semantics.color.foreground[4]};
+
+  margin: unset;
+
+  ${media.small(css`
+    font-size: 14px;
+  `)}
+`;
+
+const AppLocaleText = styled(Text)`
+  font-size: 1.125rem;
+
+  text-align: center;
+
+  margin: unset;
+
+  color: ${colors.oc.blue[8].value};
+`;
+
+const AppLocaleTextSeparator = styled.span`
+  color: ${semantics.color.foreground[4]};
 `;
 
 const RendererSection = styled.section`
@@ -29,14 +68,24 @@ const RendererSection = styled.section`
   `)}
 `;
 
+const AppLocalesWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 1rem;
+`;
+
 export const LogDetailRenderer = ({
   slug,
   locale,
   seriesCategory,
+  onClickAppLocale,
 }: {
   slug: string;
   locale: AppLocale;
   seriesCategory: SeriesCategory;
+  onClickAppLocale: (appLocale: AppLocale) => void;
 }) => {
   const { data } = useSuspenseQuery(
     queryKeyFactory.series.item(slug, {
@@ -44,6 +93,7 @@ export const LogDetailRenderer = ({
       seriesCategory,
     })
   );
+
   const page = useMemo(() => data?.page, [data]);
   const recordMap = useMemo(() => data?.recordMap, [data?.recordMap]);
 
@@ -71,23 +121,44 @@ export const LogDetailRenderer = ({
     [page]
   );
 
+  const formattedCreatedAt = useMemo(
+    () =>
+      format(
+        new Date((data.page.properties['Publish date'] as { date: { start: string } }).date.start),
+        'MMMM d, yyyy'
+      ),
+    [data.page.properties['Publish date']]
+  );
+
   return (
     <PageLayout title={pageTitle?.at(0)?.plain_text}>
       <article style={{ marginTop: '2rem' }}>
         <TagList tags={tags} />
-        <div
-          style={{
-            textAlign: 'center',
-            marginBottom: '1rem',
-            marginTop: '1rem',
-          }}
-        >
-          <WriterText>Written by {writerName}</WriterText>
-        </div>
-        <RendererSection>
-          {recordMap && <NotionRenderer recordMap={recordMap} />}
-          {/* <CommonBack /> */}
-        </RendererSection>
+        <AppLocalesWrapper>
+          {AVAILABLE_APP_LOCALES.map((appLocale, index) => (
+            <div
+              key={appLocale}
+              onClick={() => onClickAppLocale(appLocale)}
+              onKeyUp={(e) => e.key === 'Enter' && onClickAppLocale(appLocale)}
+              // biome-ignore lint/a11y/useSemanticElements: <explanation>
+              role="button"
+              tabIndex={0}
+              style={{ cursor: 'pointer' }}
+            >
+              <AppLocaleText as="p">
+                {APP_LOCALES_TO_DISPLAY_NAMES[appLocale]}
+                {index !== AVAILABLE_APP_LOCALES.length - 1 && (
+                  <AppLocaleTextSeparator> | </AppLocaleTextSeparator>
+                )}
+              </AppLocaleText>
+            </div>
+          ))}
+        </AppLocalesWrapper>
+        <WriterText as="p">
+          Written by <span style={{ fontWeight: 'bold' }}>{writerName}</span>
+        </WriterText>
+        <CreatedAtText as="p">{formattedCreatedAt}</CreatedAtText>
+        <RendererSection>{recordMap && <NotionRenderer recordMap={recordMap} />}</RendererSection>
       </article>
     </PageLayout>
   );
--- apps/billets-web/app/blog/[series]/[slug]/page.tsx
+++ apps/billets-web/app/blog/[series]/[slug]/page.tsx
@@ -3,7 +3,13 @@ import { Suspense, use } from 'react';
 
 import { GlobalErrorBoundaryRegistry } from '@/libs/registries/global-error-boundary-registry';
 import { LogDetailRenderer } from 'app/blog/(notion-render)/log-detail-renderer';
+import { AppLocaleSchema } from 'app/blog/(types)/i18n';
 import type { SeriesCategory } from 'app/blog/(types)/series';
+import { parseAsStringLiteral, useQueryState } from 'nuqs';
+
+export const appLocaleParser = parseAsStringLiteral(
+  AppLocaleSchema.options // -> readonly ['ko','en']
+).withDefault('ko');
 
 export default function SeriesSlugPage(props: {
   params: Promise<{
@@ -12,10 +18,17 @@ export default function SeriesSlugPage(props: {
   }>;
 }) {
   const params = use(props.params);
+  const [lang, setLang] = useQueryState('lang', appLocaleParser);
+
   return (
     <GlobalErrorBoundaryRegistry>
       <Suspense>
-        <LogDetailRenderer slug={params.slug} locale={'ko'} seriesCategory={params.series} />
+        <LogDetailRenderer
+          slug={params.slug}
+          locale={lang}
+          seriesCategory={params.series}
+          onClickAppLocale={setLang}
+        />
       </Suspense>
     </GlobalErrorBoundaryRegistry>
   );
