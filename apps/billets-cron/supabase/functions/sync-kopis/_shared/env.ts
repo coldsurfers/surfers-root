ef9be10c5d70d59a9f67760f34bec245736fc5bc
yungblud <killi8n@gmail.com>
2025-09-02 19:31:48 +0900

    feat(prisma-schema): update prisma schema

--- apps/billets-cron/supabase/functions/sync-kopis/_shared/env.ts
+++ apps/billets-cron/supabase/functions/sync-kopis/_shared/env.ts
@@ -2,3 +2,4 @@ export const kopisKey = Deno.env.get('KOPIS_KEY') ?? '';
 export const dataApiUrl = Deno.env.get('DATA_API_URL') ?? '';
 export const dataApiKey = Deno.env.get('DATA_API_KEY') ?? '';
 export const slackWebhookUrl = Deno.env.get('SLACK_WEBHOOK_URL') ?? '';
+export const adminHost = Deno.env.get('ADMIN_HOST') ?? '';
