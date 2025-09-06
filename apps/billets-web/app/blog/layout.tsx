de1d1fc3f95386211dc5186c00b67257561ec3a8
Dong-Ho Choi <killi8n@gmail.com>
2025-09-06 09:59:23 +0900

    feat(billets-web): enhance ssg for blog landing (#516)
    * feat(billets-web): enhance blog layout and series fetching
    
    - Updated RouteLoading component to accept optional dependencies for loading state management.
    - Simplified BlogLayout by removing unnecessary data fetching and hydration logic.
    - Implemented static fetching for series data in RootPage, improving performance and reducing load times.
    - Refactored SeriesListAll to utilize initial data and suspense queries for better data handling.
    - Updated pagination component to use GlobalLink for navigation.
    - Added static parameters generation for user pages in the about section.
    
    * chore: add changeset for soft lies film blog layout enhancement
    
    * refactor(billets-web): optimize series data fetching and component hydration
    
    - Replaced static data fetching in RootPage with React Query's prefetching for improved performance.
    - Updated SeriesListAll component to remove initialData prop and utilize suspense queries for data handling.
    - Wrapped PageLayout in HydrationBoundary to manage server-side rendered state more effectively.

--- apps/billets-web/app/blog/layout.tsx
+++ apps/billets-web/app/blog/layout.tsx
@@ -1,23 +1,7 @@
-import { getQueryClient } from '@/libs/utils';
-import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
-import { type ReactNode, Suspense } from 'react';
-import { queryKeyFactory } from './(react-query)/react-query.key-factory';
+import type { ReactNode } from 'react';
 
 export const revalidate = 3600;
 
-export const dynamic = 'force-static';
-
 export default async function BlogLayout({ children }: { children: ReactNode }) {
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery({
-    ...queryKeyFactory.series.listAll('ko'),
-  });
-
-  const dehydratedState = dehydrate(queryClient);
-
-  return (
-    <Suspense>
-      <HydrationBoundary state={dehydratedState}>{children}</HydrationBoundary>
-    </Suspense>
-  );
+  return children;
 }
