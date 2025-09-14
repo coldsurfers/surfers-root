3f3f02de12042056d3629a97841923e3cb2cd99a
yungblud <killi8n@gmail.com>
2025-09-14 14:05:15 +0900

    fix: fixed nav text

--- apps/billets-web/app/browse/[city]/(components)/navigation/navigation.styled.ts
+++ apps/billets-web/app/browse/[city]/(components)/navigation/navigation.styled.ts
@@ -1,9 +1,9 @@
-import { GLOBAL_Z_INDEX } from '@/libs/constants'
-import { Button, colors, semantics, Text } from '@coldsurfers/ocean-road'
-import { css } from '@emotion/react'
-import styled from '@emotion/styled'
-import { motion } from 'framer-motion'
-import { Disc3, MapPin, MicVocal, VenetianMask } from 'lucide-react'
+import { GLOBAL_Z_INDEX } from '@/libs/constants';
+import { Button, Text, colors, semantics } from '@coldsurfers/ocean-road';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+import { Disc3, MapPin, MicVocal, VenetianMask } from 'lucide-react';
 
 export const NavContainer = styled.nav`
   display: flex;
@@ -11,7 +11,7 @@ export const NavContainer = styled.nav`
   & + & {
     margin-top: 0.75rem;
   }
-`
+`;
 
 export const NavButton = styled(Button)<{ $isActive: boolean }>`
   background: initial;
@@ -24,40 +24,41 @@ export const NavButton = styled(Button)<{ $isActive: boolean }>`
   align-items: center;
 
   padding: 1rem;
-`
+`;
 
 export const NavBtnText = styled(Text)`
   font-size: 16px;
   font-weight: 500;
-`
+  color: ${semantics.color.foreground[1]};
+`;
 
 export const MapPinIcon = styled(MapPin)`
   color: ${semantics.color.foreground[1]};
   width: 20px;
   height: 20px;
   margin-right: 0.25rem;
-`
+`;
 
 export const MicVocalIcon = styled(MicVocal)`
   color: ${semantics.color.foreground[1]};
   width: 16px;
   height: 16px;
   margin-right: 0.5rem;
-`
+`;
 
 export const TheatreIcon = styled(VenetianMask)`
   color: ${semantics.color.foreground[1]};
   width: 16px;
   height: 16px;
   margin-right: 0.5rem;
-`
+`;
 
 export const DanceIcon = styled(Disc3)`
   color: ${semantics.color.foreground[1]};
   width: 16px;
   height: 16px;
   margin-right: 0.5rem;
-`
+`;
 
 export const NavigationDropdownMotionDiv = styled(motion.div)`
   background-color: ${semantics.color.background[4]};
@@ -66,18 +67,18 @@ export const NavigationDropdownMotionDiv = styled(motion.div)`
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
   z-index: ${GLOBAL_Z_INDEX.APP_HEADER + 2};
-`
+`;
 
 export const DropdownItemSectionHeader = styled.div`
   padding: 1rem;
-`
+`;
 
 export const DropdownItemSectionHeaderTitle = styled(Text)`
   font-size: 18px;
   font-weight: 500;
   margin: unset;
   color: ${semantics.color.foreground[4]};
-`
+`;
 
 export const DropdownItem = styled.div<{ $isLast?: boolean }>`
   padding-left: 1rem;
@@ -95,11 +96,11 @@ export const DropdownItem = styled.div<{ $isLast?: boolean }>`
       border-bottom-left-radius: 8px;
       border-bottom-right-radius: 8px;
     `}
-`
+`;
 
 export const DropdownItemText = styled(Text)<{ $isSelected?: boolean }>`
   font-size: 16px;
   font-weight: 400;
   margin: unset;
   color: ${(props) => (props.$isSelected ? colors.oc.indigo[4].value : semantics.color.foreground[3])};
-`
+`;
