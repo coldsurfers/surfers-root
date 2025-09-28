b03bd5e7e12bb316db71bbe696622211aa11bf4f
yungblud <killi8n@gmail.com>
2025-09-28 12:25:27 +0900

    refactor(coldsurf-io): check diff on header scroll ui

--- apps/billets-web/shared/ui/app-header/app-header.tsx
+++ apps/billets-web/shared/ui/app-header/app-header.tsx
@@ -23,10 +23,13 @@ export function AppHeader() {
     let lastScrollTop = 0;
     const onScroll = () => {
       const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-      if (currentScroll > lastScrollTop) {
-        setAnimation('hide');
-      } else {
-        setAnimation('show');
+      const diff = Math.abs(currentScroll - lastScrollTop);
+      if (diff >= 30) {
+        if (currentScroll > lastScrollTop) {
+          setAnimation('hide');
+        } else {
+          setAnimation('show');
+        }
       }
       lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
     };
