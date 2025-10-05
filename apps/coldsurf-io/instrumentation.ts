649f58bcf94467c84623c7fbb281c1824ff947aa
Dong-Ho Choi <killi8n@gmail.com>
2025-10-06 00:05:34 +0900

    fix(coldsurf-io): :wrench: fixed sentry dynamic import issue (#585)

--- apps/coldsurf-io/instrumentation.ts
+++ apps/coldsurf-io/instrumentation.ts
@@ -1,15 +1,18 @@
 import * as Sentry from '@sentry/nextjs';
 
 export async function register() {
-  if (process.env.NODE_ENV !== 'production') {
-    return;
-  }
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
-  }
+  if (process.env.NEXT_RUNTIME === 'nodejs' || process.env.NEXT_RUNTIME === 'edge') {
+    if (process.env.NODE_ENV === 'production') {
+      Sentry.init({
+        dsn: process.env.SENTRY_DSN,
 
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+        // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+        tracesSampleRate: 1,
+
+        // Setting this option to true will print useful information to the console while you're setting up Sentry.
+        debug: false,
+      });
+    }
   }
 }
 
