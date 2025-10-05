9598018d1d4ea83b34a13ff9e9b4ae979da08cc4
yungblud <killi8n@gmail.com>
2025-10-06 00:20:02 +0900

    fix(coldsurf-io): fixed sst app platform

--- apps/coldsurf-io/sst.config.ts
+++ apps/coldsurf-io/sst.config.ts
@@ -5,11 +5,8 @@ dotenv.config({
   path: '.env.production',
 });
 
-let inputStage: 'production' | 'staging' | undefined = undefined;
-
 export default $config({
   app(input) {
-    inputStage = input?.stage as 'production' | 'staging' | undefined;
     const name =
       input?.stage === 'production' ? 'coldsurf-io-client' : 'coldsurf-io-client-staging';
     const removal =
@@ -28,9 +25,6 @@ export default $config({
     };
   },
   async run() {
-    if (!inputStage) {
-      throw new Error('inputStage is not set');
-    }
     const name = (() => {
       switch (process.env.APP_PLATFORM) {
         case 'production':
@@ -41,7 +35,7 @@ export default $config({
     })();
 
     const domain = (() => {
-      switch (inputStage) {
+      switch (process.env.APP_PLATFORM) {
         case 'production':
           return {
             name: process.env.COLDSURF_IO_DOMAIN_NAME ?? '',
