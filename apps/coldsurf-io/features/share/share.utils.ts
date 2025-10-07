4fb27ab3ae7b0ae8e56512c7b75998216253a926
yungblud <killi8n@gmail.com>
2025-10-07 20:57:51 +0900

    feat(coldsurf-io): add share facbeook

--- apps/coldsurf-io/features/share/share.utils.ts
+++ apps/coldsurf-io/features/share/share.utils.ts
@@ -17,3 +17,18 @@ export const shareTwitter = ({
 
   window.open(shareUrl.toString(), '_blank', 'noopener,noreferrer,width=600,height=400');
 };
+
+export const shareFacebook = ({
+  url,
+  quote,
+}: {
+  url: string;
+  quote: string;
+}) => {
+  const shareUrl = new URL('https://www.facebook.com/sharer/sharer.php');
+  shareUrl.searchParams.set('u', url);
+  if (quote) shareUrl.searchParams.set('quote', quote);
+
+  // 새 창 열기
+  window.open(shareUrl.toString(), '_blank', 'noopener,noreferrer,width=600,height=500');
+};
