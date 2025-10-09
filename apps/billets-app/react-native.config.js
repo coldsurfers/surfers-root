d421b7baa21bca999b45ff5a089869f922060258
yungblud <killi8n@gmail.com>
2025-10-09 13:08:06 +0900

    chore(billets-app): version bump

--- apps/billets-app/react-native.config.js
+++ apps/billets-app/react-native.config.js
@@ -4,9 +4,5 @@ module.exports = {
     android: {},
   },
   assets: ['./assets/fonts/'],
-  // dependencies: {
-  //   // https://github.com/lugg/react-native-config/issues/848
-  //   'react-native-config': { platforms: { android: null } },
-  // },
   // commands: require('@callstack/repack/commands/rspack'),
 };
