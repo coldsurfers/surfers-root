affaa0ec8ffe0f2df53ac539bb6922321666796b
yungblud <killi8n@gmail.com>
2025-10-11 20:55:18 +0900

    feat(coldsurf-io): api route for tags

--- apps/coldsurf-io/app/api/blog/tags/route.ts
+++ apps/coldsurf-io/app/api/blog/tags/route.ts
@@ -1,8 +1,9 @@
 import { queryTags } from 'app/blog/(notion)/query';
 import { type NextRequest, NextResponse } from 'next/server';
 
-export async function GET(_: NextRequest) {
-  const tags = await queryTags();
+export async function GET(request: NextRequest) {
+  const isOfficialBlogParam = request.nextUrl.searchParams.get('isOfficialBlog');
+  const tags = await queryTags(isOfficialBlogParam === 'true')();
   return NextResponse.json(
     {
       tags,
