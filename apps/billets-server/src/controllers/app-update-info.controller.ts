21c499f871c35284ae573b859eb7a532593cd658
yungblud <killi8n@gmail.com>
2025-10-16 23:23:10 +0900

    chore(billets-server): update app update info

--- apps/billets-server/src/controllers/app-update-info.controller.ts
+++ apps/billets-server/src/controllers/app-update-info.controller.ts
@@ -2,9 +2,9 @@ import type { AppUpdateInfoDTO } from '@/dtos/app-update-info.dto';
 
 const HARD_CODED_APP_UPDATE_INFO: AppUpdateInfoDTO = {
   ios: {
-    forceUpdate: true,
-    latestVersion: '2.3.0',
-    updateType: 'native',
+    forceUpdate: false,
+    latestVersion: '2.5.3',
+    updateType: 'ota',
   },
   android: {
     forceUpdate: true,
